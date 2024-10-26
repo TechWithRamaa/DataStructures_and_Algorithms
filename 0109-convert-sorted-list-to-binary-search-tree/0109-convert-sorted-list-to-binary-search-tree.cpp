06754a83e62815eef5f0b92f299be9e50c32b4d6
Ramaa <rkramadhevi@gmail.com>
2024-10-26 16:08:55 +0530

    Time: 4 ms (95.61%), Space: 31.7 MB (14.05%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (!head)
+            return nullptr;
+
+        ListNode* middle = findMiddleElement(head);
+
+        TreeNode* root = new TreeNode(middle->val);
+
+        if (head != middle)
+            root->left = sortedListToBST(head);
+        root->right = sortedListToBST(middle->next);
+
+        return root;
+    }
+
+    ListNode* findMiddleElement(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        if (prev)
+            prev->next = nullptr;
+
+        return slow;
+    }
+};
