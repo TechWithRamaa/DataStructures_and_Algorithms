ae81ce66923ac91bd9f233395f93c0ba0d2e2206
Ramaa <rkramadhevi@gmail.com>
2024-11-12 20:18:26 +0530

    Time: 19 ms (23.87%), Space: 17.7 MB (81.35%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,45 @@
+#include<algorithm>
+
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        const int ROWS = matrix.size();
+        const int COLS = matrix[0].size();
+        vector<int> histogram(COLS, 0);
+        int maxArea = 0;
+
+        for(int i  = 0; i < ROWS; i++) {
+            for(int j = 0; j < COLS; j++) {
+               histogram[j] = matrix[i][j] == '1' ? histogram[j] + 1 : 0;
+            }
+            maxArea = std::max(maxArea, rectangleArea(histogram));
+        }
+
+        return maxArea;
+    }
+
+private:
+    int rectangleArea(vector<int> & histogram) {
+        const int COLS = histogram.size();
+        stack<int> s;
+        int maxArea = 0;
+
+        for(int i = 0; i <= COLS; i++) {
+            
+            while(!s.empty() && (i == COLS || histogram[s.top()] >= histogram[i])) {
+                int height = histogram[s.top()];
+                s.pop();
+
+                int width;
+                
+                if(s.empty()) width = i;
+                else width = i - s.top() - 1;
+                maxArea = std :: max(maxArea, height * width);
+            }
+
+            s.push(i);
+        }
+
+        return maxArea;
+    }
+};
