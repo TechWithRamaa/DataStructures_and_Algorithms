d9c8af7c51efe987df5240528cecb31f063db43f
Ramaa <rkramadhevi@gmail.com>
2024-11-13 21:33:27 +0530

    Time: 0 ms (100%), Space: 93.3 MB (36.38%) - LeetHub

--- 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.cpp
@@ -14,27 +14,18 @@ public:
     int longestZigZag(TreeNode* root) {
         if(!root) return 0;
 
-        std :: unordered_map<const TreeNode*, std :: pair<int, int>> memo;
-
-        int leftPathLength = dfs(root -> left, true, 1, memo);
-        int rightPathLength = dfs(root -> right, false, 1, memo);
+        int leftPathLength = dfs(root -> left, true, 1);
+        int rightPathLength = dfs(root -> right, false, 1);
 
         return std :: max(leftPathLength, rightPathLength);
     }
 private:
-    int dfs(const TreeNode* node, bool isLeft, int length,
-            std :: unordered_map<const TreeNode*, std :: pair<int, int>>& memo) {
-        
-        if(!node) 
+    int dfs(const TreeNode* root, bool isLeft, int length) {
+        if(!root) 
             return length - 1;
 
-        if(memo.find(node) != memo.end())
-            return isLeft ? memo[node].first : memo[node].second;
-
-        int leftPathLength = dfs(node -> left, true, isLeft ? 1 : length + 1, memo);
-        int rightPathLength = dfs(node -> right, false, isLeft ? length + 1 : 1, memo);
-
-        memo[node] = {leftPathLength, rightPathLength};
+        int leftPathLength = dfs(root -> left, true, isLeft ? 1 : length + 1);
+        int rightPathLength = dfs(root -> right, false, isLeft ? length + 1 : 1);
 
         return std :: max(leftPathLength, rightPathLength);
     }
