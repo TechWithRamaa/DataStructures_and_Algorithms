799423face429a931a90f760c9673a1fa8b4da55
Ramaa <rkramadhevi@gmail.com>
2024-11-24 19:18:36 +0530

    Time: 0 ms (100%), Space: 27 MB (79.61%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int majorityElement(const vector<int>& nums) {
+        return majorityElementByBoyerMooreVoting(nums);
+    }
+private:
+    int majorityElementByBoyerMooreVoting(const vector<int>& nums) {
         int count = 0;
         int candidate = 0;
 
         for (const int& num : nums) {
             if (count == 0) {
                 candidate = num;
-                count++;
+                count = 1;
             } else if (num != candidate)
                 count--;
             else if (num == candidate)
