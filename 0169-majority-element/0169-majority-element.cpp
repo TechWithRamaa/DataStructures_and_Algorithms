1715f71857cc528e075fc855faf3b2637c382559
Ramaa <rkramadhevi@gmail.com>
2024-11-24 19:22:20 +0530

    Time: 0 ms (100%), Space: 27.1 MB (40.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,7 +3,22 @@ public:
     int majorityElement(const vector<int>& nums) {
         return majorityElementByBoyerMooreVoting(nums);
     }
+
 private:
+    int majorityElementByHashing(const vector<int>& nums) {
+        unordered_map<int, int> frequencyMap;
+        const int N = nums.size();
+
+        for (const int& num : nums) {
+            frequencyMap[num] += 1;
+
+            if (frequencyMap[num] > N / 2)
+                return num;
+        }
+
+        return -1;
+    }
+
     int majorityElementByBoyerMooreVoting(const vector<int>& nums) {
         int count = 0;
         int candidate = 0;
