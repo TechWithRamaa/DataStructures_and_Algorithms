989ff216a5b07ef6eac882dea24ad581304ee521
Ramaa <rkramadhevi@gmail.com>
2024-11-24 18:09:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (96.79%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-   vector<int> spiralOrder(vector<vector<int>>& matrix) {
-    vector<int> result;
-    
-    const int ROWS = matrix.size();
-    const int COLUMNS = matrix[0].size();
+    vector<int> spiralOrder(const vector<vector<int>>& matrix) {
+        vector<int> result;
 
-    int top = 0, left = 0;
-    int bottom = ROWS - 1, right = COLUMNS - 1;
+        const int ROWS = matrix.size();
+        const int COLUMNS = matrix[0].size();
 
-    while (left <= right && top <= bottom) {
-        // Traverse from left to right
-        for (int index = left; index <= right; index++) 
-            result.push_back(matrix[top][index]);
-        top++;
-        
-        // Traverse downwards
-        for (int index = top; index <= bottom; index++) 
-            result.push_back(matrix[index][right]);
-        right--;
+        int top = 0, left = 0;
+        int bottom = ROWS - 1, right = COLUMNS - 1;
 
-        // Check again in case boundaries crossed before reverse traveral
-        if (top <= bottom) {
+        while (left <= right && top <= bottom) {
+            // Traverse from left to right
+            for (int index = left; index <= right; index++)
+                result.push_back(matrix[top][index]);
+            top++;
+
+            // Traverse downwards
+            for (int index = top; index <= bottom; index++)
+                result.push_back(matrix[index][right]);
+            right--;
+
+            // Check again in case boundaries crossed before reverse traveral
+
             // Traverse from right to left
-            for (int index = right; index >= left; index--) 
+            for (int index = right; index >= left && top <= bottom; index--)
                 result.push_back(matrix[bottom][index]);
             bottom--;
-        }
 
-        // Check again in case boundaries crossed before reverse traveral
-        if (left <= right) {
+            // Check again in case boundaries crossed before reverse traveral
+
             // Traverse upwards
-            for (int index = bottom; index >= top; index--) 
+            for (int index = bottom; index >= top && left <= right; index--)
                 result.push_back(matrix[index][left]);
             left++;
         }
-    }
 
-    return result;
-    
+        return result;
     }
-
 };
