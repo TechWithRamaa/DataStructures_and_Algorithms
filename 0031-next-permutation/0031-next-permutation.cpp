53b1ed27060f8d80aa258e066a43fea2cfc4d289
Ramaa <rkramadhevi@gmail.com>
2024-11-25 22:21:44 +0530

    Time: 0 ms (100%), Space: 15 MB (5.67%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,17 +4,20 @@ public:
         int n = nums.size();
         int i = n - 2;
 
+        // Find the first decreasing element
         while (i >= 0 && nums[i] >= nums[i + 1])
             i--;
 
         if (i >= 0) {
+            // Find the next larger element
             int j = n - 1;
             while (nums[j] <= nums[i])
                 j--;
 
+            // Swap the elements
             swap(nums[i], nums[j]);
         }
-
+        // Reverse the elements from i+1 to the end
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
