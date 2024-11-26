68e084609a40459eab50fd7edbf015f144ca7710
Ramaa <rkramadhevi@gmail.com>
2024-11-27 00:14:24 +0530

    Time: 87 ms (53.25%), Space: 44.5 MB (6.14%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,25 +1,3 @@
-// class MyCalendarTwo {
-// public:
-//     bool book(int start, int end) {
-//         for (const auto& [s, e] : overlaps)
-//             if (max(start, s) < min(end, e))
-//                 return false;
-
-//         for (const auto& [s, e] : ranges) {
-//             const int ss = max(start, s);
-//             const int ee = min(end, e);
-//             if (ss < ee)
-//                 overlaps.emplace_back(ss, ee);
-//         }
-
-//         ranges.emplace_back(start, end);
-//         return true;
-//     }
-
-// private:
-//     vector<pair<int, int>> ranges;
-//     vector<pair<int, int>> overlaps;
-// };
 
 class MyCalendarTwo {
 public:
@@ -31,15 +9,15 @@ public:
 
         for (const auto& [_, count] : timeline) {
             activeEvents += count;
-            
+
             if (activeEvents > 2) {
-            
+
                 if (--timeline[start] == 0)
                     timeline.erase(start);
-            
+
                 if (++timeline[end] == 0)
                     timeline.erase(end);
-            
+
                 return false;
             }
         }
