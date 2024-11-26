591e8a3cc2eef82e1da409efe73be064d1775d1c
Ramaa <rkramadhevi@gmail.com>
2024-11-26 23:55:44 +0530

    Time: 28 ms (75%), Space: 93.5 MB (9.01%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        // Step 1: Initialize a difference array
-        vector<vector<int>> diff(n + 1, vector<int>(n + 1, 0));
+    vector<vector<int>> rangeAddQueries(const int N, const vector<vector<int>>& queries) {
+        // Initialize a difference matrix
+        vector<vector<int>> diff(N + 1, vector<int>(N + 1, 0));
 
-        // Step 2: Process each query
+        // Process each query and follow  2D difference array technique
         for (const auto& q : queries) {
             int row1 = q[0], col1 = q[1], row2 = q[2], col2 = q[3];
-            diff[row1][col1] += 1;
-            if (col2 + 1 < n)
-                diff[row1][col2 + 1] -= 1;
-            if (row2 + 1 < n)
-                diff[row2 + 1][col1] -= 1;
-            if (row2 + 1 < n && col2 + 1 < n)
-                diff[row2 + 1][col2 + 1] += 1;
+            
+            diff[row1][col1] += 1;  
+            
+            if (row2 + 1 < N)  diff[row2 + 1][col1] -= 1;
+            
+            if (col2 + 1 < N)  diff[row1][col2 + 1] -= 1;
+            
+            if (row2 + 1 < N && col2 + 1 < N)  diff[row2 + 1][col2 + 1] += 1;
         }
 
-        // Step 3: Build the resultant matrix using prefix sums
-        vector<vector<int>> result(n, vector<int>(n, 0));
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i > 0)
-                    diff[i][j] += diff[i - 1][j];
-                if (j > 0)
-                    diff[i][j] += diff[i][j - 1];
-                if (i > 0 && j > 0)
-                    diff[i][j] -= diff[i - 1][j - 1];
+        // Build the resultant matrix using (Row-Wise Prefix sum and Column-wise Prefix sum)
+        vector<vector<int>> result(N, vector<int>(N, 0));
+
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (i > 0)  diff[i][j] += diff[i - 1][j];
+
+                if (j > 0)  diff[i][j] += diff[i][j - 1];
+
+                if (i > 0 && j > 0) diff[i][j] -= diff[i - 1][j - 1];
+
                 result[i][j] = diff[i][j];
             }
         }
