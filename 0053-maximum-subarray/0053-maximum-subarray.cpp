028b44274c1a8b1bfc248edfe04284e80c47dc4a
Ramaa <rkramadhevi@gmail.com>
2024-12-03 21:37:22 +0530

    Time: 0 ms (100%), Space: 71.8 MB (8.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int runningSum = nums[0];
-        int largestSum = nums[0];
-        // Kadane's algorithm
-        for(int i = 1; i < nums.size(); i++) {
-            if(runningSum >= 0) 
-                runningSum += nums[i];
-            else 
-                runningSum = nums[i];
+        int largestSum = nums[0], windowSum = nums[0];
+        int winStart = 0;
 
-            largestSum = max(largestSum, runningSum);
-        } 
+        for(int winEnd = 1; winEnd < nums.size(); winEnd++) {
+            if(windowSum >= 0) 
+                windowSum += nums[winEnd];
+            else {
+                winStart = winEnd;
+                windowSum = nums[winEnd];
+            } 
+
+            largestSum = max(largestSum, windowSum);
+        }
 
         return largestSum;
     }
