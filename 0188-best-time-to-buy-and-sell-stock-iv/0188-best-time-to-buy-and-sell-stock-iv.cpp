a3e733d2048676a550217af2528db7eb5943c928
Ramaa <rkramadhevi@gmail.com>
2024-12-03 10:11:08 +0530

    Time: 2 ms (85.66%), Space: 14.5 MB (70.85%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,11 +2,36 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int N = prices.size();
-
-        return maxProfitTopDown(k, prices);
+        return maxProfitBottomUp(k, prices);
+        //return maxProfitTopDown(k, prices);
     }
 
 private:
+    int maxProfitBottomUp(int k, vector<int>& prices) {
+        int N = prices.size();
+        vector<vector<int>> ahead(2, vector<int>(k + 1, 0));
+        vector<vector<int>> cur(2, vector<int>(k + 1, 0));
+
+        for (int ind = N - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= k; cap++) {
+                    if (buy == 0) { // We can buy the stock
+                        cur[buy][cap] = max(0 + ahead[0][cap],
+                                       -prices[ind] + ahead[1][cap]);
+                    }
+
+                    if (buy == 1) { // We can sell the stock
+                        cur[buy][cap] = max(0 + ahead[1][cap],
+                                       prices[ind] + ahead[0][cap - 1]);
+                }
+            }
+        }
+        // Update the 'ahead' array with the current values
+        ahead = cur;
+    }
+
+    return ahead[0][k];
+    }
     int maxProfitTopDown(int k, vector<int>& prices) {
          int N = prices.size();
         vector<vector<vector<int>>> dp(N, vector<vector<int>>(2, vector<int>(k + 1, -1)));
