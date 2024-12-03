f123ca0c674fb4e5807eaa2ea60a5851f320305e
Ramaa <rkramadhevi@gmail.com>
2024-12-03 10:07:19 +0530

    Time: 17 ms (25.36%), Space: 18.3 MB (16.7%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
+        int N = prices.size();
+
+        return maxProfitTopDown(k, prices);
+    }
+
+private:
+    int maxProfitTopDown(int k, vector<int>& prices) {
          int N = prices.size();
         vector<vector<vector<int>>> dp(N, vector<vector<int>>(2, vector<int>(k + 1, -1)));
 
-        return maximumProfit(prices, N, 0, 0, k, dp);
+        return maxProfitTopDown(prices, N, 0, 0, k, dp);
     }
-
-public:
-    int maximumProfit(const vector<int>& arr, const int N, int ind, int buy, int cap, vector<vector<vector<int>>>& dp) {
+    int maxProfitTopDown(const vector<int>& arr, const int N, int ind, int buy, int cap, vector<vector<vector<int>>>& dp) {
         if (ind == N || cap == 0)
             return 0;
        
@@ -18,12 +23,12 @@ public:
         int profit;
 
         if (buy == 0)
-            profit = max(0 + maximumProfit(arr, N, ind + 1, 0, cap, dp),
-                         -arr[ind] + maximumProfit(arr, N, ind + 1, 1, cap, dp));
+            profit = max(0 + maxProfitTopDown(arr, N, ind + 1, 0, cap, dp),
+                         -arr[ind] + maxProfitTopDown(arr, N, ind + 1, 1, cap, dp));
 
         if (buy == 1)
-            profit = max(0 + maximumProfit(arr, N, ind + 1, 1, cap, dp),
-                        arr[ind] + maximumProfit(arr, N, ind + 1, 0, cap - 1, dp));
+            profit = max(0 + maxProfitTopDown(arr, N, ind + 1, 1, cap, dp),
+                        arr[ind] + maxProfitTopDown(arr, N, ind + 1, 0, cap - 1, dp));
         
 
         return dp[ind][buy][cap] = profit;
