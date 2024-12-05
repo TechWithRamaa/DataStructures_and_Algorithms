1cebc0470d3b1e195b38a8a5b6d5f54bd412f414
Ramaa <rkramadhevi@gmail.com>
2024-12-05 23:32:04 +0530

    Time: 5 ms (64.76%), Space: 30.4 MB (6.32%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -5,6 +5,33 @@ public:
         vector<vector<int>> dp(M, vector<int>(N, -1));
         int totalSquares = 0;
 
+        for (int i = 0; i < M; i++) dp[i][0] = matrix[i][0];
+        for (int j = 0; j < N; j++) dp[0][j] = matrix[0][j];
+
+        for (int i = 1; i < M; i++) {
+            for (int j = 1; j < N; j++) {
+                if (matrix[i][j] == 1)  
+                    dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+                else 
+                    dp[i][j] = 0;
+            }
+        }
+        
+
+          for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+               totalSquares += dp[i][j];
+            }
+        }
+        
+        return totalSquares;
+    }
+
+    int countSquaresTopDown(vector<vector<int>>& matrix) {
+        const int M = matrix.size(), N = matrix[0].size();
+        vector<vector<int>> dp(M, vector<int>(N, -1));
+        int totalSquares = 0;
+
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) 
                 totalSquares += countSquares(matrix, i, j, dp);   
