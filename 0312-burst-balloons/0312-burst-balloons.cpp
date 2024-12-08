64b6d3c47a719728de8ee0097b39e326ada27635
Ramaa <rkramadhevi@gmail.com>
2024-12-08 23:35:52 +0530

    Time: 203 ms (42.11%), Space: 13.8 MB (7.89%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
-    int maxCoins(vector<int>& nums) {
+     int maxCoins(vector<int>& nums) {
+        return maxCoinsTopDown(nums);        
+    }
+    
+
+private:
+    int maxCoinsBottomUp(vector<int>& nums) {
         int n = nums.size();
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
 
-       vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
 
-        return burst(nums, 0, n + 1, dp); 
+        return burst(nums, 0, n + 1, dp);
     }
 
-private:
-    int burst(const vector<int>& nums, int left, int right, vector<vector<int>>& dp) {
-        if (left + 1 == right) return 0;
+    int maxCoinsTopDown(vector<int>& nums) {
+        int n = nums.size();
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
 
-        if (dp[left][right] != -1) return dp[left][right];
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
+
+        return burst(nums, 0, n + 1, dp);
+    }
+
+    int burst(const vector<int>& nums, int left, int right,
+              vector<vector<int>>& dp) {
+        if (left + 1 == right)
+            return 0;
+
+        if (dp[left][right] != -1)
+            return dp[left][right];
 
         int maxCoins = 0;
 
         for (int k = left + 1; k < right; k++) {
-            int coins = nums[left] * nums[k] * nums[right]
-                      + burst(nums, left, k, dp) 
-                      + burst(nums, k, right, dp); 
+            int coins = nums[left] * nums[k] * nums[right] +
+                        burst(nums, left, k, dp) + burst(nums, k, right, dp);
 
             maxCoins = max(maxCoins, coins);
         }
