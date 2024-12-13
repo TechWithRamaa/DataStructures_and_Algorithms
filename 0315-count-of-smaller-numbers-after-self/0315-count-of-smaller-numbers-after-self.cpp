1d53e5036a35251c6f26fc39b904834f9ec555a5
Ramaa <rkramadhevi@gmail.com>
2024-12-13 23:55:08 +0530

    Time: 103 ms (87.06%), Space: 96.4 MB (79.99%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
-        vector<int> counts(n, 0); // To store the result counts
-        vector<pair<int, int>> nums_with_indices(
-            n); // To store the numbers with original indices
+        vector<int> counts(n, 0); 
 
-        // Store the numbers with their original indices
-        for (int i = 0; i < n; ++i) {
+        vector<pair<int, int>> nums_with_indices(n); 
+        for (int i = 0; i < n; ++i) 
             nums_with_indices[i] = {nums[i], i};
-        }
-
-        // Call merge sort to count the smaller elements during merging
+        
         mergeSort(nums_with_indices, counts);
 
         return counts;
     }
 
 private:
-    // Merge Sort Helper function that counts the smaller elements during
-    // merging
     void mergeSort(vector<pair<int, int>>& nums, vector<int>& counts) {
         vector<pair<int, int>> temp(nums.size());
         mergeSortHelper(nums, temp, 0, nums.size() - 1, counts);
@@ -28,10 +22,9 @@ private:
     void mergeSortHelper(vector<pair<int, int>>& nums,
                          vector<pair<int, int>>& temp, int left, int right,
                          vector<int>& counts) {
-        if (left >= right) {
+        if (left >= right) 
             return;
-        }
-
+        
         int mid = left + (right - left) / 2;
         mergeSortHelper(nums, temp, left, mid, counts);
         mergeSortHelper(nums, temp, mid + 1, right, counts);
@@ -43,7 +36,6 @@ private:
         int i = left, j = mid + 1, k = left;
         int smaller_count = 0;
 
-        // Merge the two halves while counting smaller elements for each number
         while (i <= mid && j <= right) {
             if (nums[i].first <= nums[j].first) {
                 temp[k] = nums[i];
@@ -57,7 +49,6 @@ private:
             k++;
         }
 
-        // Copy remaining elements from the left half
         while (i <= mid) {
             temp[k] = nums[i];
             counts[nums[i].second] += smaller_count;
@@ -65,16 +56,14 @@ private:
             k++;
         }
 
-        // Copy remaining elements from the right half
         while (j <= right) {
             temp[k] = nums[j];
             j++;
             k++;
         }
 
-        // Copy the merged array back to the original nums array
-        for (int i = left; i <= right; ++i) {
+        for (int i = left; i <= right; ++i) 
             nums[i] = temp[i];
-        }
+        
     }
 };
