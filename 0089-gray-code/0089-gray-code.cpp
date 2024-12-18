07c41f27f360b1ba5acd4f3c13f14114150962d6
Ramaa <rkramadhevi@gmail.com>
2024-12-18 22:59:57 +0530

    Time: 2 ms (38.02%), Space: 14.6 MB (32.95%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
    vector<int> grayCode(int n) {
         vector<int> result;  
-        int size = 1 << n; // Total numbers in Gray Code sequence = 2^n
+        int size = 1 << n; 
         
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < size; i++) 
             result.push_back(i ^ (i >> 1)); // Compute Gray Code using XOR
-        }
         
         return result;
     }
