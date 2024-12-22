6298b163bcd6e4c1e00dfe2f813952778c4429b5
Ramaa <rkramadhevi@gmail.com>
2024-12-22 23:25:28 +0530

    Time: 18 ms (89.89%), Space: 23.6 MB (13.45%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,21 +11,16 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-          // Iterate until we find the LCA
         while (root) {
-            // If both p and q are smaller, move to the left
-            if (p->val < root->val && q->val < root->val) {
+            if (p->val < root->val && q->val < root->val) 
                 root = root->left;
-            }
-            // If both p and q are larger, move to the right
-            else if (p->val > root->val && q->val > root->val) {
+            
+            else if (p->val > root->val && q->val > root->val) 
                 root = root->right;
-            }
-            // Otherwise, we found the split point, so root is the LCA
-            else {
+            else 
                 return root;
-            }
+            
         }
-        return nullptr;  // In case the tree is empty, but this won't happen for valid inputs
+        return nullptr;  
     }
 };
