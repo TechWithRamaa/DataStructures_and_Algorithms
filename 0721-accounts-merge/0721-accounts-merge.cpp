b98561aae44b4f37ed84c7b48674ed355a1dfeb3
Ramaa <rkramadhevi@gmail.com>
2025-01-01 22:51:55 +0530

    Time: 40 ms (57.38%), Space: 42.9 MB (39.96%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,68 @@
+class UnionFind {
+public:
+    vector<int> parent;
+
+    UnionFind(int n) {
+        parent.resize(n);
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]); // Path compression
+
+        return parent[x];
+    }
+
+    void unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY)
+            parent[rootY] = rootX;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        unordered_map<string, int> emailToIndex;
+        unordered_map<string, string> emailToName;
+        int n = accounts.size();
+
+        UnionFind uf(n);
+
+        // Step 1: Map emails to accounts and perform unions
+        for (int i = 0; i < n; i++) {
+            string name = accounts[i][0];
+            for (int j = 1; j < accounts[i].size(); j++) {
+                string email = accounts[i][j];
+                emailToName[email] = name;
+                if (emailToIndex.find(email) == emailToIndex.end())
+                    emailToIndex[email] = i;
+                else
+                    uf.unite(i, emailToIndex[email]);
+            }
+        }
+
+        // Step 2: Group emails by their root account
+        unordered_map<int, vector<string>> groupedEmails;
+        for (const auto& [email, _] : emailToIndex) {
+            int root = uf.find(emailToIndex[email]);
+            groupedEmails[root].push_back(email);
+        }
+
+        // Step 3: Construct the result
+        vector<vector<string>> result;
+        for (auto& [root, emails] : groupedEmails) {
+            sort(emails.begin(), emails.end());
+            vector<string> account;
+            account.push_back(emailToName[emails[0]]); // Add the name
+            account.insert(account.end(), emails.begin(),
+                           emails.end()); // Add sorted emails
+            result.push_back(account);
+        }
+
+        return result;
+    }
+};
