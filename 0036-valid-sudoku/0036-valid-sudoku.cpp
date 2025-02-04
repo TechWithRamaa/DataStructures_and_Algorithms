d0a7b8816185359873b5e81be3a596b0ca9094f7
Ramaa <rkramadhevi@gmail.com>
2025-02-04 23:39:27 +0530

    Time: 13 ms (9.53%), Space: 25.7 MB (17.58%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        const int ROWS = board.size();
+        const int COLS = board[0].size();
+
+        unordered_set<string> alreadyPresent;
+
+        for(int i = 0; i < ROWS; i++) {
+            for(int j = 0; j < COLS; j++) {
+                if(board[i][j] == '.') 
+                  continue;
+
+                char c = board[i][j];
+                string rowKey = string(1, c) + "@row" + to_string(i);
+                string colKey = string(1, c) + "@col" + to_string(j);
+                string boxKey = string(1, c) + "@box" + to_string(i/3) + to_string(j/3);
+
+                if(alreadyPresent.count(rowKey) || alreadyPresent.count(colKey) || alreadyPresent.count(boxKey)) {
+                    return false;
+                }
+
+                alreadyPresent.insert(rowKey);
+                alreadyPresent.insert(colKey);
+                alreadyPresent.insert(boxKey);
+            }
+
+        }
+
+        return true;
+    }
+};
