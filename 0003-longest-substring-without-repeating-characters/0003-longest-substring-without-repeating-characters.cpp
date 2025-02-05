911b92af775905246cb6f9e764fd0212ea11e199
Ramaa <rkramadhevi@gmail.com>
2025-02-05 12:39:04 +0530

    Time: 8 ms (60.41%), Space: 11.8 MB (68.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int lengthOfLongestSubstring(string s) {
         vector<int> counter(256, 0) ; // stores the last index of every character encountered
 
@@ -18,27 +19,27 @@ public:
             maxLength = max(maxLength, end - start + 1);
         }
         return maxLength;
-    }
+    }*/
 
     // HashMap approach aids in quicker shrinking of windows
-    /* int lengthOfLongestSubstring(string s) {
-    //     unordered_map<char, int> lastIndicesMap; // stores the last index of every character encountered
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> lastIndicesMap; // stores the last index of every character encountered
 
-    //     int start = 0;
-    //     int maxLength = 0;
+        int start = 0;
+        int maxLength = 0;
 
-    //     for(int end = 0; end < s.length(); end++) {
-    //         char currentChar = s[end];
+        for(int end = 0; end < s.length(); end++) {
+            char currentChar = s[end];
 
-    //         if(lastIndicesMap.find(currentChar) != lastIndicesMap.end() && lastIndicesMap[currentChar] >= start) {
-    //             start = lastIndicesMap[currentChar] + 1;
-    //         } 
+            if(lastIndicesMap.find(currentChar) != lastIndicesMap.end() && lastIndicesMap[currentChar] >= start) {
+                start = lastIndicesMap[currentChar] + 1;
+            } 
 
-    //         lastIndicesMap[currentChar] = end;
-    //         maxLength = max(maxLength, end - start + 1);
-    //     }
-    //     return maxLength;
-     }*/
+            lastIndicesMap[currentChar] = end;
+            maxLength = max(maxLength, end - start + 1);
+        }
+        return maxLength;
+     }
 
 
 };
