8383c90212a55f880d454553959e9f5f92bfd850
Ramaa <rkramadhevi@gmail.com>
2025-02-05 12:37:34 +0530

    Time: 6 ms (70.28%), Space: 11.6 MB (81.4%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> lastIndicesMap;
+        vector<int> counter(256, 0) ; // stores the last index of every character encountered
 
         int start = 0;
         int maxLength = 0;
@@ -9,13 +9,36 @@ public:
         for(int end = 0; end < s.length(); end++) {
             char currentChar = s[end];
 
-            if(lastIndicesMap.find(currentChar) != lastIndicesMap.end() && lastIndicesMap[currentChar] >= start) {
-                start = lastIndicesMap[currentChar] + 1;
+            while(counter[currentChar] >= 1) {
+                counter[s[start]] -= 1;
+                start += 1;
             } 
 
-            lastIndicesMap[currentChar] = end;
+            counter[currentChar] += 1;
             maxLength = max(maxLength, end - start + 1);
         }
         return maxLength;
     }
+
+    // HashMap approach aids in quicker shrinking of windows
+    /* int lengthOfLongestSubstring(string s) {
+    //     unordered_map<char, int> lastIndicesMap; // stores the last index of every character encountered
+
+    //     int start = 0;
+    //     int maxLength = 0;
+
+    //     for(int end = 0; end < s.length(); end++) {
+    //         char currentChar = s[end];
+
+    //         if(lastIndicesMap.find(currentChar) != lastIndicesMap.end() && lastIndicesMap[currentChar] >= start) {
+    //             start = lastIndicesMap[currentChar] + 1;
+    //         } 
+
+    //         lastIndicesMap[currentChar] = end;
+    //         maxLength = max(maxLength, end - start + 1);
+    //     }
+    //     return maxLength;
+     }*/
+
+
 };
