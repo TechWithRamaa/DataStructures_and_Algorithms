c905822264dbc2e6d974f1e8ab9df86a85e576de
Ramaa <rkramadhevi@gmail.com>
2025-02-05 09:10:25 +0530

    Time: 55 ms (43.26%), Space: 29.1 MB (49.95%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        std ::vector<std ::vector<int>> triplets;
+        vector<vector<int>> result;
 
-        std ::sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size() - 2; i++) {
-            int left = i + 1, right = nums.size() - 1;
+        int leftPointer = 0;
+        int rightPointer = nums.size() - 1;
+        
+        // -4, -1, -1, 0, 1, 2
 
-            if (i > 0 && nums[i] == nums[i - 1])
+        for(int i = 0; i < nums.size(); i++) {
+            if(i > 0 && nums[i-1] == nums[i])
                 continue;
 
+            int left = i + 1;
+            int right = nums.size() - 1;
+
             while (left < right) {
-                int tripletSum = nums[i] + nums[left] + nums[right];
+                int sum  = nums[i] + nums[left] +  nums[right];
 
-                if (tripletSum == 0) {
-                    triplets.push_back({nums[i], nums[left], nums[right]});
+                if(sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
 
-                    while (left < right && nums[left] == nums[left + 1])
-                        left++;
-                    while (left < right && nums[right] == nums[right - 1])
-                        right--;
+                    while(left < right && nums[left] == nums[left + 1])
+                       left++;
+
+                    while(left < right && nums[right] == nums[right - 1])
+                       right--;
 
                     left++;
                     right--;
-                } else if (tripletSum < 0)
+                } else if (sum < 0) 
                     left++;
-                else
+                 else 
                     right--;
             }
         }
-        return triplets;
+
+        return result;
     }
 };
