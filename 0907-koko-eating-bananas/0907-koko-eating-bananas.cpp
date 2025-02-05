ab6683ac054d9438da35a0746bfe9c5337bd0bbe
Ramaa <rkramadhevi@gmail.com>
2025-02-05 18:07:56 +0530

    Time: 3 ms (95.97%), Space: 22.9 MB (74.69%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int minEatingSpeed(const vector<int>& piles, const int H) {
-        int left = 1, right = *max_element(piles.begin(), piles.end());
-        int k = right;
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+    
 
-        while (left <= right) {
+        while(left <= right) {
             int mid = left + (right - left) / 2;
-
-            if (canFinish(piles, H, mid)) {
-                k = mid; // Update result and try for smaller k
+            if(canKokoFinish(piles, H, mid)) {
                 right = mid - 1;
-            } else
-                left = mid + 1; // Try for larger k
+            } else {
+                left = mid + 1;
+            }
         }
 
-        return k;
+        return left;
     }
-
 private:
-    bool canFinish(const vector<int>& piles, const int H, const int k) {
-        long long hours = 0;
+    bool canKokoFinish(const vector<int>& piles, const int H, int k) {
+        long long h = 0;
 
-        for (int pile : piles)
-            hours += ((pile + k - 1) / k);
-              
-        return hours <= H;
+        for(int pile : piles) {
+            h += (pile + k - 1)/k;
+        }
+
+        return h <= H;
     }
+
+
 };
