cefce11963d2790931946b9dcea505cf71f31450
Ramaa <rkramadhevi@gmail.com>
2025-02-16 14:27:35 +0530

    Time: 0 ms (100%), Space: 12.3 MB (99.59%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // BruteForce ~ O (n)
+    int findPeakElementBruteForce(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if((i == 0 || nums[i] > nums[i-1]) && ( i == nums.size() - 1 || nums[i] > nums[i + 1])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    int findPeakElement(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if(nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+};
