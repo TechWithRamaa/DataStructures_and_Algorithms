92094b46601291f87f0a751b893a235c42af8f66
Ramaa <rkramadhevi@gmail.com>
2025-02-16 19:51:36 +0530

    Time: 3 ms (89.85%), Space: 23.9 MB (58.6%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-
+        
         vector<vector<int>> merged;
-
-        for (const auto& interval : intervals) {
-            // If merged list is empty or current interval does not overlap, add it
-            if (merged.empty() || merged.back()[1] < interval[0])
-                merged.push_back(interval);
-            else
-                // Overlapping intervals; merge them by updating the end time
-                merged.back()[1] = max(merged.back()[1], interval[1]);
+        
+        for(vector<int>& current : intervals) {
+            if(!merged.empty() && current[0] <= merged.back()[1]) {
+                merged.back()[1] = max( current[1], merged.back()[1]);
+            } else {
+                merged.push_back(current);
+            }
         }
-
+        
         return merged;
     }
 };
