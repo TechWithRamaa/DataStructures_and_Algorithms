5a15675231fb4473a45ce8aa059d6ed2b608b2a9
Ramaa <rkramadhevi@gmail.com>
2025-02-16 21:58:39 +0530

    Time: 55 ms (51.34%), Space: 18.8 MB (36.98%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    // approach 1 - BruteForce ~ O (n ^ 2)
+    // approach 2 - Binary Search on Each Row ~ O ( n * log m) 
+    // approach 3 - Binary Search on Specifc Row ~ O ( n + log m) 
+    // approach 4 - Saddle Back Search ~ O ( m + n )
     bool searchMatrix(const vector<vector<int>>& matrix, const int target) {
         const int NO_OF_ROWS = matrix.size();
         const int NO_OF_COLS = matrix[0].size();
@@ -8,9 +12,12 @@ public:
         int x = 0, y = NO_OF_COLS - 1;
 
         while(x < NO_OF_ROWS && y >= 0) {
-            if(target == matrix[x][y]) return true;
-            else if (target > matrix[x][y]) x++;
-            else if (target < matrix[x][y]) y--;
+            if(target == matrix[x][y])
+                return true;
+            else if (target > matrix[x][y]) 
+                x++; // search in increasing side
+            else if (target < matrix[x][y]) 
+                y--; // search in decreasing side
         }
         
         return false;
