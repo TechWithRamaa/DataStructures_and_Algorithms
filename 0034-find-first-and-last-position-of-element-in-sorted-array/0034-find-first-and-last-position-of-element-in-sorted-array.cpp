c97d182b6f7baf94ee14ce18b344ea214253cf22
Ramaa <rkramadhevi@gmail.com>
2025-02-16 15:12:08 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.3%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+    // BruteForce ~ O(N)
+    vector<int> searchRangeBruteForce(vector<int>& nums, int target) {
         int startingPosition = -1, lastPosition = -1;
         
         for(int i = 0; i < nums.size(); i++) {
@@ -12,5 +13,51 @@ public:
         }
         
         return {startingPosition, lastPosition};
+    }
+
+    // BinarySearch ~ O(log N) + O(log N)
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {findFirstPosition(nums, target), findLastPosition(nums, target)};
+    }
+
+private:
+    int findFirstPosition(vector<int>& nums, int target) {
+        int firstPosition = -1;
+        
+        int left = 0, right = nums.size() - 1;
+        while(left <= right) {
+            int mid = left + (right - left ) / 2;
+
+            if(nums[mid] == target) {
+                firstPosition = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return firstPosition;
+    }
+
+    int findLastPosition(vector<int>& nums, int target) {
+        int lastPosition = -1;
+        
+        int left = 0, right = nums.size() - 1;
+        while(left <= right) {
+            int mid = left + (right - left ) / 2;
+
+            if(nums[mid] == target) {
+                lastPosition = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return lastPosition;
     }
 };
