c30aba694422322057c94edaeb76cc8e03492128
Ramaa <rkramadhevi@gmail.com>
2025-02-16 14:26:34 +0530

    Time: 3 ms (53.01%), Space: 19.9 MB (11.08%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,29 +1,57 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> frequencyMap;
-
-        for(auto &num : nums) {
-            frequencyMap[num]++;
+    // Frequency Map + Min Heap ~ O(n) + O(n log k) + O(k)
+    vector<int> topKFrequentMinHeap(vector<int>& nums, int k) {
+        unordered_map<int, int> freqMap;
+        
+        for(auto num : nums) {
+            freqMap[num]++;
         }
-
-        auto comp = [](const pair<int, int>& a, const pair<int, int>& b) {
-            return a.first > b.first;
-        };
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> minHeap(comp);
-        for(auto &entry:frequencyMap) {
-            minHeap.push({entry.second, entry.first});
-            if(minHeap.size() > k) {
-                minHeap.pop();
+        
+        // {frequency, num}
+        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<>> pq;
+        
+        for(auto [num, freq] : freqMap) {
+            pq.push({freq, num});
+            if(pq.size() > k) {
+                pq.pop();
             }
         }
-
-        vector<int> topKElements;
-        while(!minHeap.empty()) {
-            topKElements.push_back(minHeap.top().second);
-            minHeap.pop();
+        
+        vector<int> result;
+        while(!pq.empty()) {
+            auto [freq, num] = pq.top();
+            pq.pop();
+            result.push_back(num);
         }
-
-        return topKElements;
+        
+        return result;
+    }
+    
+    // Frequence Map + Bucket Sort ~ O(n) + O(n) + O(k)
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freqMap;
+        
+        for(auto num : nums) {
+            freqMap[num]++;
+        }
+        
+        vector<vector<int>> bucket(nums.size() + 1);
+        
+        for(auto [num, freq] : freqMap) {
+            bucket[freq].push_back(num);
+        }
+        
+        vector<int> result;
+        for(int i = nums.size(); i >= 0; i--) {
+            for(int num : bucket[i]) {
+                result.push_back(num);
+            }
+            
+             if(result.size() == k) 
+                break;
+        }
+        
+        return result;
     }
 };
