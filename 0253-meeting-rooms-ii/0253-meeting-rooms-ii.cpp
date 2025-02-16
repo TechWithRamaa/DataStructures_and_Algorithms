000a0fab43dfa8d275e33b80aedf7681cd199fab
Ramaa <rkramadhevi@gmail.com>
2025-02-16 21:05:08 +0530

    Time: 1 ms (71.05%), Space: 16.6 MB (41.89%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int minMeetingRooms(vector<vector<int>>& intervals) {
+    // Sort + MinHeap => TC ~ O(n log n) + O (n log n)
+    //                => SC ~ O(n) + O(n)
+    int minMeetingRoomsHeap(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
  
         priority_queue<int, vector<int>, greater<>> minHeap;
@@ -13,5 +15,28 @@ public:
         }
         
         return minHeap.size();
+    }
+
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        vector<pair<int, int>> events;
+
+        for(vector<int>& interval : intervals) {
+            events.push_back({interval[0], 1});
+            events.push_back({interval[1], -1});
+        }
+
+        sort(events.begin(), events.end(), [] (const pair<int, int>& a, const pair<int, int>& b) {
+            if(a.first == b.first)
+                return a.second < b.second;
+            return a.first < b.first;
+        });
+
+        int meetingRooms = 0, activeMeetings = 0;
+        for(pair<int, int>& event : events) {
+            activeMeetings += event.second;
+            meetingRooms = max(meetingRooms, activeMeetings);
+        }
+
+        return meetingRooms;
     }
 };
