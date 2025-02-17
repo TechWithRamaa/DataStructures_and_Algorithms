a3bf963284a67ba5f1944b466241e5c706a7e887
Ramaa <rkramadhevi@gmail.com>
2025-02-18 00:42:07 +0530

    Time: 642 ms (5.05%), Space: 295.4 MB (31.11%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));  // Use n instead of n+1
+        vector<vector<int>> dp(n, vector<int>(n, -1));
         return lis(0, -1, nums, dp);
     }
 
 private:
-    int lis(int index, int prev_index, vector<int>& nums, vector<vector<int>>& dp) {
-        if (index == nums.size()) return 0;  // Base case
+    int lis(int index, int prevIndex, vector<int>& nums, vector<vector<int>>& dp) {
+        if (index == nums.size()) return 0;  
 
-        // Adjust prev_index + 1 only when prev_index is valid
-        if (prev_index != -1 && dp[index][prev_index] != -1) 
-            return dp[index][prev_index];
+        if (prevIndex != -1 && dp[index][prevIndex] != -1) 
+            return dp[index][prevIndex];
 
         int take = 0;
-        if (prev_index == -1 || nums[index] > nums[prev_index]) {
+        if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
             take = 1 + lis(index + 1, index, nums, dp);
         }
-        int notTake = lis(index + 1, prev_index, nums, dp);
 
-        // Store the result only if prev_index is valid
-        if (prev_index != -1) 
-            dp[index][prev_index] = max(take, notTake);
+        int notTake = lis(index + 1, prevIndex, nums, dp);
+
+        if (prevIndex != -1) 
+            dp[index][prevIndex] = max(take, notTake);
 
         return max(take, notTake);
     }
