667edfd90cdf27b7c9f732c434bc86e4c74b744f
Ramaa <rkramadhevi@gmail.com>
2025-02-18 00:57:17 +0530

    Time: 0 ms (100%), Space: 14 MB (94.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
+    // Binary Search + DP ~ O ( n log n )
     int lengthOfLIS(vector<int>& nums) {
+        vector<int> sub; 
+
+        for (int num : nums) {
+            auto it = lower_bound(sub.begin(), sub.end(), num); 
+
+            if (it == sub.end()) {
+                sub.push_back(num); 
+            } else {
+                *it = num;
+            }
+        }
+        return sub.size();
+    }
+
+    // Recursion ~ O ( 2 ^ n )
+    int lengthOfLISDP(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
         return lis(0, -1, nums, dp);
     }
 
 private:
-    int lis(int index, int prevIndex, vector<int>& nums, vector<vector<int>>& dp) {
-        if (index == nums.size()) return 0;  
+    int lis(int index, int prevIndex, vector<int>& nums,
+            vector<vector<int>>& dp) {
+        if (index == nums.size())
+            return 0;
 
-        if (prevIndex != -1 && dp[index][prevIndex] != -1) 
+        if (prevIndex != -1 && dp[index][prevIndex] != -1)
             return dp[index][prevIndex];
 
         int take = 0;
