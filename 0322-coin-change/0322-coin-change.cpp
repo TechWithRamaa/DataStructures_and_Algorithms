e7139b836bf5cc5514300ffa5c2c93e7d9155730
Ramaa <rkramadhevi@gmail.com>
2025-02-18 00:28:45 +0530

    Time: 45 ms (36.82%), Space: 49.1 MB (31.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -10,7 +10,14 @@ public:
 private:
     int coinChange(int index, vector<int>& coins, int amount, vector<vector<int>>& dp) {
         if (amount == 0) return 0;  
-        if (index < 0) return INT_MAX - 1; 
+        if (index == 0) {
+            if(amount % coins[index] == 0) {
+                return amount / coins[index];
+            } else {
+                return INT_MAX - 1; 
+            } 
+        } 
+            
 
         if (dp[index][amount] != -1) return dp[index][amount]; 
 
