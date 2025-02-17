1b88b9fc8c0a8bddc9c492ef775630a26501d8d0
Ramaa <rkramadhevi@gmail.com>
2025-02-17 23:31:13 +0530

    Time: 0 ms (100%), Space: 9.4 MB (9.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,35 @@
-// class Solution {
-// public:
-//     int uniquePaths(int m, int n) {
-//         if (m == 1 || n == 1) return 1;  // Base case
-//             return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);  // Recursion
-//     }
-// };
+/* Recursion ~ O ( 2 ^ n )
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) return 1; 
+
+        int noOfLeftPaths = uniquePaths(m, n - 1);
+        int noOfTopPaths = uniquePaths(m-1, n);
+
+        return noOfLeftPaths + noOfTopPaths;
+    }
+};
+*/
 
 class Solution {
 public:
      int solve(int m, int n, vector<vector<int>>& dp) {
-        if (m == 1 || n == 1) return 1;  // Base case
-        if (dp[m][n] != -1) return dp[m][n];  // If already computed, return it
+        if (m == 0 || n == 0)
+            return 1;
 
-        return dp[m][n] = solve(m - 1, n, dp) + solve(m, n - 1, dp);
+        if (dp[m][n] != -1) 
+            return dp[m][n]; 
+
+        int noOfLeftPaths = solve(m, n - 1, dp);
+        int noOfTopPaths = solve(m - 1, n, dp);
+
+        return dp[m][n] = noOfLeftPaths + noOfTopPaths;
     }
 
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp;
-        dp.resize(m + 1, vector<int>(n + 1, -1));  
-        return solve(m, n, dp);
+        dp.resize(m, vector<int>(n, -1));  
+        return solve(m - 1, n - 1, dp);
     }
 };
