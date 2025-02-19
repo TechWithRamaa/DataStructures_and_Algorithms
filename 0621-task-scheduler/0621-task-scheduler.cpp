a73df8966dd5eb80d1d4e11ed95c6dbd88b6753b
Ramaa <rkramadhevi@gmail.com>
2025-02-19 13:32:00 +0530

    Time: 28 ms (46.35%), Space: 47.8 MB (11.85%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,6 +1,55 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
+        int totalIntervals = 0;
+        
+        unordered_map<char, int> freqMap;
+        for(auto task : tasks) {
+            freqMap[task] += 1;
+        }
+
+        priority_queue<int> maxHeap;
+        for(auto [task, freq] : freqMap) {
+            maxHeap.push(freq);
+        }
+
+        while(!maxHeap.empty()) {
+            vector<int> waitList;
+            int interval = 0;
+
+            for(int i = 0; i < n + 1; i++) {
+                if(!maxHeap.empty()) {
+                    auto currentTask = maxHeap.top();
+                    maxHeap.pop();
+
+                    interval += 1;
+
+                    if(--currentTask > 0) {
+                        waitList.push_back(currentTask);
+                    }
+                } else {
+                    if(waitList.empty())
+                        break;
+
+                    interval += 1;
+                }
+            }
+
+            for(auto t : waitList) {
+                maxHeap.push(t);
+            }
+
+            totalIntervals += maxHeap.empty() ? interval : n + 1;
+        }
+
+        return totalIntervals;
+    }
+
+    // This approach is based on mathematical greedy approach
+    // Best suited for CP 
+    // Quicker calculation, but works only on static set of tasks & interval
+    // TC ~ O ( n log n ) ; SC ~ O (n)
+    int leastIntervalMathGreedy(vector<char>& tasks, int n) {
         unordered_map<char, int> freqMap;
         for (char task : tasks) 
             freqMap[task]++;
