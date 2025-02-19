28090a5986addccb3462ef2724faf9a7200cbe16
Ramaa <rkramadhevi@gmail.com>
2025-02-19 21:20:41 +0530

    Time: 47 ms (17.32%), Space: 47.6 MB (30.35%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -44,4 +44,29 @@ public:
         }
         return totalIntervals;
     }
+
+    // This approach is based on mathematical greedy approach
+    // Best suited for CP 
+    // Quicker calculation, but works only on static set of tasks & interval
+    // TC ~ O ( n log n ) ; SC ~ O (n)
+    int leastIntervalMathGreedy(vector<char>& tasks, int n) {
+        unordered_map<char, int> freqMap;
+        for (char task : tasks) 
+            freqMap[task]++;
+
+        vector<int> frequencies;
+        for (auto& pair : freqMap) 
+            frequencies.push_back(pair.second);
+        
+        sort(frequencies.rbegin(), frequencies.rend());
+
+        int maxFreq = frequencies[0];
+        int idleSlots = (maxFreq - 1) * n;
+
+        for (int i = 1; i < frequencies.size(); i++) 
+            idleSlots -= min(frequencies[i], maxFreq - 1);
+        
+        idleSlots = max(0, idleSlots);
+        return tasks.size() + idleSlots;  
+    }
 };
