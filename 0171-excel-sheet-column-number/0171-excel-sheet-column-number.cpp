7de4a64a8e670c14061a35dd5c3d1902a7919ca3
Ramaa <rkramadhevi@gmail.com>
2025-02-20 23:55:28 +0530

    Time: 0 ms (100%), Space: 8.8 MB (46.25%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -3,8 +3,7 @@ public:
     int titleToNumber(string columnTitle) {
         int result = 0;
         for (char c : columnTitle) {
-            int value =
-                c - 'A' + 1; // Convert 'A' to 1, 'B' to 2, ..., 'Z' to 26
+            int value =  c - 'A' + 1; // Convert 'A' to 1, 'B' to 2, ..., 'Z' to 26
             result = result * 26 + value; // Build the number in base 26
         }
         return result;
