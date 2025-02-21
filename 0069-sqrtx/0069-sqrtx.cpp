1ba3fb2d1065cf8139691b24bb57eee5a5578c81
Ramaa <rkramadhevi@gmail.com>
2025-02-21 21:52:07 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.34%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,34 @@
 class Solution {
 public:
-    int mySqrt(int x) {
+    // BruteForce 
+    // ~ O (n)
+    int mySqrtBrute(int x) {
         int i = 0;
         while ((long long)i * i <= x) {
             i++;
         }
         return i - 1;
+    }
+
+    // Binary Search
+    // ~ O ( log n )
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+        
+        int low = 0, high = x, ans = 0;
+        while (low <= high) {
+            long long mid = low + (high - low) / 2; // Use long long to prevent overflow
+            long long square = mid * mid;
+            
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                ans = mid; // Store mid as the potential answer
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
     }
 };
