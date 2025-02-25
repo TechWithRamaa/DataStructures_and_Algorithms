31721a5f0da0e6b2f32f7f65521b3a478722a63b
Ramaa <rkramadhevi@gmail.com>
2025-02-26 00:31:09 +0530

    Time: 0 ms (100%), Space: 20.6 MB (58.96%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    // Multiple approaches are there
+    // Approach 1 - HashSet 
+    // Approach 2 - HashMap
+    // Approach 3 - XOR
     int singleNumber(vector<int>& nums) {
         int unique = 0;
         for (int num : nums) {
