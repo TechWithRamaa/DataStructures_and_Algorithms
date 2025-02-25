c6be5f186e0be0313ec7dd17f603170435b915b8
Ramaa <rkramadhevi@gmail.com>
2025-02-26 00:19:00 +0530

    Time: 0 ms (100%), Space: 29.4 MB (79.94%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // Reverse approach ~ O ( n )
-    void rotate(vector<int>& nums, int k) {
+    void rotate2(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n; 
         
@@ -13,6 +13,26 @@ public:
         // Reverse the remaining n - k elements
         reverse(nums.begin() + k, nums.end());
     }
+
+    // Cycle approach ~ O ( n ) 
+    void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    k = k % n;  
+    int count = 0; 
+    
+    for (int start = 0; count < n; start++) {
+        int current = start;
+        int prev = nums[start];
+        
+        do {
+            int next = (current + k) % n;  
+            swap(prev, nums[next]);
+            current = next;
+            count++;
+        } while (start != current);
+    }
+}
+
 
     // BruteForce ~ O ( n )
     void rotateApproach1(vector<int>& nums, int k) {
