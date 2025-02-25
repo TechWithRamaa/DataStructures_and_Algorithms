87b762fc788a9d4262db8f96feafc186b5cfd0da
Ramaa <rkramadhevi@gmail.com>
2025-02-26 00:22:05 +0530

    Time: 23 ms (82.27%), Space: 61.2 MB (88.71%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
+    // Hashset Approach ~ O ( n )
+    bool containsDuplicateApproach1(vector<int>& nums) {
         unordered_set<int> set;
 
         for(auto num: nums){
@@ -8,6 +9,17 @@ public:
                 return true;
             }
             set.insert(num);
+        }
+        return false;
+    }
+
+    // Sorting
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
         }
         return false;
     }
