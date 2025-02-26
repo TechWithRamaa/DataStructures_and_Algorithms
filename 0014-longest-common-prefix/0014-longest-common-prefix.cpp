070be1c47412e0836a015321025ffe97b22fe511
Ramaa <rkramadhevi@gmail.com>
2025-02-26 22:47:20 +0530

    Time: 0 ms (100%), Space: 12.1 MB (22.41%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,7 +3,7 @@ public:
     // Approach 1 
     // Vertical Matching on all Strings
     // TC ~ O ( n * m )
-    string longestCommonPrefix(vector<string>& strs) {
+    string longestCommonPrefix2(vector<string>& strs) {
         for(int i = 0; i < strs[0].size(); i++) {
             for(int j = 1; j < strs.size(); j++) {
                 if(i >= strs[j].length() || strs[j][i] != strs[0][i]) {
@@ -13,5 +13,22 @@ public:
         }
 
         return strs[0];
+    }
+
+    // Approach 2
+    // Sorting every string
+    // Matching only first & last string
+    // TC ~ O (n log n + M)
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(), strs.end());
+
+        string first = strs[0], last = strs.back();
+        int i = 0;
+
+        while(i < first.length() && first[i] == last[i]) {
+            i++;
+        } 
+
+        return first.substr(0, i);
     }
 };
