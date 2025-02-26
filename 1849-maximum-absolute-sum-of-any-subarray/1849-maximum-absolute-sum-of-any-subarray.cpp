1233033eba369b29e7da7e7d41c5bc9c6a33f08c
Ramaa <rkramadhevi@gmail.com>
2025-02-26 12:29:44 +0530

    Time: 0 ms (100%), Space: 45 MB (95.34%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
-    int maxAbsoluteSum(vector<int>& nums) {
+    // Kadane's Algorithm ~ O ( n )
+    // 2 styles are there
+
+     int maxAbsoluteSum(vector<int>& nums) {
+        int maxSum = INT_MIN, minSum = INT_MAX;
+        int currentMaxSum = 0, currentMinSum = 0;
+
+        for (auto num : nums) {
+            currentMaxSum += num;
+            maxSum = max(maxSum, currentMaxSum);
+
+            currentMinSum += num;
+            minSum = min(minSum, currentMinSum);
+
+            if(currentMaxSum < 0) {
+                currentMaxSum = 0;
+            }
+
+            if(currentMinSum > 0) {
+                currentMinSum = 0;
+            }
+        }
+
+        return max(maxSum, abs(minSum));
+    }
+
+    int maxAbsoluteSum2(vector<int>& nums) {
         int maxSum = 0, minSum = 0;
         int currentMaxSum = 0, currentMinSum = 0;
 
