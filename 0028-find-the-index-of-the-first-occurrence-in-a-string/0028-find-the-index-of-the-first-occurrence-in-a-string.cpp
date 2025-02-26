aa642b1692b2211539f8ec4a327765cb1590c0db
Ramaa <rkramadhevi@gmail.com>
2025-02-26 18:48:54 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.87%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,12 +1,36 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
+    // BruteForce using substr
+    // TC ~ O ( n * m )
+    int strStr3(string haystack, string needle) {
         int n = haystack.length(), m = needle.length();
 
         for(int i = 0; i <= n - m; i++) {
             if(haystack.substr(i, m) == needle) {
                 return i;
             }
+        }
+
+        return -1;
+    }
+    
+    // BruteForce using TwoPointer
+    // TC ~ O ( n * m )
+    int strStr(string hayback, string needle) {
+        int n = hayback.length(), m = needle.length();
+        int left = 0;
+
+        while(left <= n - m) {
+            int right = 0;
+            while(right < m && hayback[left + right] == needle[right]) {
+                right++;
+            }
+
+            if(right == m) {
+                return left;
+            }
+            
+            left++;
         }
 
         return -1;
