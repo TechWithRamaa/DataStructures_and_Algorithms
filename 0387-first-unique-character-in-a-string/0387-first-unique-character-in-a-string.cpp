aef051f78a7a5c4188e1034f2f33c90dd11dd8a8
Ramaa <rkramadhevi@gmail.com>
2025-02-26 16:48:20 +0530

    Time: 19 ms (26.55%), Space: 20.5 MB (5.2%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int firstUniqChar(string s) {
+    // Approach 1 -> TC ~ O ( n )
+    // Using HashMap or Vector as Frequence Map
+    int firstUniqChar1(string s) {
         unordered_map<char, int> freq;
 
         // Count frequency of each character
@@ -15,5 +17,26 @@ public:
         }
 
         return -1;
+    }
+
+    // Approach 2 -> TC ~ O ( n )
+    // Queue based, most suited for dynamic stream of data
+    int firstUniqChar(string s) {
+        vector<int> freq(26, 0);
+        queue<int> q;
+
+        // Process characters
+        for (int i = 0; i < s.length(); i++) {
+            char c = s[i];
+            freq[c - 'a']++;
+            q.push(i);
+
+            // Remove non-unique elements
+            while (!q.empty() && freq[s[q.front()] - 'a'] > 1) {
+                q.pop();
+            }
+        }
+
+        return q.empty() ? -1 : q.front();
     }
 };
