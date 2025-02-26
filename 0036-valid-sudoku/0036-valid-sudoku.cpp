eacfdd42fc0f21e824a38326e48631dd3aa0df0d
Ramaa <rkramadhevi@gmail.com>
2025-02-26 13:52:07 +0530

    Time: 11 ms (17.36%), Space: 25.8 MB (11.9%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -6,25 +6,27 @@ public:
 
         unordered_set<string> alreadyPresent;
 
-        for(int i = 0; i < ROWS; i++) {
-            for(int j = 0; j < COLS; j++) {
-                if(board[i][j] == '.') 
-                  continue;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (board[i][j] == '.')
+                    continue;
 
                 char c = board[i][j];
                 string rowKey = string(1, c) + "@row" + to_string(i);
                 string colKey = string(1, c) + "@col" + to_string(j);
-                string boxKey = string(1, c) + "@box" + to_string(i/3) + to_string(j/3);
+                string boxKey =
+                    string(1, c) + "@box" + to_string(i / 3) + to_string(j / 3);
 
-                if(alreadyPresent.count(rowKey) || alreadyPresent.count(colKey) || alreadyPresent.count(boxKey)) {
-                    return false;
+                if (alreadyPresent.count(rowKey) ||
+                    alreadyPresent.count(colKey) ||
+                    alreadyPresent.count(boxKey)) {
+                    return false; // c already present in one of the 3 attributes
                 }
 
                 alreadyPresent.insert(rowKey);
                 alreadyPresent.insert(colKey);
                 alreadyPresent.insert(boxKey);
             }
-
         }
 
         return true;
