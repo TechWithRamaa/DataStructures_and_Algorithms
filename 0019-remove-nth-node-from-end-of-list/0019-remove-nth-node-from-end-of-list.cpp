32b19efce0847ba4e7de9a098aa96995e794f544
Ramaa <rkramadhevi@gmail.com>
2025-02-27 23:22:42 +0530

    Time: 0 ms (100%), Space: 15 MB (25.12%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,7 +10,41 @@
  */
 class Solution {
 public:
+    // Two-pass approach 
+    // ~ O ( 2 * n )
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // I keep forgetting the dummy node
+        // dummy node will take care of deleting head node
+        // using the same logic as old nodes are deleted
+        
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* current = head;
+        int length = 0;
+        while(current) {
+            length += 1;
+            current = current->next;
+        }
+
+        length -= n;
+        current = dummy;
+        while(length > 0) {
+            length--;
+            current = current->next;
+        }
+
+        ListNode* nodeToDelete = current->next;
+        current->next = current->next->next;
+        delete nodeToDelete;
+
+        ListNode* newHead = dummy->next;
+        delete dummy;
+
+        return newHead;
+    }
+
+    ListNode* removeNthFromEnd2(ListNode* head, int n) {
         ListNode* dummy = new ListNode(0, head);
         ListNode* first = dummy;
         ListNode* second = dummy;
