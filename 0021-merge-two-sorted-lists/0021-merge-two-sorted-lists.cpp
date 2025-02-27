cf060af391acc885a372407c34056be3c23c6761
Ramaa <rkramadhevi@gmail.com>
2025-02-28 00:12:18 +0530

    Time: 0 ms (100%), Space: 19.4 MB (86.61%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,9 +10,18 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    // Approach 1 -> Two Pointer Approach ( Iterative )
+    // TC ~ O ( n + m )
+    // Useful for Merge Sort Algorithm 
+    ListNode* mergeTwoLists1(ListNode* list1, ListNode* list2) {
         ListNode* dummy = new ListNode(0);
         ListNode* temp = dummy;
+
+        // whenever there is a scenario involving
+        // creation of new nodes or deletion of nodes
+        // this operation can involve at head or tail
+        // so introducing dummy head will generalize the solution
+        // edge cases don't ve to be handled
 
         while(list1 && list2) {
             if(list1->val < list2->val) {
@@ -34,5 +43,24 @@ public:
         }
 
         return dummy->next;
+    }
+
+    // Approach 2 -> Recursion
+    // May this solution type be useful in solving Merge K sorted lists
+    // TC ~ O ( n + m ) + Recursice Stack Overhead ~ O ( n + m )
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1) 
+            return list2;
+        
+        if(!list2)
+            return list1;
+
+        if(list1->val < list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1; // I forget to return here
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2; // I forget to return here
+        }
     }
 };
