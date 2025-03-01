6ec9be864adf0e18bf2f9bd156d2bb258d840d6d
Ramaa <rkramadhevi@gmail.com>
2025-03-01 20:00:27 +0530

    Time: 312 ms (36.85%), Space: 150 MB (11.12%) - LeetHub

--- 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
+++ 0905-length-of-longest-fibonacci-subsequence/0905-length-of-longest-fibonacci-subsequence.cpp
@@ -1,33 +1,61 @@
 class Solution {
 public:
+    // Approach 3 -> Pair DP
+    // TC ~ O ( N ^ 2 )
+    // Extending length from previous subsequence
+    int lenLongestFibSubseq(vector<int>& arr) {
+        int n = arr.size();
+        // Maps arr[i] → i for fast lookup
+        unordered_map<int, int> index; 
+        // DP table initialized to 2
+        vector<vector<int>> dp(n, vector<int>(n, 2)); 
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++)
+            index[arr[i]] = i; // Build index map
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int k =
+                    index.count(arr[i] - arr[j]) ? index[arr[i] - arr[j]] : -1;
+
+                if (k >= 0 && k < j) { // Valid Fibonacci condition
+                    dp[j][i] = dp[k][j] + 1;
+                    maxLen = max(maxLen, dp[j][i]);
+                }
+            }
+        }
+        return maxLen >= 3 ? maxLen : 0;
+    }
+
     // Approach 2 -> Hashset
     // TC ~ O ( N ^ 2 * log S )
     // Improvisation from the brute force
     // Internal loop is optimized
-    int lenLongestFibSubseq(vector<int>& arr) {
+    int lenLongestFibSubseq2(vector<int>& arr) {
         const int N = arr.size();
         unordered_set<int> s(arr.begin(), arr.end()); // Fast lookup set
-        
+
         int maxLength = 0;
-        
+
         // Try every pair as the start of a Fibonacci-like sequence
         for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
                 int a = arr[i], b = arr[j];
                 int length = 2; // The sequence starts with two numbers
-                
-                // Extend the sequence as long as a+b exists in the array
+
+                // Extend the sequence as long as a + b exists in the array
                 while (s.find(a + b) != s.end()) {
                     int c = a + b;
                     a = b;
                     b = c;
                     length++;
                 }
-                
+
                 maxLength = max(maxLength, length);
             }
         }
-        
+
         return maxLength >= 3 ? maxLength : 0;
     }
 
