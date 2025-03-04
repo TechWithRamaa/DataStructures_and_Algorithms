733a0a1b019e8f5e101ee1187a074e719e9d3ec7
Ramaa <rkramadhevi@gmail.com>
2025-03-04 23:32:58 +0530

    Time: 6 ms (64.81%), Space: 133.4 MB (62.02%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     // Approach 1 -> 3 Lists Approach
     // BruteForce ~ O ( N ) ; TC ~ O (N)
-    vector<int> pivotArray(vector<int>& nums, int pivot) {
+    vector<int> pivotArray1(vector<int>& nums, int pivot) {
         vector<int> less, equal, greater;
 
         for (int num : nums) {
@@ -20,6 +20,32 @@ public:
         result.insert(result.end(), less.begin(), less.end());
         result.insert(result.end(), equal.begin(), equal.end());
         result.insert(result.end(), greater.begin(), greater.end());
+
+        return result;
+    }
+
+    // Approach 2 -> Counter In-Place approach
+    // TC ~ O ( N ) ; SC ~ O ( 1 ) 
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> result;
+
+        for(auto num : nums) {
+            if(num < pivot) {
+                result.push_back(num);
+            }
+        }   
+
+        for(auto num : nums) {
+            if(num == pivot) {
+                result.push_back(num);
+            }
+        } 
+
+         for(auto num : nums) {
+            if(num > pivot) {
+                result.push_back(num);
+            }
+        } 
 
         return result;
     }
