25656aa1e465cdd3aadab2ea96d046c2d255af1b
Ramaa <rkramadhevi@gmail.com>
2025-03-05 00:16:29 +0530

    Time: 0 ms (100%), Space: 22.2 MB (18.54%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -4,32 +4,37 @@ using namespace std;
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st;
+        stack<int> st; // Stack to store moving right asteroids
 
-        for (int ast : asteroids) {
-            bool exploded = false;
+        for (int asteroid : asteroids) {
+            bool destroyed = false;
 
-            while (!st.empty() && st.top() > 0 && ast < 0) { // Collision happens
-                if (abs(st.top()) < abs(ast)) { // Right-moving asteroid is smaller
-                    st.pop();
-                    continue; // Keep checking further collisions
-                } else if (abs(st.top()) == abs(ast)) { // Both are equal, both explode
-                    st.pop();
+            // Handle collisions
+            while (!st.empty() && asteroid < 0 && st.top() > 0) {
+                if (abs(asteroid) > st.top()) {
+                    st.pop(); // Right-moving asteroid is destroyed
+                } else if (abs(asteroid) == st.top()) {
+                    st.pop(); // Both asteroids are destroyed
+                    destroyed = true;
+                    break; // No need to check further
+                } else {
+                    destroyed = true; // Left-moving asteroid is destroyed
+                    break;            // No need to check further
                 }
-                exploded = true;
-                break;
             }
 
-            if (!exploded) {
-                st.push(ast);
+            if (!destroyed) {
+                st.push(asteroid); // Push only if it wasn't destroyed
             }
         }
 
+        // Convert stack to vector (in correct order)
         vector<int> result(st.size());
-        for (int i = result.size() - 1; i >= 0; i--) {
+        for (int i = st.size() - 1; i >= 0; --i) {
             result[i] = st.top();
             st.pop();
         }
+
         return result;
     }
 };
