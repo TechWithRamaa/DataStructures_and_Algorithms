1d5d7d26dfd8b826c710b5e3af521275a3847395
Ramaa <rkramadhevi@gmail.com>
2025-03-05 08:13:33 +0530

    Time: 12 ms (40.49%), Space: 11.7 MB (79.92%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if (!head || !head->next) 
-            return false; // If the list is empty or has only one node, no cycle is possible
+            return false;
 
         ListNode *slow = head, *fast = head;
 
         while (fast && fast->next) {
-            slow = slow->next;          // Move slow by 1 step
-            fast = fast->next->next;    // Move fast by 2 steps
+            slow = slow->next;          
+            fast = fast->next->next;   
 
-            if (slow == fast) {         // If they meet, there is a cycle
+            if (slow == fast) {     
                 return true;
             }
         }
