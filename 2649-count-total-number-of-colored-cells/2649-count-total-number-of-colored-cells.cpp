8630d2e9647074a64bd38382e8f39ca5e05a2643
Ramaa <rkramadhevi@gmail.com>
2025-03-05 22:47:15 +0530

    Time: 0 ms (100%), Space: 8 MB (7.1%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+    // Approach 1 
+    // Most optimal // Mathematical approach 
+    // TC ~ O ( 1 )
     long long coloredCells(int n) {
         return 1LL * n * n + 1LL * (n - 1) * (n - 1);
     }
+
+    // Approach 2 
+    // BruteForce // BFS
+    // TC ~ O ( n ^ 2 ) && SC ~ O ( n ^ 2)
+    // Fails for larger N
+    long long coloredCells2(int n) {
+        set<pair<int, int>> colored;
+        colored.insert({0, 0});  // Start with one colored cell
+
+        for (int t = 1; t < n; t++) {
+            set<pair<int, int>> newColored;
+            for (auto [x, y] : colored) {
+                newColored.insert({x + 1, y});
+                newColored.insert({x - 1, y});
+                newColored.insert({x, y + 1});
+                newColored.insert({x, y - 1});
+            }
+            for (auto cell : newColored) {
+                colored.insert(cell);
+            }
+        }
+
+        return colored.size();
+    }
+
 };
