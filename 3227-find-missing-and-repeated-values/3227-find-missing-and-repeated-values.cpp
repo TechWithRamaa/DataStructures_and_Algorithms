cc2941d721b4fb594bc2cf1a5c4b44092fbac5ad
Ramaa <rkramadhevi@gmail.com>
2025-03-06 23:18:11 +0530

    Time: 4 ms (74.78%), Space: 26.8 MB (74.89%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+    // Approach 1
+    // Bruteforce -> TC ~ O ( n ^ 2 )
+    vector<int> findMissingAndRepeatedValues1(vector<vector<int>>& grid) {
         int n = grid.size();
         int N = n * n;
 
@@ -18,6 +20,33 @@ public:
             if (count == 2)
                 repeated = num;
             if (count == 0)
+                missing = num;
+        }
+
+        return {repeated, missing};
+    }
+
+    // Approach 1
+    // FrequencyMap -> TC ~ O ( n ) + SC ~ O ( n )
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int N = n * n;
+
+        vector<int> freq(N + 1, 0); 
+        int repeated = -1, missing = -1;
+
+        // Count occurrences
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                freq[grid[i][j]]++;
+            }
+        }
+
+        // Identify the missing and repeated numbers
+        for (int num = 1; num <= N; num++) {
+            if (freq[num] == 2)
+                repeated = num;
+            if (freq[num] == 0)
                 missing = num;
         }
 
