742201997793a79accb1d7532a2d52001b8b836a
Ramaa <rkramadhevi@gmail.com>
2025-03-06 23:22:25 +0530

    Time: 5 ms (72.13%), Space: 27 MB (72.97%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -26,13 +26,14 @@ public:
         return {repeated, missing};
     }
 
-    // Approach 1
-    // FrequencyMap -> TC ~ O ( n ) + SC ~ O ( n )
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+    // Approach 2
+    // FrequencyMap -> TC ~ O ( n ^ 2 ) 
+    // SC ~ O ( n ^ 2 )
+    vector<int> findMissingAndRepeatedValues2(vector<vector<int>>& grid) {
         int n = grid.size();
         int N = n * n;
 
-        vector<int> freq(N + 1, 0); 
+        vector<int> freq(N + 1, 0);
         int repeated = -1, missing = -1;
 
         // Count occurrences
@@ -48,6 +49,45 @@ public:
                 repeated = num;
             if (freq[num] == 0)
                 missing = num;
+        }
+
+        return {repeated, missing};
+    }
+
+    // Approach 3
+    // Sorting -> TC ~ O ( n ^ 2 log n ^ 2 ) 
+    // SC ~ O ( n ^ 2 )
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int N = n * n;
+
+        vector<int> arr;
+
+        // Flatten the matrix into 1D array
+        // ~ O ( n ^ 2 )
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr.push_back(grid[i][j]);
+            }
+        }
+
+        // Sort the array
+        sort(arr.begin(), arr.end());
+
+        int repeated = -1, missing = -1;
+
+        // Traverse the sorted array to find repeated and missing numbers
+        for (int i = 0; i < N - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                repeated = arr[i];
+            } else if (arr[i + 1] - arr[i] > 1) {
+                missing = arr[i] + 1;
+            }
+        }
+
+        // If missing is still not found, it must be N
+        if (missing == -1) {
+            missing = (arr[0] != 1) ? 1 : N;
         }
 
         return {repeated, missing};
