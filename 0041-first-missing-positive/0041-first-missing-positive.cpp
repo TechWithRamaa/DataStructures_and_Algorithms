c22a12d09f127a366a9367a65e9b233645b77d2d
Ramaa <rkramadhevi@gmail.com>
2025-03-07 07:42:50 +0530

    Time: 63 ms (14.05%), Space: 78.4 MB (11.25%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
+    // Approach 1
+    // Hashset - A BruteForce
+    // TC ~ O ( N ) ; SC ~ O ( N )
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
+        unordered_set<int> numSet(nums.begin(), nums.end());
 
-        // Step 1: Place each number in its correct position
-        // Baselined on Count Sort 
-        for (int i = 0; i < n; i++) {
-            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-                swap(nums[i], nums[nums[i] - 1]);
-            }
+        for (int i = 1; i <= nums.size(); i++) {
+            if (!numSet.count(i))
+                return i;
         }
 
-        // Step 2: Find the first missing positive
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1) 
-                return i + 1;
-        }
-
-        // If all numbers are in their correct positions
-        return n + 1;
+        return nums.size() + 1;
     }
 };
