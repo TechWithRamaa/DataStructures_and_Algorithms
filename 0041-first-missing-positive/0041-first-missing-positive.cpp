8c93b87749e77cc471682946e135db55f9a72680
Ramaa <rkramadhevi@gmail.com>
2025-03-07 08:05:54 +0530

    Time: 0 ms (100%), Space: 55.2 MB (19.33%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,7 +3,7 @@ public:
     // Approach 1
     // Hashset - A BruteForce
     // TC ~ O ( N ) ; SC ~ O ( N )
-    int firstMissingPositive(vector<int>& nums) {
+    int firstMissingPositive1(vector<int>& nums) {
         unordered_set<int> numSet(nums.begin(), nums.end());
 
         for (int i = 1; i <= nums.size(); i++) {
@@ -12,5 +12,45 @@ public:
         }
 
         return nums.size() + 1;
+    }
+
+    // Approach 2
+    // Cyclic Index - Best
+    // TC ~ O ( N ) ; SC ~ O ( 1 )
+    int firstMissingPositive2(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n &&
+                   nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i],
+                     nums[nums[i] - 1]); // Place nums[i] at correct index
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1; // First missing positive
+        }
+
+        return n + 1; // If all numbers are in place, return n + 1
+    }
+
+    int firstMissingPositive(vector<int>& nums) {
+        int N = nums.size();
+
+        for(int i = 0; i < N; i++) {
+            while(nums[i] > 0 && nums[i] <= N && nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            if(i + 1 != nums[i]) {
+                return i + 1;
+            }
+        }
+
+        return N + 1;
     }
 };
