d41444bf38adb86aa418a4b00d1238c458cc1dac
Ramaa <rkramadhevi@gmail.com>
2025-03-09 01:46:39 +0530

    Time: 0 ms (100%), Space: 46.3 MB (47.56%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    // Approach 1
+    // Problem transformed into Fixed Sliding Window
+    /* Intuition for reframing the problem 
+    By reframing the problem to find the contiguous subarray of length \U0001d45b − \U0001d458
+    with the minimum sum, you've transformed it into a fixed sliding window problem
+
+    The minimum sum found within these windows tells you which block to leave behind, 
+    so that the sum of the removed cards (which is the total sum minus this minimum sum) is maximized.
+    */
     int maxScore(vector<int>& cardPoints, int k) {
         int windowSize = cardPoints.size() - k;
         
