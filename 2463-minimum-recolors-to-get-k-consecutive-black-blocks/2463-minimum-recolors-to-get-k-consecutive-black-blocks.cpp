507f88441eed87a2ffde292c7a36309593c05558
Ramaa <rkramadhevi@gmail.com>
2025-03-09 00:35:40 +0530

    Time: 0 ms (100%), Space: 8.4 MB (62.68%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    /* Problem Statement Restated 
+    /* Problem Statement Restated
     Essentially, the problem is asking:
 
-    Find a sliding window of length k in the string 
-    where converting all white blocks inside that window 
-    to black requires the fewest changes.  
+    Find a sliding window of length k in the string
+    where converting all white blocks inside that window
+    to black requires the fewest changes.
     */
 
     int minimumRecolors(string blocks, int k) {
         int whiteCount = 0;
         int minimumWhiteInAWindow = 0;
 
-        for(int i = 0; i < k; i++) {
-            if(blocks[i] == 'W') {
-                whiteCount += 1; 
+        for (int i = 0; i < k; i++) {
+            if (blocks[i] == 'W') {
+                whiteCount += 1;
             }
         }
 
         minimumWhiteInAWindow = whiteCount;
 
-        for(int end = k; end < blocks.length(); end++) {
-            if(blocks[end - k] == 'W') {
-                    whiteCount--;
+        for (int end = k; end < blocks.length(); end++) {
+            if (blocks[end - k] == 'W') {
+                whiteCount--;
             }
 
-            if(blocks[end] == 'W') {
+            if (blocks[end] == 'W') {
                 whiteCount++;
             }
 
