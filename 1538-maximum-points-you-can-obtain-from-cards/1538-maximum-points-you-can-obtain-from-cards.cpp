94addb8727178bd3336a238c19a47812912c8c30
Ramaa <rkramadhevi@gmail.com>
2025-03-09 01:48:34 +0530

    Time: 0 ms (100%), Space: 46.2 MB (47.56%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     // Approach 1
     // Problem transformed into Fixed Sliding Window
-    /* Intuition for reframing the problem 
+    /* Intuition for reframing the problem
     By reframing the problem to find the contiguous subarray of length \U0001d45b − \U0001d458
-    with the minimum sum, you've transformed it into a fixed sliding window problem
+    with the minimum sum, you've transformed it into a fixed sliding window
+    problem
 
-    The minimum sum found within these windows tells you which block to leave behind, 
-    so that the sum of the removed cards (which is the total sum minus this minimum sum) is maximized.
+    The minimum sum found within these windows tells you which block to leave
+    behind, so that the sum of the removed cards (which is the total sum minus
+    this minimum sum) is maximized.
     */
-    int maxScore(vector<int>& cardPoints, int k) {
+    int maxScore1(vector<int>& cardPoints, int k) {
         int windowSize = cardPoints.size() - k;
-        
+
         int totalSum = 0;
-        for(auto point : cardPoints) {
+        for (auto point : cardPoints) {
             totalSum += point;
         }
 
         int subArraySum = 0;
-        for(int i = 0; i < windowSize; i++) {
+        for (int i = 0; i < windowSize; i++) {
             subArraySum += cardPoints[i];
         }
 
         int maxPoints = totalSum - subArraySum;
 
-        for(int end = windowSize; end < cardPoints.size(); end++) {
+        for (int end = windowSize; end < cardPoints.size(); end++) {
             subArraySum -= cardPoints[end - windowSize];
             subArraySum += cardPoints[end];
 
@@ -32,5 +34,30 @@ public:
         }
 
         return maxPoints;
-     }
+    }
+
+    /*  Approach 2
+    */
+    int maxScore(vector<int>& cardPoints, int k) {
+         int n = cardPoints.size();
+
+        // Initially, take all k cards from the right end.
+        int currentScore = 0;
+        for (int i = n - k; i < n; i++) {
+            currentScore += cardPoints[i];
+        }
+        
+        int maxScore = currentScore;
+        
+        // Try taking cards from the left instead one by one.
+        // x is the number of cards taken from the left, and k - x from the right.
+        for (int x = 0; x < k; x++) {
+            currentScore = currentScore - cardPoints[n - k + x] + cardPoints[x];
+            maxScore = max(maxScore, currentScore);
+        }
+        
+        return maxScore;
+    }
+
+   
 };
