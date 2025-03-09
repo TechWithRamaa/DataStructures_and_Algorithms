5144729e1854d2dc37781d0b8c5368c0c22cbb34
Ramaa <rkramadhevi@gmail.com>
2025-03-09 22:15:58 +0530

    Time: 41 ms (72.47%), Space: 45.4 MB (67.5%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
+    // very very high potential technique
+    // works on negatives, zeroes, positives
+    // window sliding will not work for negatives
+
+    // prefix sum + hashmap
+    // O ~ ( N ) - both TC & SC
     int subarraySum(const vector<int>& nums, const int k) {
         unordered_map<int, int> prefixSumCount;
+        // map has this structure = <prefixSum, count>
+
+        // sub-array with sum as zero exists and has 1 such sub-array (empty-sub-array)
         prefixSumCount[0] = 1;
 
         int count = 0;
