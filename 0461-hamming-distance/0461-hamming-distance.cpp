08e0a9a5b15f48cdad2bd790c072c2cf69606e60
Ramaa <rkramadhevi@gmail.com>
2025-03-09 18:58:22 +0530

    Time: 0 ms (100%), Space: 8 MB (6.91%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
+    int hammingDistance1(int x, int y) {
+        int count = 0;
+        int xorV = x ^ y;
+
+        // Hamming Weight logic
+        while(xorV > 0) {
+            count += xorV & 1;
+            xorV = xorV >> 1;
+        }
+
+        return count;
+    }
+
     int hammingDistance(int x, int y) {
         int count = 0;
         int xorV = x ^ y;
 
+        // Hamming Weight logic
         while(xorV > 0) {
-            count += xorV & 1;
-            xorV = xorV >> 1;
+            int t = xorV - 1;
+            xorV = xorV & t;
+            count++;
         }
 
         return count;
