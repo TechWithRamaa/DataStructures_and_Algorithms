57d840b7225fc87b9c93d845c2a027ff16a668c0
Ramaa <rkramadhevi@gmail.com>
2025-03-09 13:13:12 +0530

    Time: 12 ms (11.3%), Space: 27 MB (56.49%) - LeetHub

---
+++ 3463-alternating-groups-i/3463-alternating-groups-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    // Core skill required
+    // Array wrapping
+    int numberOfAlternatingGroups(vector<int>& colors) {
+        int solution = 0;
+        int N = colors.size();
+
+        for(int current = 0; current < N; current++) {
+            int previous = ( current - 1 + N ) % N;
+            int next = ( current + 1 ) % N;
+
+            if(colors[previous] != colors[current] && colors[current] != colors[next]) {
+                solution++;
+            }
+        }
+
+        return solution;
+    }
+};
