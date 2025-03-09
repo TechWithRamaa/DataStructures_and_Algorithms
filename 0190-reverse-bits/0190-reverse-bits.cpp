5dd66354d00186b7492e8d63d654af609b8a43ca
Ramaa <rkramadhevi@gmail.com>
2025-03-09 19:22:00 +0530

    Time: 2 ms (44.14%), Space: 7.6 MB (87.04%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+
+        for(int i = 1; i <= 32; i++) {
+            int lastBit = n & 1; // extracts last bit
+            result = result << 1; // multiplies by 2 or left shift by 1 bit
+            result = result | lastBit; // appends the extracted bit to result
+            n = n >> 1;
+        }
+
+        return result;
+    }
+};
