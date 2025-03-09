c07b0dafd9f985ad73ce62af146518784c389db0
Ramaa <rkramadhevi@gmail.com>
2025-03-09 18:42:29 +0530

    Time: 0 ms (100%), Space: 8.2 MB (80.3%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -4,8 +4,8 @@ public:
         int count = 0;
         
         while(n > 0) {
-            count += (n & 1);
-            n = n >> 1;
+            count += (n & 1); // & 1 -> extracts the last bit
+            n = n >> 1; // right shift by 1 bit or equivalent to divide by 2
         }
         
         return count;
