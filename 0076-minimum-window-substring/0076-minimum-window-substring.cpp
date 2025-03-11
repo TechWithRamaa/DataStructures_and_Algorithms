c54da8215360190b65b12cee696da9c61ece3fb0
Ramaa <rkramadhevi@gmail.com>
2025-03-12 00:18:02 +0530

    Time: 20 ms (18.24%), Space: 11.5 MB (41.9%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    // one of the unique problems I ve worked on
+    // mistakes I committed 
+    // counted freqMap for s instead of t (How silly)
+    // not closed square brackets on map indices
+    // if condition for checking new min was ulta
+
+    // Dynamic Sliding Window
+    // note down how frequency of each character is stored in 'targetMap'
+    // and no of each character that occurred atleast once is stored in 'need'
+    // these are very unique to this problem
+    
+    // if you closely observe, we used windowCount map to recall the characters we seen so far
+    // as soon as a character enter the sliding window, we need to check the count required in the targetMap
+    // if so, one need is satisifed
+    // so we increment 'have'
+
+    // if have meets needs
+    // then we can calculate minLength
+    // we shrink the window to find the best minLength as long as have == need is meeting 
+    // whenever we update the minLength, we store the starting index of that minLenth substring
+    // as soon as we decide to shrink the window, we have to decrement the leaving character in windowCount
+    // when windowCount gets updated, regressively we update have
+
+    // note down, we update minLength & have only when have == need
     string minWindow(string s, string t) {
         int minLengthWin = INT_MAX;
         int N = s.size();
@@ -14,7 +38,8 @@ public:
         unordered_map<char, int> windowCount;
 
         for (int right = 0; right < N; right++) {
-            windowCount[s[right]]++;
+            // step 1 - count the newly entered char into the window
+            windowCount[s[right]]++; 
 
             if (targetCount.find(s[right]) != targetCount.end() &&
                 targetCount[s[right]] == windowCount[s[right]]) {
