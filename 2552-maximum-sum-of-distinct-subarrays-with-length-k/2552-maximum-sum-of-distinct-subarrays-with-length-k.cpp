a4adb2f415967f68ffabbacf3a3ab239d1cb995d
Ramaa <rkramadhevi@gmail.com>
2025-03-11 23:16:53 +0530

    Time: 121 ms (25.25%), Space: 99.7 MB (34.45%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    // Dynamic Window Sliding
+    // Cant use fixed even though k length subarray is expected
+    // as we had to ensure subarray elements must be distinct within the window
     long long maximumSubarraySum(vector<int>& nums, int k) {
         long long maxSubArraySum = 0;
         int N = nums.size();
+        
         int left = 0;
-
         long long runningSum = 0;
         unordered_set<int> distinctNumbers;
         for (int right = 0; right < N; right++) {
 
-            while (distinctNumbers.count(nums[right]) > 0) {
+            while (distinctNumbers.count(nums[right])) {
                 distinctNumbers.erase(nums[left]);
                 runningSum -= nums[left];
                 left++;
