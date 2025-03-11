45f4506947a271176857890f84720dd2cb24e193
Ramaa <rkramadhevi@gmail.com>
2025-03-12 00:52:23 +0530

    Time: 0 ms (100%), Space: 10.1 MB (44.95%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -8,37 +8,39 @@ public:
     // second necessity is we maintain a frequency map of both the strings
     // s2's frequencymap keeps getting updated as we slide the window
 
-    // Check for frequency equality — not the exact string — since permutations allow any order.
+    // Check for frequency equality — not the exact string — since permutations
+    // allow any order.
     bool checkInclusion(string s1, string s2) {
         int N = s1.size();
         int M = s2.size();
 
-        if (N > M) return false; 
-
+        if (N > M)
+            return false;
 
         vector<int> freqS1Map(26, 0), freqS2Map(26, 0);
 
-        for(auto c : s1) {
+        for (auto c : s1) {
             freqS1Map[c - 'a']++;
         }
 
         // Fixed Sliding Window is already started here
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             freqS2Map[s2[i] - 'a']++;
         }
 
         // Check initial window
-        if (freqS1Map == freqS2Map) 
+        if (freqS1Map == freqS2Map)
             return true;
 
-        for(int i = N; i < M; i++) {
-            freqS2Map[s2[i] - 'a']++;  // adding the frequency of newly entered character
+        for (int i = N; i < M; i++) {
+            freqS2Map[s2[i] - 'a']++; // adding the frequency of newly entered character
 
-            freqS2Map[s2[i - N] - 'a']--;  // subtracting the frequency of exiting character in the left
+            freqS2Map[s2[i - N] - 'a']--; // subtracting the frequency of
+                                          // exiting character in the left
 
-            if(freqS1Map == freqS2Map) {
+            // Check initial window
+            if (freqS1Map == freqS2Map)
                 return true;
-            }
         }
 
         return false;
