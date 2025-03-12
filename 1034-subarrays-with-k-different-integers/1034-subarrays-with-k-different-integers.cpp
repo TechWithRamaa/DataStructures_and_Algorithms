66b98e482d0537e721baf3a817cdc21aa7b14301
Ramaa <rkramadhevi@gmail.com>
2025-03-13 00:36:00 +0530

    Time: 83 ms (63.26%), Space: 56.2 MB (62.55%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     // It is a beautiful formula
+    // whenever you re given exactly K
+    // we transform into Inclusion Exclusion principle as details below
+
+    // it is too risky, it involves hell a lot of computations
+    // so we transform into known standard problems
+    // we have transformed into Dynamic Sliding Window problem
+    // Longest SubArray Sum with atmost K distinct numbers
+
+    // Math formula
+    // Exactly K  =At most K − At most (K - 1)
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         return atMostK(nums, k) - atMostK(nums, k - 1);
     }
 
 private:
+    // template from Longest SubArray Sum with atmost K distinct numbers
     int atMostK(vector<int>& nums, int k) {
         int noOfSubArrays = 0;
         
