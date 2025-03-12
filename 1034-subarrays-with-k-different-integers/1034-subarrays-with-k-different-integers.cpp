7afb0d165668fd3e26243f0bd54c56c0745b8a79
Ramaa <rkramadhevi@gmail.com>
2025-03-13 00:33:16 +0530

    Time: 87 ms (50.09%), Space: 56.1 MB (74.73%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -29,33 +29,4 @@ private:
 
         return noOfSubArrays;
     } 
-
-
-    /* 
-    int atMostK(vector<int>& nums, int k) {
-        int left = 0, right = 0;
-        int count = 0;
-        unordered_map<int, int> freq;
-
-        for (right = 0; right < nums.size(); right++) {
-            if (freq[nums[right]] == 0) // Expansion
-                k--;                    // budget decreases when a num is bought
-
-            freq[nums[right]]++;
-
-            // Validation
-            while (k < 0) {
-                freq[nums[left]]--;
-
-                if (freq[nums[left]] == 0)
-                    k++; // budget increases when a num is sold
-
-                left++; // Shrinking
-            }
-
-            count += (right - left + 1); // gives no of sub-arrays within valid window
-        }
-
-        return count;
-    }*/
 };
