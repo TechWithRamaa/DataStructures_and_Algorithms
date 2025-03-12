a2440f341698d6b1938faa0a0bf3d96b5f518d53
Ramaa <rkramadhevi@gmail.com>
2025-03-13 00:05:07 +0530

    Time: 51 ms (22.59%), Space: 75 MB (57.44%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    // It s a beautiful experience solving this problem
+    // This problem is nothing but the Longest subarray that contains at most k distinct characters
+    
+    // Dynamic Sliding Window
     int totalFruit(vector<int>& fruits) {
         int maxFruits = INT_MIN;
 
