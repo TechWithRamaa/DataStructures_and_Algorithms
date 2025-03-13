92a140265ef7d318bcd307a792cb29e0498e219e
Ramaa <rkramadhevi@gmail.com>
2025-03-13 09:34:20 +0530

    Time: 1 ms (92.26%), Space: 71.4 MB (69.88%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
+    int numberOfSubarrays1(vector<int>& nums, int k) {
         int count = 0, prefixSum = 0;
         unordered_map<int, int> freq;
         freq[0] = 1; // Base case for zero prefix sum
@@ -20,24 +20,7 @@ public:
         return count;
     }
 
-    int numberOfSubarrays3(vector<int>& nums, int k) {
-        int count = 0, left = 0, oddCount = 0;
-
-        for (int right = 0; right < nums.size(); right++) {
-            oddCount += (nums[right] % 2);
-
-            while (oddCount > k) {
-                oddCount -= (nums[left] % 2);
-                left++;
-            }
-
-            count += (right - left + 1); // Add valid subarrays
-        }
-
-        return count;
-    }
-
-    int numberOfSubarrays4(vector<int>& nums, int k) {
+    int numberOfSubarrays(vector<int>& nums, int k) {
         // Helper function to count subarrays with at most 'k' odd numbers
         auto atMostK = [&](int k) -> int {
             int left = 0, oddCount = 0, total = 0;
