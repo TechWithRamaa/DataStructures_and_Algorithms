26f44d4c7a0cae3ce0e755f56b8e06446d449697
Ramaa <rkramadhevi@gmail.com>
2025-03-13 20:52:08 +0530

    Time: 0 ms (100%), Space: 71.5 MB (69.88%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,45 +1,51 @@
 class Solution {
 public:
     int numberOfSubarrays1(vector<int>& nums, int k) {
-        int count = 0, prefixSum = 0;
+        int subArrayCount = 0, prefixSum = 0;
+
+        // <k, v> => prefixSum, count 
         unordered_map<int, int> freq;
         freq[0] = 1; // Base case for zero prefix sum
 
         for (int num : nums) {
-            prefixSum += (num % 2); // Odd → 1, Even → 0
+            prefixSum += (num % 2); 
 
-            // Check if prefixSum - k exists
             if (freq.find(prefixSum - k) != freq.end()) {
-                count += freq[prefixSum - k];
+                subArrayCount += freq[prefixSum - k];
             }
 
-            // Update frequency map
             freq[prefixSum]++;
         }
 
-        return count;
+        return subArrayCount;
     }
 
+    // Approach 1 
+    // Variable or Dynamic Sliding Window
+    // Exactly K in a Sliding Window approach is difficult to solve
+    // So we atMost formula to solve this challenge in Sliding Window
     int numberOfSubarrays(vector<int>& nums, int k) {
         // Helper function to count subarrays with at most 'k' odd numbers
-        auto atMostK = [&](int k) -> int {
-            int left = 0, oddCount = 0, total = 0;
-
-            for (int right = 0; right < nums.size(); ++right) {
-                oddCount += (nums[right] % 2);
-
-                while (oddCount > k) {
-                    oddCount -= (nums[left] % 2);
-                    left++;
-                }
-
-                total += (right - left + 1); // All subarrays ending at 'right'
-            }
-
-            return total;
-        };
 
         // Exactly K = AtMost(K) - AtMost(K - 1)
-        return atMostK(k) - atMostK(k - 1);
+        return atMostK(nums, k) - atMostK(nums, k - 1);
+    }
+
+private:
+    int atMostK(vector<int>& nums, int k) {
+        int left = 0, oddCount = 0, subArrayCount = 0;
+
+        for(int right = 0; right < nums.size(); right++) {
+            oddCount += (nums[right] % 2); 
+
+            while(oddCount > k) {
+                oddCount -= (nums[left] % 2);
+                left++;
+            }
+
+            subArrayCount += ( right - left + 1); 
+        }
+
+        return subArrayCount;
     }
 };
