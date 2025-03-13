991f2dfe38bc2349e2e88eb17720e5a085596c6d
Ramaa <rkramadhevi@gmail.com>
2025-03-13 21:31:36 +0530

    Time: 5 ms (72.13%), Space: 71.5 MB (69.88%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -5,7 +5,7 @@ public:
 
     // Tranform this problem into Number of subarrays having exactly k sum 
     // by considering all odd numbers as 1 & even numbers as 0
-    int numberOfSubarrays(vector<int>& nums, int k) {
+    int numberOfSubarrays2(vector<int>& nums, int k) {
         int subArrayCount = 0, prefixSum = 0;
 
         // <k, v> => prefixSum, count 
@@ -29,7 +29,7 @@ public:
     // Variable or Dynamic Sliding Window
     // Exactly K in a Sliding Window approach is difficult to solve
     // So we atMost formula to solve this challenge in Sliding Window
-    int numberOfSubarrays1(vector<int>& nums, int k) {
+    int numberOfSubarrays(vector<int>& nums, int k) {
         // Helper function to count subarrays with at most 'k' odd numbers
 
         // Exactly K = AtMost(K) - AtMost(K - 1)
