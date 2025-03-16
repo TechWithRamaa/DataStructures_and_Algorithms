6b6899519ca9825de81924e69d98d5f69b901c3f
Ramaa <rkramadhevi@gmail.com>
2025-03-16 12:55:52 +0530

    Time: 11 ms (43.77%), Space: 14.7 MB (26.08%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    // Approach 1 
+    // Approach 1
     // Using Stack to mark ) or ( as invalid
     // Constructing result by eliminating invalid parenthesis
     // TC ~ O ( N ) and SC ~ O ( N )
-    string minRemoveToMakeValid(string s) {
+    string minRemoveToMakeValid1(string s) {
         stack<int> openParentheses;
 
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == '(') {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
                 openParentheses.push(i);
             } else if (s[i] == ')') {
-                if(!openParentheses.empty()) {
+                if (!openParentheses.empty()) {
                     openParentheses.pop();
                 } else {
                     s[i] = '#';
@@ -19,18 +19,59 @@ public:
             }
         }
 
-        while(!openParentheses.empty()) {
+        while (!openParentheses.empty()) {
             s[openParentheses.top()] = '#';
             openParentheses.pop();
         }
 
         string validString;
-        for(char c : s) {
-            if(c != '#') {
+        for (char c : s) {
+            if (c != '#') {
                 validString += c;
             }
         }
 
         return validString;
+    }
+
+    // Approach 2
+    // Greedy Approach
+    // TC ~ O ( N + N ) Two pass
+    string minRemoveToMakeValid(string s) {
+        string result;
+        string finalResult;
+
+        // removes extra )
+        int openCount = 0;
+        for (char c : s) {
+            if (c == '(')
+                openCount++;
+            if (c == ')') {
+                if (openCount == 0)
+                    continue;
+
+                openCount--;
+            }
+
+            result.push_back(c);
+        }
+
+        // removes extra (
+        int closedCount = 0;
+        for (int i = result.size() - 1; i >= 0 ; i--) {
+            if (result[i] == ')')
+                closedCount++;
+            if (result[i] == '(') {
+                if (closedCount == 0)
+                    continue;
+
+                closedCount--;
+            }
+
+            finalResult.push_back(result[i]);            
+        }
+
+        reverse(finalResult.begin(), finalResult.end());;
+        return finalResult;
     }
 };
