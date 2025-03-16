e371267ae85df497438842139b4a4faab9626035
Ramaa <rkramadhevi@gmail.com>
2025-03-16 18:10:38 +0530

    Time: 144 ms (51.92%), Space: 170.1 MB (76.67%) - LeetHub

---
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.cpp
@@ -0,0 +1,38 @@
+class SparseVector {
+
+public:
+    vector<pair<int, int>> nonZeroVector;
+
+    SparseVector(vector<int> &nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                nonZeroVector.push_back({i, nums[i]});
+            }
+        }
+    }
+    
+    // Return the dotProduct of two sparse vectors
+    int dotProduct(SparseVector& vec) {
+        int i = 0, j = 0, dotProduct = 0;
+        long long result = 0; // Use long long for safety
+
+        while(i < nonZeroVector.size() && j < vec.nonZeroVector.size()) {
+            if(nonZeroVector[i].first == vec.nonZeroVector[j].first) {
+                result += nonZeroVector[i].second * vec.nonZeroVector[j].second;
+                i++;
+                j++;
+            } else if (nonZeroVector[i].first < vec.nonZeroVector[j].first) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return static_cast<int>(result);
+    }
+};
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1(nums1);
+// SparseVector v2(nums2);
+// int ans = v1.dotProduct(v2);
