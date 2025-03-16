265f9790b3eadbc622c423a2cf4a553de28d898b
Ramaa <rkramadhevi@gmail.com>
2025-03-16 14:18:38 +0530

    Time: 2 ms (32.82%), Space: 59.3 MB (26.14%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     // Dyanmic Sized Sliding Window
     int longestOnes(vector<int>& nums, int k) {
-        int maxConsecutiveOnes = 0;
-       
-        int zeros = 0;
+        int maxOnes = 0, zeroCount = 0;
 
         int left = 0;
         for(int right = 0; right < nums.size(); right++) {
             if(nums[right] == 0) {
-                zeros++;
+                zeroCount++;
             }
 
-            while(zeros > k) {
+            while(zeroCount > k) {
                 if(nums[left] == 0)
-                    zeros--;
+                    zeroCount--;
+                    
                 left++;
             }
 
-            maxConsecutiveOnes = max(maxConsecutiveOnes, right - left + 1);
+            maxOnes = max(maxOnes, right - left + 1);
         }
 
-        return maxConsecutiveOnes;
+        return maxOnes;
     }
 };
