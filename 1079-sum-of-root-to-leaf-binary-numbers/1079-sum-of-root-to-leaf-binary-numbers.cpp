2eaecb5f88948f693b8d0a4c5d33590972d6b711
Ramaa <rkramadhevi@gmail.com>
2025-03-16 15:56:06 +0530

    Time: 0 ms (100%), Space: 17.5 MB (37.23%) - LeetHub

--- 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/1079-sum-of-root-to-leaf-binary-numbers.cpp
@@ -12,10 +12,13 @@
 class Solution {
 public:
     int sumRootToLeaf(TreeNode* root) {
-        return sumRootToLeafDFS(root, 0);
+        //return sumRootToLeafDFS(root, 0);
+        return sumRootToLeafBFS(root);
     }
 private:
     // Pre-Order DFS Traversal
+    // Recursion 
+    // TC ~ O ( N ) ; SC ~ O ( N )
     int sumRootToLeafDFS(TreeNode* root, int currentVal) {
         // base condition
         if(!root) {
@@ -36,4 +39,35 @@ private:
 
         return sumRootToLeafDFS(root->left, currentVal) + sumRootToLeafDFS(root->right, currentVal);
     }
+
+    // BFS Traversal
+    // Iterative 
+    // TC ~ O ( N ) ; SC ~ O ( N )
+    int sumRootToLeafBFS(TreeNode* root) {
+        int totalSum = 0;
+
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+
+        while(!q.empty()) {
+            auto current = q.front();
+            q.pop();
+            TreeNode* node = current.first;
+            int currentValue = current.second;
+
+            currentValue = currentValue << 1 | node->val;
+
+            if(!node->left && !node->right) {
+                totalSum += currentValue;
+            }
+
+            if(node->left) 
+                q.push({node->left, currentValue});
+            if(node->right) 
+                q.push({node->right, currentValue});
+        }
+
+        return totalSum;
+    }
+
 };
