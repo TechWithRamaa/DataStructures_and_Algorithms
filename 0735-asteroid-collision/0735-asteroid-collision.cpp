e0da06b4504c02335b4dbf142acd0586f89d845d
Ramaa <rkramadhevi@gmail.com>
2025-03-17 09:19:24 +0530

    Time: 0 ms (100%), Space: 22 MB (60.06%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,39 +1,39 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st; // Stack to store moving right asteroids
+        stack<int> st;
 
-        for (int asteroid : asteroids) {
-            bool destroyed = false;
+        for(int asteroid : asteroids) {
+            if(asteroid > 0) {
+                st.push(asteroid);
+            } else {
+                bool destroyed = false;
 
-            // Handle collisions
-            while (!st.empty() && asteroid < 0 && st.top() > 0) {
-                if (abs(asteroid) > st.top()) {
-                    st.pop(); // Right-moving asteroid is destroyed
-                } else if (abs(asteroid) == st.top()) {
-                    st.pop(); // Both asteroids are destroyed
-                    destroyed = true;
-                    break; // No need to check further
-                } else {
-                    destroyed = true; // Left-moving asteroid is destroyed
-                    break;            // No need to check further
+                while(!st.empty() && st.top() > 0) {
+                    if(abs(asteroid) == st.top()) {
+                        st.pop();
+                        destroyed = true;
+                        break;
+                    } else if (abs(asteroid) > st.top()) {
+                        st.pop();
+                    } else {
+                        destroyed = true;
+                        break;
+                    }
                 }
-            }
 
-            if (!destroyed) {
-                st.push(asteroid); // Push only if it wasn't destroyed
+                if(!destroyed)
+                    st.push(asteroid); 
             }
         }
 
-        // Convert stack to vector (in correct order)
-        vector<int> result(st.size());
-        for (int i = st.size() - 1; i >= 0; --i) {
-            result[i] = st.top();
+        vector<int> result;
+        while(!st.empty()) {
+            result.push_back(st.top());
             st.pop();
         }
+
+        reverse(result.begin(), result.end());
 
         return result;
     }
