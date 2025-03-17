63695a82f5a7501e742df2c1b187ccf57fbbcfbb
Ramaa <rkramadhevi@gmail.com>
2025-03-17 23:16:44 +0530

    Time: 0 ms (100%), Space: 17.1 MB (66.52%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
-    bool divideArray(vector<int>& nums) {
+    // Approach 1
+    // TC ~ O ( 2 * n ) ; SC ~ O ( n )
+    bool divideArray1(vector<int>& nums) {
         unordered_map<int, int> freqMap;
 
-        for(int num : nums) {
+        for (int num : nums) {
             freqMap[num]++;
         }
 
-        for(auto [num, freq] : freqMap) {
-            if(freqMap[num] % 2 != 0) 
+        for (auto [num, freq] : freqMap) {
+            if (freqMap[num] % 2 != 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    // Approach 2
+    // TC ~ O ( 2 * n ) ; SC ~ O ( 1 )
+    bool divideArray(vector<int>& nums) {
+        vector<int> freqVector(501, 0);
+
+        for (int num : nums) {
+            freqVector[num]++;
+        }
+
+        for (int freq : freqVector) {
+            if (freq % 2 != 0)
                 return false;
         }
 
