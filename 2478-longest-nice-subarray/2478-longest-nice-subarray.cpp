93297622e110691a28912dc905f9e9950fce35b5
Ramaa <rkramadhevi@gmail.com>
2025-03-18 22:43:08 +0530

    Time: 0 ms (100%), Space: 60.9 MB (87.7%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int longestSubarray = 0;
+        int left = 0, cumulativeSetBits = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            while ((cumulativeSetBits & nums[right]) != 0) {
+                cumulativeSetBits ^= nums[left];
+                left++;
+            }
+            cumulativeSetBits |= nums[right];
+            longestSubarray = max(longestSubarray, right - left + 1);
+        }
+
+        return longestSubarray;
+    }
+};
