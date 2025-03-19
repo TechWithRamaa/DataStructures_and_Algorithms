e130411e0f489854e075891d62fd1a1b80decf0e
Ramaa <rkramadhevi@gmail.com>
2025-03-19 13:04:19 +0530

    Time: 0 ms (100%), Space: 14.7 MB (63.7%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int findLengthOfLCIS1(vector<int>& nums) {
-       int maxLength = 1;
-
-        int start = 0;
-       for(int end = 1; end < nums.size(); end++) {
-            if(nums[end] > nums[end - 1]) {
-                maxLength = max(maxLength, end - start + 1);
-            } else {
-                start = end;
-            }
-       }
-
-
-       return maxLength; 
-    }
-
+    // Approach 1
+    // Dynamic Sliding Window 
+    // Window shrinks by x unit, just reset the start to new end
     int findLengthOfLCIS(vector<int>& nums) {
         int maxLength = 1;
 
         int start = 0;
-        for(int end = 1; end < nums.size(); end++) {
-            while(nums[end] <= nums[end-1] && start < end) {
+        for (int end = 1; end < nums.size(); end++) {
+            if (nums[end] > nums[end - 1]) {
+                maxLength = max(maxLength, end - start + 1);
+            } else {
+                start = end;
+            }
+        }
+
+        return maxLength;
+    }
+
+    // Approach 2
+    // Dynamic Sliding Window
+    // Window shrinks by 1 unit
+    int findLengthOfLCIS2(vector<int>& nums) {
+        int maxLength = 1;
+
+        int start = 0;
+        for (int end = 1; end < nums.size(); end++) {
+            while (nums[end] <= nums[end - 1] && start < end) {
                 start++;
             }
 
@@ -29,5 +34,5 @@ public:
         }
 
         return maxLength;
-     }
+    }
 };
