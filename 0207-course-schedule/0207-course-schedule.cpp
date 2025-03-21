6eb4f43d7b203a2670f20ce7d909d92819a2b2dc
Ramaa <rkramadhevi@gmail.com>
2025-03-21 22:40:21 +0530

    Time: 0 ms (100%), Space: 19.1 MB (94.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adjList(numCourses);
-        vector<int> inDegrees(numCourses, 0);
+        vector<int> inDegree(numCourses, 0);
+        vector<vector<int>> graph(numCourses);
 
-        for(auto& entry: prerequisites) {
-            int preReq = entry[1];
-            int course = entry[0];
-            adjList[preReq].push_back(course);
-            inDegrees[course] += 1;
+        for(auto& pre : prerequisites) {
+            int a = pre[0], b = pre[1];
+            graph[b].push_back(a);
+            inDegree[a]++;
         }
 
         queue<int> q;
         for(int i = 0; i < numCourses; i++) {
-            if(inDegrees[i] == 0) {
+            if(inDegree[i] == 0) {
                 q.push(i);
             }
         }
 
-        int courses = 0;
+        int coursesTaken = 0;
         while(!q.empty()) {
-            int currentCourse = q.front();
+            int completedCourse = q.front();
             q.pop();
 
-            courses += 1;
-            for(auto next : adjList[currentCourse]) {
-                if(--inDegrees[next] == 0) {
-                    q.push(next);
+            for(auto dependent : graph[completedCourse]) {
+                inDegree[dependent]--;
+                if(inDegree[dependent] == 0) {
+                    q.push(dependent);
+                    // all its prerequisites are completed, now this dependent can be taken
                 }
             }
+
+            coursesTaken++;
         }
 
-        return courses == numCourses;
+        return coursesTaken == numCourses;
     }
 };
