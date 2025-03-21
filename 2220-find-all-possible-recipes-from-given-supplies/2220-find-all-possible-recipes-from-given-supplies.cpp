dd8dad5019484d20e629beb5ef93ecc29e3e16fb
Ramaa <rkramadhevi@gmail.com>
2025-03-22 01:11:05 +0530

    Time: 46 ms (86.75%), Space: 107.7 MB (74.68%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -3,44 +3,41 @@ public:
     vector<string> findAllRecipes(vector<string>& recipes,
                                   vector<vector<string>>& ingredients,
                                   vector<string>& supplies) {
-        unordered_map<string, vector<string>> graph; 
         unordered_map<string, int> inDegree;
+        unordered_map<string, vector<string>> graph;
         unordered_set<string> available(supplies.begin(), supplies.end());
 
-        queue<string> q;
-        vector<string> result;
-
-        // Step 1: Build the Graph & Compute inDegree
         for (int i = 0; i < recipes.size(); i++) {
             string recipe = recipes[i];
             inDegree[recipe] = 0;
-
-            for (string& ingredient : ingredients[i]) {
-                if (available.count(ingredient) == 0) { 
+            for (auto ingredient : ingredients[i]) {
+                if (available.count(ingredient) == 0) {
                     graph[ingredient].push_back(recipe);
                     inDegree[recipe]++;
                 }
             }
+        }
 
-            // If recipe has no missing dependencies, it can be made immediately
-            if (inDegree[recipe] == 0) {
-                q.push(recipe);
-                //result.push_back(recipe);
+        queue<string> q;
+        for(auto [key, value] : inDegree) {
+            if(value == 0) {
+                q.push(key);
             }
         }
 
-        // Step 2: Process Recipes in Order (Topological Sorting using BFS)
-        while (!q.empty()) {
-            string curr = q.front();
+        vector<string> result;
+
+        while(!q.empty()) {
+            auto processing = q.front();
             q.pop();
 
-            result.push_back(curr);
+            result.push_back(processing);
 
-            for (string& nextRecipe : graph[curr]) {
-                inDegree[nextRecipe]--;
+            for(auto dependent : graph[processing]) {
+                inDegree[dependent]--;
 
-                if (inDegree[nextRecipe] == 0) { 
-                    q.push(nextRecipe);
+                if(inDegree[dependent] == 0) {
+                    q.push(dependent);
                 }
             }
         }
