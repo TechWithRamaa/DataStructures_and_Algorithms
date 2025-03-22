5cbc86072ccc408efa121a673c6f6c5f24acfc65
Ramaa <rkramadhevi@gmail.com>
2025-03-22 09:17:23 +0530

    Time: 0 ms (100%), Space: 7.7 MB (92.32%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    // Approach 1 
-    // Most optimal // Mathematical approach 
+    // Approach 1
+    // Most optimal // Mathematical approach
     // TC ~ O ( 1 )
     long long coloredCells(int n) {
         return 1LL * n * n + 1LL * (n - 1) * (n - 1);
     }
 
-    // Approach 2 
+    // Approach 2
     // BruteForce // BFS with a SET instead of a QUEUE
     // TC ~ O ( n ^ 2 ) && SC ~ O ( n ^ 2)
     // Slow for larger N
     long long coloredCells2(int n) {
         set<pair<int, int>> colored; // set helps us maintain distinct cells
-        colored.insert({0, 0});  // Start with one colored cell
+        colored.insert({0, 0});      // Start with one colored cell
 
         for (int t = 1; t < n; t++) {
             set<pair<int, int>> newColored;
@@ -31,4 +31,35 @@ public:
         return colored.size();
     }
 
+    // we cant use pair directly in unordered_set
+    // it doesnt know how to hash a pair
+    // we have to provide a custom hash function to store pair
+
+    // that s why we have used set because we are lazy
+    // set underneath uses Red Black AVL tree
+    // so log (n) for lookup and insertion
+
+    long long coloredCells3(int n) {
+    auto hashLambda = [] (const pair<int, int>& p) {  // Add `&` to avoid unnecessary copies
+        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+    };
+
+    unordered_set<pair<int, int>, decltype(hashLambda)> colored(10, hashLambda); 
+    colored.insert({0, 0});  // Start with one colored cell
+
+    for (int t = 1; t < n; t++) {
+        unordered_set<pair<int, int>, decltype(hashLambda)> newColored(10, hashLambda);
+        for (auto [x, y] : colored) {
+            newColored.insert({x + 1, y});
+            newColored.insert({x - 1, y});
+            newColored.insert({x, y + 1});
+            newColored.insert({x, y - 1});
+        }
+        for (auto cell : newColored) {
+            colored.insert(cell);
+        }
+    }
+
+    return colored.size();
+}
 };
