2c294e1ed469a72a4f8dc55cc97c80a3d4386d53
Ramaa <rkramadhevi@gmail.com>
2025-03-22 13:36:58 +0530

    Time: 0 ms (100%), Space: 21.5 MB (8.45%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
+    // Approach 1
+    // BruteForce 
+    // TC ~ O ( n )
+    int maximumCount1(vector<int>& nums) {
+        int negCount = 0, posCount = 0;
+        for (int num : nums) {
+            if (num < 0) negCount++;
+            else if (num > 0) posCount++;
+        }
+        return max(negCount, posCount);
+    }
+
+    // Approach 2
+    // BinarySearch STL
     int maximumCount(vector<int>& nums) {
         // Binary search for the first non-negative element
-        int negCount = lower_bound(nums.begin(), nums.end(), 0) - nums.begin();
+        auto lastIndexOfNegatives = lower_bound(nums.begin(), nums.end(), 0);
+        int negCount = lastIndexOfNegatives - nums.begin();
 
         // Binary search for the first positive element
-        int posCount = nums.end() - upper_bound(nums.begin(), nums.end(), 0);
+        auto firstIndexOfPositives = upper_bound(nums.begin(), nums.end(), 0);
+        int posCount = nums.end() - firstIndexOfPositives;
 
         return max(negCount, posCount);
     }
