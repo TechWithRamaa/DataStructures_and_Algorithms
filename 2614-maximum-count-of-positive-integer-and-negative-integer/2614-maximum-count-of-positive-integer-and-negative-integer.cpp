387961ba9b9e0ed0c57d7e3f7c5e6e111ce643c8
Ramaa <rkramadhevi@gmail.com>
2025-03-22 13:38:50 +0530

    Time: 0 ms (100%), Space: 21.3 MB (95.52%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     // Approach 1
-    // BruteForce 
+    // BruteForce
     // TC ~ O ( n )
     int maximumCount1(vector<int>& nums) {
         int negCount = 0, posCount = 0;
         for (int num : nums) {
-            if (num < 0) negCount++;
-            else if (num > 0) posCount++;
+            if (num < 0)
+                negCount++;
+            else if (num > 0)
+                posCount++;
         }
         return max(negCount, posCount);
     }
 
     // Approach 2
     // BinarySearch STL
-    int maximumCount(vector<int>& nums) {
-        // Binary search for the first non-negative element
+    // TC ~ O ( log n )
+    int maximumCount2(vector<int>& nums) {
+        // Binary search for the first non-negative element or zero
         auto lastIndexOfNegatives = lower_bound(nums.begin(), nums.end(), 0);
         int negCount = lastIndexOfNegatives - nums.begin();
 
@@ -23,6 +26,14 @@ public:
         auto firstIndexOfPositives = upper_bound(nums.begin(), nums.end(), 0);
         int posCount = nums.end() - firstIndexOfPositives;
 
+        return max(negCount, posCount);
+    }
+
+    // Approach 2
+    // in few lines
+    int maximumCount(vector<int>& nums) {
+        int negCount = lower_bound(nums.begin(), nums.end(), 0) - nums.begin();
+        int posCount = nums.end() - upper_bound(nums.begin(), nums.end(), 0);
         return max(negCount, posCount);
     }
 };
