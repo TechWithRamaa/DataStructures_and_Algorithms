07fa050e4c0699760c64391bc8947000745e8232
Ramaa <rkramadhevi@gmail.com>
2025-03-22 06:56:46 +0530

    Time: 80 ms (27.32%), Space: 137 MB (36.53%) - LeetHub

---
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        int noOfCompleteComponents = 0;
+
+        vector<vector<int>> graph(n);
+        for(auto edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        vector<bool> visited(n, false);
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                vector<int> nodes;
+                int edgeCount = 0;
+
+                dfs(graph, visited, i, nodes, edgeCount);
+
+                int noOfNodes = nodes.size();
+                int expectedEdges = (noOfNodes * (noOfNodes - 1)) / 2; 
+
+                if(expectedEdges == edgeCount/2) {
+                    noOfCompleteComponents++;
+                }
+            }
+        }
+
+        return noOfCompleteComponents;
+    }
+
+    void dfs(vector<vector<int>>& graph, vector<bool>& visited, int node, vector<int>& nodes, int& edgeCount) {
+        visited[node] = true;
+        nodes.push_back(node); //  Add node to the current component
+
+        for(auto neighbor : graph[node]) {
+            edgeCount++; // Count the edge
+           
+            if(!visited[neighbor]) {
+                dfs(graph, visited, neighbor, nodes, edgeCount);
+            }
+        }
+    }
+};
