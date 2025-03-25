56f1b8f380624a6e983207c97f361551c6bf7451
Ramaa <rkramadhevi@gmail.com>
2025-03-25 15:14:46 +0530

    Time: 4 ms (77.55%), Space: 84.1 MB (74.42%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -4,8 +4,8 @@ public:
     // name - Copy Trick
     // Extending the circular nums array
     // To make the given cirucular array into a linear array
-    // Fixed Sliding Window
-    int minSwaps(vector<int>& nums) {
+    // Fixed Sliding Window - Classic template
+    int minSwaps1(vector<int>& nums) {
 
         // detemining the window size
         int countOnes = count(nums.begin(), nums.end(), 1);
@@ -13,7 +13,8 @@ public:
         vector<int> extended = nums;
         extended.insert(extended.end(), nums.begin(), nums.end());
 
-        int countZeroes = count(extended.begin(), extended.begin() + countOnes, 0);
+        int countZeroes =
+            count(extended.begin(), extended.begin() + countOnes, 0);
 
         int minSwaps = countZeroes;
 
@@ -70,5 +71,32 @@ public:
     // Approach 2
     // Modulo Indexing
     // Very memory efficient
-    int minSwaps3(vector<int>& nums) { return 0; }
+    // Fixed Sliding Window
+    int minSwaps(vector<int>& nums) {
+        // determining the window size
+        int countOnes = count(nums.begin(), nums.end(), 1);
+
+        // no of zeros in first fixed window
+        int countZeroes = count(nums.begin(), nums.begin() + countOnes, 0);
+
+        int minSwaps = countZeroes;
+        int N = nums.size();
+        for (int i = 1; i < N; i++) {
+
+            int outIdx = (i - 1) % N;
+            int inIdx = (i + countOnes - 1) % N;
+
+            // outgoing element
+            if (nums[outIdx] == 0)
+                countZeroes--;
+
+            // incoming element
+            if (nums[inIdx] == 0)
+                countZeroes++;
+
+            minSwaps = min(minSwaps, countZeroes);
+        }
+
+        return minSwaps;
+    }
 };
