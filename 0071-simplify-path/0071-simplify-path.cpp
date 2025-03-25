14e1d58d042377b9411824b0c2b6f8c70b4b1607
Ramaa <rkramadhevi@gmail.com>
2025-03-25 21:29:42 +0530

    Time: 7 ms (14.77%), Space: 12 MB (24.27%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stringstream ss(path);
+        string token;
+
+        string result = "";
+        stack<string> st;
+
+        while(getline(ss, token, '/')) {
+            if(token == "." || token == "")
+                continue;
+
+            if(token == "..") {
+                if(!st.empty()) {
+                    st.pop();
+                }
+            } else {
+                st.push(token);
+            }
+        }
+
+        while(!st.empty()) {
+            result = "/" + st.top() + result;
+            st.pop();
+        }
+
+        return result.empty() ? "/" : result;
+    }
+};
