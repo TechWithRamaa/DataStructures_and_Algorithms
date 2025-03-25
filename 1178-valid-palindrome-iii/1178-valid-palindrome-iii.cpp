35b3df54cad4bc18f5b06d4a7327c35d155ff38e
Ramaa <rkramadhevi@gmail.com>
2025-03-26 00:05:32 +0530

    Time: 14 ms (60.4%), Space: 19.4 MB (70.44%) - LeetHub

---
+++ 1178-valid-palindrome-iii/1178-valid-palindrome-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValidPalindrome(string s, int k) {
+        int lps = longestPalindromeSubsequence(s);
+        return s.size() - lps <= k;
+    }
+
+private:
+    int longestPalindromeSubsequence(string s) {
+        int N = s.size();
+
+        vector<vector<int>> dp(N, vector<int>(N, 0));
+
+        for(int i = 0; i < N; i++) 
+            dp[i][i] = 1;
+
+        for(int len = 2; len <= N; len++) {
+            for(int i = 0; i <= N - len; i++) {
+                
+                int j = i + len - 1;
+
+                if(s[i] == s[j]) 
+                    dp[i][j] = 2 + dp[i+1][j-1];
+                else 
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[0][N-1];
+    }
+};
