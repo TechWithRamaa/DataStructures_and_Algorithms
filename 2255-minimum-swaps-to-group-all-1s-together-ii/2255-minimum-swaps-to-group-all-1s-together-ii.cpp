9308260a2c371e13da4cac666c49158c6e7d504b
Ramaa <rkramadhevi@gmail.com>
2025-03-25 14:53:44 +0530

    Time: 8 ms (48.38%), Space: 95.3 MB (5.56%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,10 +1,46 @@
 class Solution {
 public:
     // Approach 1
-    // name - Copy Trick 
-    // Extending the circular nums array 
+    // name - Copy Trick
+    // Extending the circular nums array
     // To make the given cirucular array into a linear array
+    // Fixed Sliding Window
     int minSwaps(vector<int>& nums) {
+
+        // detemining the window size
+        int countOnes = count(nums.begin(), nums.end(), 1);
+
+        vector<int> extended = nums;
+        extended.insert(extended.end(), nums.begin(), nums.end());
+
+        int countZeroes = count(extended.begin(), extended.begin() + countOnes, 0);
+
+        int minSwaps = countZeroes;
+
+        for (int i = countOnes; i < extended.size(); i++) {
+
+            // outgoing zero
+            if (extended[i - countOnes] == 0)
+                countZeroes--;
+
+            // incoming zero
+            if (extended[i] == 0)
+                countZeroes++;
+
+            // updating optimized result
+            minSwaps = min(minSwaps, countZeroes);
+        }
+
+        return minSwaps;
+    }
+
+    // Approach 1
+    // name - Copy Trick
+    // Extending the circular nums array
+    // To make the given cirucular array into a linear array
+    // Fixed Sliding Window - Hybrid (template almost like a Dynamic Sliding
+    // Window)
+    int minSwaps2(vector<int>& nums) {
         vector<int> extended = nums;
         extended.insert(extended.end(), nums.begin(), nums.end());
 
@@ -13,18 +49,18 @@ public:
 
         int minSwaps = INT_MAX, countZeroes = 0;
 
-        for(int i = 0; i < extended.size(); i++) {
+        for (int i = 0; i < extended.size(); i++) {
 
             // outgoing zero
-            if(i >= countOnes && extended[i - countOnes] == 0) 
+            if (i >= countOnes && extended[i - countOnes] == 0)
                 countZeroes--;
 
             // incoming zero
-            if(extended[i] == 0) 
+            if (extended[i] == 0)
                 countZeroes++;
-            
+
             // updating optimized result
-            if(i >= countOnes)
+            if (i >= countOnes)
                 minSwaps = min(minSwaps, countZeroes);
         }
 
@@ -34,7 +70,5 @@ public:
     // Approach 2
     // Modulo Indexing
     // Very memory efficient
-    int minSwaps2(vector<int>& nums) {
-        return 0;
-    }
+    int minSwaps3(vector<int>& nums) { return 0; }
 };
