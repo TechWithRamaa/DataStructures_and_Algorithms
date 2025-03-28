c56c7e73461cd7550d51ae44688568f3e4958e01
Ramaa <rkramadhevi@gmail.com>
2025-03-28 21:24:11 +0530

    Time: 80 ms (90.22%), Space: 39.9 MB (89.86%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
@@ -1,46 +1,50 @@
 class Solution {
 public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
-        int m = grid.size(), n = grid[0].size();
-        int dirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int M = grid.size();
+        int N = grid[0].size();
 
-        // Store queries with their original index
-        vector<pair<int, int>> sortedQueries;
+        vector<pair<int, int>> queriesIndexList;
         for (int i = 0; i < queries.size(); i++) {
-            sortedQueries.push_back({queries[i], i});
+            queriesIndexList.push_back({queries[i], i});
         }
-        sort(sortedQueries.begin(), sortedQueries.end()); // Sort queries
 
-        // Min-Heap to process grid cells in increasing order
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>>  minHeap;
-        minHeap.push({grid[0][0], {0, 0}});
+        sort(queriesIndexList.begin(), queriesIndexList.end());
 
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        visited[0][0] = true;
-
-        vector<int> result(queries.size(), 0);
+        vector<pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        vector<vector<bool>> isVisited(M, vector<bool>(N, false));
+        vector<int> results(queries.size(), 0);
         int count = 0;
 
-        for (auto& [query, idx] : sortedQueries) {
-            // Expand cells in increasing order while grid[i][j] < query
-            while (!minHeap.empty() && minHeap.top().first < query) {
-                auto [val, pos] = minHeap.top();
-                minHeap.pop();
-                int r = pos.first, c = pos.second;
-                count++; // Increment the count of processed cells
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> minHeap;
+        minHeap.push({grid[0][0], {0, 0}});
+        isVisited[0][0] = true;       
 
-                // Explore neighbors
-                for (auto& dir : dirs) {
-                    int nr = r + dir[0], nc = c + dir[1];
-                    if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
-                        !visited[nr][nc]) {
+        for (auto& [query, index] : queriesIndexList) {
+           
+            while (!minHeap.empty() && minHeap.top().first < query) {
+                auto [val, position] = minHeap.top();
+                minHeap.pop();
+                
+                int r = position.first;
+                int c = position.second;
+
+                count++;
+
+                for(auto& [dr, dc] : directions) {
+                    int nr = r + dr;
+                    int nc = c + dc;
+
+                    if(nr >= 0 && nr < M && nc >= 0 && nc < N && !isVisited[nr][nc]) {
                         minHeap.push({grid[nr][nc], {nr, nc}});
-                        visited[nr][nc] = true;
+                        isVisited[nr][nc] = true;
                     }
                 }
             }
-            result[idx] = count; // Store answer for this query
+
+            results[index] = count;
         }
-        return result;
+
+        return results;
     }
 };
