da41ffb0822ab68a2c32a26a41f7b512c839cd6b
Ramaa <rkramadhevi@gmail.com>
2025-03-29 09:58:59 +0530

    Time: 0 ms (100%), Space: 12.8 MB (30.41%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -6,24 +6,18 @@ public:
         stack<int> st;
 
         for (int i = nums2.size() - 1; i >= 0; i--) {
-            while (!st.empty() && st.top() <= nums2[i]) {
+            while (!st.empty() && st.top() <= nums2[i])
                 st.pop();
-            }
 
-            if (!st.empty()) {
-                nge2[nums2[i]] = st.top();
-            } else {
-                nge2[nums2[i]] = -1;
-            }
+            nge2[nums2[i]] = !st.empty() ? st.top() : -1;
 
             st.push(nums2[i]);
         }
 
-        vector<int> nge1(nums1.size(), -1);
-
-        for(int i = 0; i < nums1.size(); i++) {
-            if(nge2.find(nums1[i]) != nge2.end()) {
-                nge1[i] = nge2[nums1[i]];
+        vector<int> nge1;
+        for (auto num : nums1) {
+            if (nge2.find(num) != nge2.end()) {
+                nge1.push_back(nge2[num]);
             }
         }
 
