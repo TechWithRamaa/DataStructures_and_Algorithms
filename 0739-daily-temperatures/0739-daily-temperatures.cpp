1fb0f8815835f8aa74275d952d07ddfd39c25862
Ramaa <rkramadhevi@gmail.com>
2025-03-29 15:54:34 +0530

    Time: 18 ms (77.17%), Space: 102.9 MB (69.4%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
+    // Approach 1 -> Next Greater Element -> Monotonic Stack (Decreasing order)
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int N = temperatures.size();
-
         vector<int> nextWarmerDaysIn(N, 0);
 
         // Next Greater Element // Monotonic Decreasing Stack
@@ -10,13 +10,15 @@ public:
 
         for (int i = N - 1; i >= 0; i--) {
 
-            // pop smaller elements
+            // step 1 -> pop smaller elements
             while (!st.empty() && temperatures[st.top()] <= temperatures[i])
                 st.pop();
 
+            // step 2 -> calculate warmer days
             if (!st.empty())
                 nextWarmerDaysIn[i] = st.top() - i;
 
+            // step 3 -> push the current temperature
             st.push(i);
         }
 
