fff976f846c02fc120427d5f45640ee4e3b7337c
Ramaa <rkramadhevi@gmail.com>
2025-03-30 22:36:09 +0530

    Time: 0 ms (100%), Space: 13.3 MB (59.97%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int findKthPositive(vector<int>& arr, int k) {
+    // Approach 1 ~ BruteForce 
+    int findKthPositive1(vector<int>& arr, int k) {
         int missingCount = 0, num = 1, i = 0;
 
         while (k > 0) {
@@ -15,7 +16,21 @@ public:
 
             num++; // Move to the next number
         }
-        
+
         return -1; // Should never reach here
+    }
+
+    // Approach 2 ~ Missing Count -> Number Theory
+    int findKthPositive(vector<int>& arr, int k) {
+
+        for(int i = 0; i < arr.size(); i++) {
+            int missingCount = arr[i] - ( i + 1); 
+
+            if(missingCount >= k) {
+                return i + k;
+            }
+        }
+
+        return arr.size() + k;
     }
 };
