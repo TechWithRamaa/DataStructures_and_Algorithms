e569311cb9d7a18ae6e517ff7fecfb13e114a3b8
Ramaa <rkramadhevi@gmail.com>
2025-04-04 00:25:03 +0530

    Time: 0 ms (100%), Space: 15.8 MB (5.12%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
+    // Key Intuition
+    /*
+       Scan from right → find first drop (nums[i] < nums[i+1]).
+       Find the smallest larger element in the right suffix & swap.
+       Reverse the suffix to get the smallest next permutation.
+    */
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int i = n - 2;
+        int N = nums.size();
+        int i = N - 2;
 
         // Find the first decreasing element
         while (i >= 0 && nums[i] >= nums[i + 1])
             i--;
 
         if (i >= 0) {
-            // Find the next larger element
-            int j = n - 1;
+            int j = N - 1;
+            
+            // Find the smallest largest element for i th element
             while (nums[j] <= nums[i])
                 j--;
 
             // Swap the elements
             swap(nums[i], nums[j]);
         }
-        // Reverse the elements from i+1 to the end
+
+        // Reverse the suffix to get the lexicographically next largest
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
