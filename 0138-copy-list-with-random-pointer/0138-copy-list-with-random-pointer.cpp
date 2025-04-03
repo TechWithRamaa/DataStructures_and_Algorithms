0b831d74be36944bcdbf05f6f8d3aaeb9a17a985
Ramaa <rkramadhevi@gmail.com>
2025-04-03 08:32:25 +0530

    Time: 7 ms (53.38%), Space: 15.1 MB (72.72%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -19,7 +19,7 @@ public:
     // BruteForce - Using Extra Memory
     // Hashtable & Two pass solution
     Node* copyRandomList1(Node* head) {
-        if(!head)
+        if (!head)
             return head;
 
         unordered_map<Node*, Node*> nodeMap;
@@ -27,7 +27,7 @@ public:
         Node* current = head;
 
         // step 1 -> create copy nodes & store in a hashtable
-        while(current) {
+        while (current) {
             Node* copy = new Node(current->val);
             nodeMap[current] = copy;
 
@@ -36,7 +36,7 @@ public:
 
         current = head;
         // step 2 -> connect clone nodes as they re currently disconnected
-        while(current) {
+        while (current) {
             nodeMap[current]->next = nodeMap[current->next];
             nodeMap[current]->random = nodeMap[current->random];
 
@@ -46,19 +46,59 @@ public:
         return nodeMap[head];
     }
 
-    // Optimized - In Memory processing
-    // Interleaving current & copy nodes 
-    // Three pass solution 
     Node* copyRandomList(Node* head) {
-        if(!head) 
+        if (!head)
             return head;
-        
+
+        // Create copy for the original nodes
+        // Interleave copied nodes next to original nodes
+        Node* current = head;
+        while (current) {
+            Node* copy = new Node(current->val);
+            copy->next = current->next;
+            current->next = copy;
+            current = current->next->next;
+        }
+
+        // Link random pointers in the copied nodes
+        // link newly created copied random nodes if original has random
+        current = head;
+        while (current) {
+            if (current->random)
+                current->next->random = current->random->next;
+
+            current = current->next->next;
+        }
+
+        // delink the copied nodes from original nodes
+        current = head;
+        Node* copyHead = head -> next;
+        Node* copy = head -> next;
+        while(current) {
+            current->next = current->next->next;
+            if(copy->next) 
+                copy->next = copy->next->next;
+            
+            current = current->next;
+            copy = copy->next;
+        }
+
+        return copyHead;
+    }
+
+    // Optimized - In Memory processing
+    // Interleaving current & copy nodes
+    // Three pass solution
+    Node* copyRandomList2(Node* head) {
+        if (!head)
+            return head;
+
         Node* current = head;
 
-        // create copy nodes, 
+        // create copy nodes,
         // between original nodes
         // Interleaving process
-        while(current) {
+        while (current) {
             Node* copy = new Node(current->val);
             copy->next = current->next;
             current->next = copy;
@@ -68,24 +108,26 @@ public:
 
         // copy random pointers inside cloned list
         current = head;
-        while(current) {
-            if(current->random) 
-                current->next->random = current->random->next; // clone's random not original's random
-            
+        while (current) {
+            if (current->random)
+                current->next->random =
+                    current->random
+                        ->next; // clone's random not original's random
+
             current = current->next->next;
         }
-        
+
         current = head;
         Node* copyHead = current->next;
         Node* clone = current->next;
-        while(current) {
+        while (current) {
             current->next = current->next->next;
 
             // clone->next will be null before current->next
             // as current is before clone
-            // clone will reach null first 
+            // clone will reach null first
             // so this check only needed for clone
-            if(clone->next)
+            if (clone->next)
                 clone->next = clone->next->next;
 
             current = current->next;
@@ -94,7 +136,6 @@ public:
 
         return copyHead;
     }
-
 
     Node* copyRandomList3(Node* head) {
         if (!head)
