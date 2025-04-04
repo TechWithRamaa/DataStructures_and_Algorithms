becd8379d795cb0ec6fe2a07e8b7cd80fa767670
Ramaa <rkramadhevi@gmail.com>
2025-04-04 10:14:22 +0530

    Time: 0 ms (100%), Space: 10.3 MB (88.05%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,6 +1,18 @@
+/*
+    \U0001f50d Problem Summary:
+    You are given an array nums and an integer k.
+    Split the array into k non-empty subarrays such that the largest sum among these subarrays is minimized.
+    Return that minimized largest sum.
+*/
 class Solution {
 private:
-
+    /*
+    \U0001f4a1 Binary Search Logic:
+     We’re searching for the minimum possible max sum across k splits.
+     If it's possible to split with a max sum of mid, try smaller (right = mid).
+     If not possible, go higher (left = mid + 1).
+     We stop at left == right which gives the minimum possible valid largest sum.
+    */
     bool canSplit(int guessedSum, int k, vector<int>& nums) {
         int splitCount = 1;
         int runningSum = 0;
@@ -20,7 +32,9 @@ private:
     }
 
 public:
-
+    // Guess Game - guess the minimized maximum sum
+    // Binary Search + Prefix Sum + Greedy for paritioning
+    // TC ~ O ( N * log (max sum))
     int splitArray(vector<int>& nums, int k) {
         
         int left = *max_element(nums.begin(), nums.end()); // minimum largest sum
