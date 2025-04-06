21109db08158ec4eb5e1680eae822eba6488e623
Ramaa <rkramadhevi@gmail.com>
2025-04-06 19:50:21 +0530

    Time: 146 ms (26.3%), Space: 146.9 MB (45.26%) - LeetHub

---
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/1701-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -0,0 +1,84 @@
+class DJU {
+private:
+    vector<int> parent, rank;
+    
+public:
+    int componentsCount; // specific to this problem requirement
+
+    DJU(int N) {
+        rank.resize(N + 1, 0);
+        parent.resize(N + 1);
+
+        for (int i = 1; i <= N; i++)
+            parent[i] = i;
+
+        componentsCount = N;
+    }
+
+    int findRoot(int x) {
+        // Path Compression
+        if (parent[x] != x)
+            parent[x] = findRoot(parent[x]);
+
+        return parent[x];
+    }
+
+    bool unite(int x, int y) {
+        int rootX = findRoot(x);
+        int rootY = findRoot(y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY])
+                parent[rootY] = rootX;
+            else if (rank[rootX] < rank[rootY])
+                parent[rootX] = rootY;
+            else {
+                parent[rootX] = rootY;
+                rank[rootY]++;
+            }
+
+            componentsCount--; // 2 nodes are connected, decrement the components count
+        } else
+            return false;
+
+        return true;
+    }
+};
+
+class Solution {
+public:
+    int maxNumEdgesToRemove(int N, vector<vector<int>>& edges) {
+        DJU alice(N), bob(N);
+        int noOfEdgesUsed = 0;
+
+        // Greedy 
+        sort(edges.rbegin(), edges.rend()); // Sort descending by type
+
+        for (auto &edge : edges) {
+            int type = edge[0], u = edge[1], v = edge[2];
+
+            if (type == 3) {
+                bool aliceEdge = alice.unite(u, v);
+                bool bobEdge = bob.unite(u, v);
+
+                if (aliceEdge || bobEdge)
+                    noOfEdgesUsed++;
+            } else if (type == 2) {
+                bool bobEdge = bob.unite(u, v);
+
+                if (bobEdge)
+                    noOfEdgesUsed++;
+            } else if (type == 1) {
+                bool aliceEdge = alice.unite(u, v);
+
+                if (aliceEdge)
+                    noOfEdgesUsed++;
+            }
+        }
+
+        if(alice.componentsCount != 1 || bob.componentsCount != 1)
+            return -1;
+        
+        return edges.size() - noOfEdgesUsed;
+    }
+};
