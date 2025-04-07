4632673cb53f963bbac60abbb34932662b647d11
Ramaa <rkramadhevi@gmail.com>
2025-04-07 06:40:14 +0530

    Time: 120 ms (70.3%), Space: 42.2 MB (48.37%) - LeetHub

---
+++ 0753-open-the-lock/0753-open-the-lock.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+
+        unordered_set<string> blockingSet(deadends.begin(), deadends.end());
+        unordered_set<string> visited;
+
+        if (blockingSet.count("0000"))
+            return -1;
+
+        queue<pair<string, int>> q;
+        q.emplace("0000", 0);
+        visited.insert("0000");
+
+        while (!q.empty()) {
+            auto [current, turns] = q.front();
+            q.pop();
+
+            if (current == target)
+                return turns;
+
+            // Neighbors
+            for (int i = 0; i < 4; i++) {
+                char c = current[i];
+
+                current[i] = (((c - '0') + 1) % 10) + '0';
+
+                if (!visited.count(current) && !blockingSet.count(current)) {
+                    visited.insert(current);
+                    q.emplace(current, turns + 1);
+                }
+
+                current[i] = (((c - '0') + 9) % 10) + '0';
+
+                if (visited.count(current) == 0 &&
+                    blockingSet.count(current) == 0) {
+                    visited.insert(current);
+                    q.emplace(current, turns + 1);
+                }
+
+                current[i] = c;
+            }
+        }
+
+        return -1;
+    }
+};
