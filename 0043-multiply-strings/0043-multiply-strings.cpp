94e751478d4a65d56be9a12af8efce5da651f08b
Ramaa <rkramadhevi@gmail.com>
2025-04-09 13:55:00 +0530

    Time: 0 ms (100%), Space: 9.6 MB (51.78%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0")
+            return "0";
+
+        int M = num1.size(), N = num2.size();
+        vector<int> result(M + N, 0);
+
+        for (int i = M - 1; i >= 0; i--) {
+            for (int j = N - 1; j >= 0; j--) {
+                int d1 = num1[i] - '0';
+                int d2 = num2[j] - '0';
+                int mul = d1 * d2;
+
+                int posLow = i + j + 1;
+                int posHigh = i + j;
+
+                int sum = result[posLow] + mul;
+
+                result[posLow] = sum % 10;
+                result[posHigh] += sum / 10;
+            }
+        }
+
+        string product;
+        bool leadingZero = true;
+
+        for (int digit : result) {
+            if (leadingZero && digit == 0) continue;
+            leadingZero = false;
+            product.push_back(digit + '0');
+        }
+
+        return product.empty() ? "0" : product;
+    }
+};
