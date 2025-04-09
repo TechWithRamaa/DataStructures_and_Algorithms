b09dec682830ea24dc0196d3d074641120d0e5b6
Ramaa <rkramadhevi@gmail.com>
2025-04-09 19:58:41 +0530

    Time: 0 ms (100%), Space: 9 MB (18.54%) - LeetHub

--- 0157-read-n-characters-given-read4/0157-read-n-characters-given-read4.cpp
+++ 0157-read-n-characters-given-read4/0157-read-n-characters-given-read4.cpp
@@ -10,29 +10,30 @@ public:
      * @param n   Number of characters to read
      * @return    The number of actual characters read
      */
-     /*
-     Use read4(buf4) in a loop until you’ve read n characters or until the file ends.
+    /*
+    Use read4(buf4) in a loop until you’ve read n characters or until the file
+    ends.
 
-     Copy the minimum of (n - totalRead, charsReadFromRead4) into buf.
+    Copy the minimum of (n - totalRead, charsReadFromRead4) into buf.
 
-     Return the total number of characters you’ve written to buf.
-     */
-    int read(char *buf, int n) {
+    Return the total number of characters you’ve written to buf.
+    */
+    int read(char* buf, int n) {
         char temp[4];
         int soFarRead = 0;
 
-        while(soFarRead < n) {
+        while (soFarRead < n) {
             int count = read4(temp);
             int toCopy = min(count, n - soFarRead);
 
-            for(int i = 0; i < toCopy; i++) {
-                buf[soFarRead++] = temp[i]; 
+            for (int i = 0; i < toCopy; i++) {
+                buf[soFarRead++] = temp[i];
             }
 
-            if(count < 4) 
+            if (count < 4)
                 break;
         }
 
-         return soFarRead;
+        return soFarRead;
     }
 };
