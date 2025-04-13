7235a37ed4000583fcb7af781a0d1384941f60a4
Ramaa <rkramadhevi@gmail.com>
2025-04-13 20:47:39 +0530

    Time: 7 ms (41.65%), Space: 15.3 MB (18.23%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+
+        sort(nums.begin(), nums.end());
+        permuteUnique(result, nums, 0);
+
+        return result;
+    }
+
+private:
+    void permuteUnique(vector<vector<int>>& result,
+                       vector<int>& nums, int start) {
+        if(start == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+
+        unordered_set<int> used;
+
+        for (int i = start; i < nums.size(); i++) {
+            if(used.count(nums[i]))
+                continue;
+
+            used.insert(nums[i]);
+            swap(nums[i], nums[start]); // make a choice
+            
+            permuteUnique(result, nums, start + 1); // explore
+            
+            swap(nums[i], nums[start]); // undo
+        }
+    }
+};
