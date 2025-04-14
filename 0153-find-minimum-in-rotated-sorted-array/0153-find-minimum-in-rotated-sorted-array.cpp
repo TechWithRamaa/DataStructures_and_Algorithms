2f570f499a35f6cf6045402f7659b0553052f83c
Ramaa <rkramadhevi@gmail.com>
2025-04-14 20:27:30 +0530

    Time: 0 ms (100%), Space: 14.1 MB (48.52%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
+    // the minimum element is the pivot of rotation
+    // we need to look for the pivot in the unsorted half
     int findMin(vector<int>& nums) {
         int left = 0;
         int right = nums.size() - 1;
 
         // plz note down the condition used in while & last statement inside else block
         while (left < right) {
+            // when the array is already sorted
             if (nums[left] < nums[right]) {
                 return nums[left];
             }
