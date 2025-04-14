1382d75f54438afcaab11460058cc613253778e4
Ramaa <rkramadhevi@gmail.com>
2025-04-14 23:29:56 +0530

    Time: 1 ms (49.61%), Space: 13.9 MB (90.75%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -4,7 +4,7 @@ public:
     // Approach 2 - Set + Set
     // Approach 3 - Sorting + Two Pointers 
     // Approach 4 - Binary Search + Set
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> intersection2(vector<int>& nums1, vector<int>& nums2) {
         // Approach 2 
         unordered_set<int> set1(nums1.begin(), nums1.end());
         unordered_set<int> result;
@@ -17,5 +17,31 @@ public:
 
         vector<int> resultList(result.begin(), result.end());
         return resultList;
+    }
+
+    // Approach 3
+    // Merge 2 sorted lists but only iff elements intersect
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> result;
+
+        int i = 0, j = 0;
+        while(i < nums1.size() && j < nums2.size()) {
+            if(nums1[i] == nums2[j]) {
+                if(result.empty() || result.back() != nums1[i]) {
+                    result.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return result;
     }
 };
