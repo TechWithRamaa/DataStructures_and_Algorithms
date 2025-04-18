5eda1fe3f3ce4e7d747c3b7a60369ebee6f4a4e4
Ramaa <rkramadhevi@gmail.com>
2025-04-18 09:52:31 +0530

    Time: 5 ms (41.53%), Space: 10 MB (53.06%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if (n == 1) {
+        if(n == 1) 
             return "1";
-        }
         
-        string prevTerm = countAndSay(n - 1);
+        string previousTerm = countAndSay(n-1);
         
         string currentTerm = "";
         int count = 1;
         
-        for (int i = 1; i < prevTerm.length(); i++) {
-            if (prevTerm[i] == prevTerm[i - 1]) {
-                count++;
+        for(int i = 1; i < previousTerm.length(); i++) {
+            if(previousTerm[i] == previousTerm[i-1]) {
+                count += 1;
             } else {
-                currentTerm += to_string(count) + prevTerm[i - 1];
+                currentTerm += to_string(count) + previousTerm[i - 1];
                 count = 1;
-            }
-        }
-        
-        currentTerm += to_string(count) + prevTerm[prevTerm.length() - 1];
-        
+            }          
+         }
+         
+        currentTerm += to_string(count) + previousTerm[previousTerm.length() - 1];
+            
         return currentTerm;
     }
 };
+
+
+
+// n = 4    gets "21" .. produces "1211"
+    // n = 3    gets "11" .. produces "21"
+        // n = 2   gets "1" .. produces "11"
+            // n = 1 returns "1"
