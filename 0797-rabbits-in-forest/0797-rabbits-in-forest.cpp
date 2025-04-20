1e22b83f57a5304aeae49184526ced415c21888b
Ramaa <rkramadhevi@gmail.com>
2025-04-21 00:15:19 +0530

    Time: 0 ms (100%), Space: 12.3 MB (42.46%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int numRabbits(vector<int>& answers) {
-        unordered_map<int, int> freqMap;
-        for (int ans : answers)
-            freqMap[ans]++;
+        unordered_map<int, int>
+            answerFrequency; // answer -> number of rabbits giving this answer
 
-        int res = 0;
-        for (auto& [x, freq] : freqMap) {
-            int groupSize = x + 1;
-            int groupCount = (freq + x) / groupSize; // ceil(freq / groupSize)
-            res += groupCount * groupSize;
+        for (int answer : answers)
+            answerFrequency[answer]++;
+
+        int totalRabbits = 0;
+
+        for (auto& [reportedSameColorCount, rabbitCount] : answerFrequency) {
+            int groupSize = reportedSameColorCount +  1; // Each rabbit implies a group of this size
+            int numberOfGroups = ceil((double)rabbitCount /groupSize); // How many such groups are needed
+            totalRabbits += numberOfGroups * groupSize; // Each group contributes groupSize rabbits
         }
-        return res;
+
+        return totalRabbits;
     }
 };
