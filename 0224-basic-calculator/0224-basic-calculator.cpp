a48dbb8f14d389bce45ad0f92bc04e5da98c61bf
Ramaa <rkramadhevi@gmail.com>
2025-04-20 17:06:55 +0530

    Time: 0 ms (100%), Space: 10.6 MB (56.74%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,33 +1,46 @@
 class Solution {
 public:
-    // Stack-Based Evaluation (Sign Stack Method)
-    // only signs are added into stack
-    
+    /*
+    Use a sign stack when the effect of a + or - operator 
+    can cascade through nested parentheses, and 
+    each scope needs to preserve its own sign context.
+    */
     int calculate(string s) {
-        int sign = 1;
-        stack<int> st;
-        st.push(sign);
-
+        stack<int> signStack;
         int result = 0;
-        int number = 0;
+        int sign = 1;  // +1 by default
+        int i = 0;
 
-        for (char c : s) {
+        signStack.push(1);  // global sign context
+
+        while (i < s.size()) {
+            char c = s[i];
+
             if (isdigit(c)) {
-                number = number * 10 + (c - '0');
-            } else if (!isdigit(c)) {
-                if (c == '+' || c == '-') {
-                    result += number * sign;
-                    sign = (c == '+') ? st.top() : -(st.top());
-                    number = 0;
-                } else if (c == '(') {
-                    st.push(sign);
-                } else if (c == ')') {
-                    st.pop();
+                int num = 0;
+                while (i < s.size() && isdigit(s[i])) {
+                    num = num * 10 + (s[i] - '0');
+                    i++;
                 }
+                result += sign * num;
+                continue;  // already moved `i` forward in the number loop
             }
+            else if (c == '+') {
+                sign = signStack.top();  // use current context sign
+            }
+            else if (c == '-') {
+                sign = -signStack.top();  // flip sign in current context
+            }
+            else if (c == '(') {
+                signStack.push(sign);  // push current sign to apply inside parens
+            }
+            else if (c == ')') {
+                signStack.pop();  // pop the sign context
+            }
+
+            i++;
         }
 
-        result += number * sign;
         return result;
     }
 };
