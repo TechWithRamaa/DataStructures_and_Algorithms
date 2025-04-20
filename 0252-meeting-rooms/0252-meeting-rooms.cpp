5243b9b67059e349c07c7a5413dae3d06ca9254f
Ramaa <rkramadhevi@gmail.com>
2025-04-20 17:09:18 +0530

    Time: 0 ms (100%), Space: 16 MB (50.63%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -4,6 +4,7 @@ public:
         sort(intervals.begin(), intervals.end());
 
         for(int i = 1; i < intervals.size(); i++) {
+            // start time of i th meeting is less than ending of the previous meeting
             if(intervals[i][0] < intervals[i-1][1]) {
                 return false;
             }
