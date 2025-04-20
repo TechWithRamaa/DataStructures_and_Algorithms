d03f7c8f755bc1caf8fedcc7e26e3196523a4daf
Ramaa <rkramadhevi@gmail.com>
2025-04-20 20:04:46 +0530

    Time: 19 ms (77.31%), Space: 15.2 MB (84.24%) - LeetHub

---
+++ 0761-employee-free-time/0761-employee-free-time.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for an Interval.
+class Interval {
+public:
+    int start;
+    int end;
+
+    Interval() {}
+
+    Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<Interval> employeeFreeTime(vector<vector<Interval>>& schedule) {
+        vector<Interval> allIntervals;
+
+        // 1️⃣ Flatten all employee schedules into one list
+        for (auto& empSchedule : schedule) {
+            for (auto& interval : empSchedule) {
+                allIntervals.push_back(interval);
+            }
+        }
+
+        // 2️⃣ Sort intervals based on start time
+        sort(allIntervals.begin(), allIntervals.end(), [](Interval& a, Interval& b) {
+            return a.start < b.start;
+        });
+
+        vector<Interval> result;
+
+        // 3️⃣ Start tracking the end of merged intervals
+        int prevEnd = allIntervals[0].end;
+
+        for (int i = 1; i < allIntervals.size(); ++i) {
+            // 4️⃣ If there's a gap, it's a free time
+            if (allIntervals[i].start > prevEnd) {
+                result.push_back(Interval(prevEnd, allIntervals[i].start));
+            }
+
+            // 5️⃣ Merge intervals by updating prevEnd to max
+            prevEnd = max(prevEnd, allIntervals[i].end);
+        }
+
+        return result;
+    }
+};
