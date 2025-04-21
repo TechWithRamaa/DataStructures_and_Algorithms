09f500e318e3cc6eebebf615448227f8897ff286
Ramaa <rkramadhevi@gmail.com>
2025-04-21 09:40:50 +0530

    Time: 12 ms (70.19%), Space: 13.3 MB (58.97%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
+    // This is a classic “shortest-paths + all-paths” combo.
+    // BFS + Backtracking + Hash Table + String
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string, int> depthMap;
+       
         vector<vector<string>> ans;
         
         // BFS to find the shortest path
         unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        
         queue<string> q;
         q.push(beginWord);
+        
+        unordered_map<string, int> depthMap;
         depthMap[beginWord] = 1;
+        
         wordSet.erase(beginWord);
         
         while (!q.empty()) {
             string word = q.front();
             q.pop();
+
             int steps = depthMap[word];
+
             if (word == endWord) break;
+            
             for (int i = 0; i < word.size(); ++i) {
                 char original = word[i];
+            
                 for (char ch = 'a'; ch <= 'z'; ++ch) {
                     word[i] = ch;
+            
                     if (wordSet.count(word)) {
                         q.push(word);
-                        wordSet.erase(word);
                         depthMap[word] = steps + 1;
+
+                        wordSet.erase(word);
                     }
                 }
+
                 word[i] = original;
             }
         }
@@ -49,16 +62,22 @@ private:
         }
         
         int steps = depthMap[word];
+
         for (int i = 0; i < word.size(); ++i) {
             char original = word[i];
+        
             for (char ch = 'a'; ch <= 'z'; ++ch) {
                 word[i] = ch;
+        
                 if (depthMap.count(word) && depthMap[word] + 1 == steps) {
                     seq.push_back(word);
+        
                     dfs(word, beginWord, seq, depthMap, ans);
+                    
                     seq.pop_back();
                 }
             }
+            
             word[i] = original;
         }
     }
