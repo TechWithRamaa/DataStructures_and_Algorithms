2ac2c437a3494a84f11aa2da0d113f1e80f4b9b9
Ramaa <rkramadhevi@gmail.com>
2025-04-21 23:32:59 +0530

    Time: 4 ms (47.81%), Space: 17.7 MB (73.05%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,72 +1,66 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-    vector<vector<pair<int, int>>> graph(n);
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+                          int k) {
+        vector<vector<pair<int, int>>> graph(n);
 
-    for (auto& flight : flights) {
-        graph[flight[0]].emplace_back(flight[1], flight[2]);
-    }
+        for (auto& flight : flights) {
+            graph[flight[0]].emplace_back(flight[1], flight[2]);
+        }
 
-    queue<tuple<int, int, int>> q; // {node, cost so far, stops so far}
-    q.emplace(src, 0, 0);
+        queue<tuple<int, int, int>> q; // {node, cost so far, stops so far}
+        q.emplace(src, 0, 0);
 
-    vector<int> minCost(n, INT_MAX);
-    minCost[src] = 0;
+        vector<int> minCost(n, INT_MAX);
+        minCost[src] = 0;
 
-    while (!q.empty()) {
-        auto [node, cost, stops] = q.front();
-        q.pop();
+        while (!q.empty()) {
+            auto [node, cost, stops] = q.front();
+            q.pop();
 
-        if (stops > k) continue;
+            if (stops > k)
+                continue;
 
-        for (auto& [neighbor, price] : graph[node]) {
-            int newCost = cost + price;
+            for (auto& [neighbor, price] : graph[node]) {
+                int newCost = cost + price;
 
-            // If the new cost is cheaper, or we still have stops left to explore
-            if (newCost < minCost[neighbor]) {
-                minCost[neighbor] = newCost;
-                q.emplace(neighbor, newCost, stops + 1);
+                // If the new cost is cheaper, or we still have stops left to
+                // explore
+                if (newCost < minCost[neighbor]) {
+                    minCost[neighbor] = newCost;
+                    q.emplace(neighbor, newCost, stops + 1);
+                }
             }
         }
+
+        return minCost[dst] == INT_MAX ? -1 : minCost[dst];
     }
 
-    return minCost[dst] == INT_MAX ? -1 : minCost[dst];
-}
-
-
     // Djikshtra's algorithm
-    int findCheapestPriceOption2(int n, vector<vector<int>>& flights, int src,
-                                 int dst, int k) {
-        unordered_map<int, vector<pair<int, int>>> graph;
-
-        for (auto& entry : flights) {
-            graph[entry[0]].push_back({entry[1], entry[2]});
+    int findCheapestPrice2(int n, vector<vector<int>>& flights, int src, int dst,
+                          int k) {
+        vector<vector<pair<int, int>>> graph(n);
+        for (auto& f : flights) {
+            graph[f[0]].emplace_back(f[1], f[2]);
         }
 
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>
+        // {cost so far, node, stops so far}
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
+                       greater<>>
             pq;
-        pq.push({0, src, k + 1});
-
-        vector<vector<int>> dist(n, vector<int>(k + 2, INT_MAX));
-        dist[src][k + 1] = 0;
+        pq.emplace(0, src, 0);
 
         while (!pq.empty()) {
-            auto top = pq.top();
-            int cost = top[0], node = top[1], stopsLeft = top[2];
+            auto [cost, node, stops] = pq.top();
             pq.pop();
 
             if (node == dst)
                 return cost;
+            if (stops > k)
+                continue;
 
-            if (stopsLeft > 0) {
-                for (auto [neighbor, price] : graph[node]) {
-                    int newCost = cost + price;
-
-                    if (newCost < dist[neighbor][stopsLeft - 1]) {
-                        dist[neighbor][stopsLeft - 1] = newCost;
-                        pq.push({newCost, neighbor, stopsLeft - 1});
-                    }
-                }
+            for (auto& [nei, price] : graph[node]) {
+                pq.emplace(cost + price, nei, stops + 1);
             }
         }
 
