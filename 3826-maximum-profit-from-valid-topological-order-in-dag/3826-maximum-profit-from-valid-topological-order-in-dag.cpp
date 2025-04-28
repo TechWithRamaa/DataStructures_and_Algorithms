e44de85b87fb475c59d811a29254bcb3737e1b1b
Ramaa <rkramadhevi@gmail.com>
2025-04-28 09:26:38 +0530

    Time: 1038 ms (8.33%), Space: 204.6 MB (16.67%) - LeetHub

--- 3826-maximum-profit-from-valid-topological-order-in-dag/3826-maximum-profit-from-valid-topological-order-in-dag.cpp
+++ 3826-maximum-profit-from-valid-topological-order-in-dag/3826-maximum-profit-from-valid-topological-order-in-dag.cpp
@@ -1,24 +1,45 @@
 class Solution {
-public:
-    int maxProfit(int n, vector<vector<int>>& edges, vector<int>& score) {
-        vector<int> need(n, 0);
-        for (auto& e : edges) {
-            int i = e[0], j = e[1];
-            need[j] |= (1 << i);
-        }
-        vector<int> dp(1 << n, -1);
-        dp[0] = 0;
-        for (int mask = 0; mask < (1 << n); ++mask) {
-            if (dp[mask] == -1)
-                continue;
-            int pos = __builtin_popcount(mask) + 1;
-            for (int i = 0; i < n; ++i) {
-                if (((mask >> i) & 1) == 0 && (mask & need[i]) == need[i]) {
-                    int mask2 = mask | (1 << i);
-                    dp[mask2] = max(dp[mask2], dp[mask] + score[i] * pos);
+private:
+    int helper(int mask, int pos, vector<int>& inDegree, vector<vector<int>>& adj, vector<int>& score, vector<int>& dp, int n) {
+        // all nodes processed
+        if (mask == (1 << n) - 1) 
+            return 0; 
+
+        if (dp[mask] != -1) return dp[mask];
+        
+        int res = 0;
+        
+        for (int i = 0; i < n; i++) {
+            // if node i not picked yet and indegree 0
+            if ((mask & (1 << i)) == 0 && inDegree[i] == 0) {
+                for (int ng : adj[i]) {
+                    inDegree[ng]--;
+                }
+                
+                int val = pos * score[i] + helper(mask | (1 << i), pos + 1, inDegree, adj, score, dp, n);
+                res = max(res, val);
+                
+                for (int ng : adj[i]) {
+                    inDegree[ng]++;
                 }
             }
         }
-        return dp[(1 << n) - 1];
+        
+        return dp[mask] = res;
+    }
+    
+public:
+    int maxProfit(int n, vector<vector<int>>& edges, vector<int>& score) {
+        vector<vector<int>> adj(n);
+        vector<int> inDegree(n, 0);
+        
+        for (auto& e : edges) {
+            adj[e[0]].push_back(e[1]);
+            inDegree[e[1]]++;
+        }
+        
+        vector<int> dp(1 << n, -1);
+        
+        return helper(0, 1, inDegree, adj, score, dp, n);
     }
 };
