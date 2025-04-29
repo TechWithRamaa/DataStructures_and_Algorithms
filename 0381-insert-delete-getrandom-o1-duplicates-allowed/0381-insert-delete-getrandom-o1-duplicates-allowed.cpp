0c7834931da504359e95c69a19bb14b76e5abd14
Ramaa <rkramadhevi@gmail.com>
2025-04-30 00:57:04 +0530

    Time: 56 ms (35.82%), Space: 112.7 MB (48.63%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,32 +1,62 @@
 class RandomizedCollection {
 public:
-    map <int, set<int>> m;                          //used to map element with the indices where it is present in vector v
-    vector <int> elements;                                 //used to store all the elements (contains duplicate as well)
-    RandomizedCollection() {}                       //constructor (no such initialisation needed)
-    
+    map<int, set<int>> m; // used to map element with the indices where it is
+                          // present in vector v
+    vector<int>
+        elements; // used to store all the elements (contains duplicate as well)
+
+    RandomizedCollection() {}
+
     bool insert(int val) {
-        elements.push_back(val);                           //inserting element into vector
-        m[val].insert(elements.size()-1);                  //insering the index of newly inserted element into map 
-        return m[val].size() == 1;                  
+        elements.push_back(val);
+        m[val].insert(elements.size() - 1);
+
+        return m[val].size() == 1;
     }
-    
+
     bool remove(int val) {
-        auto it = m.find(val);                      //used to find if element is present in map
-        if(it != m.end()){                          //if it is present,
-            auto pos = *it->second.begin();         //pos is an integer and not pointer
-            it->second.erase(it->second.begin());   //erases integer from the set stored in map corresponding to the element val
-            elements[pos] = elements.back();                      //element from the back is brought at the index pos
-            m[elements.back()].insert(pos);                //new index of that element is stored in map
-            m[elements.back()].erase(elements.size()-1);          //erasing the old index from the map
-            elements.pop_back();                           //erasing the element from vector 
-            if (it->second.size() == 0)             //if no indices are present in map corresponding to val in map, it means it was present only once 
-                m.erase(it);                        //so erase from map itself
-            return true;
+        // Step 1: Check if val exists in the map
+        if (m.find(val) == m.end()) {
+            return false; // Nothing to remove
         }
-        return false;
+
+        // Step 2: Get an index of 'val' from its set of indices
+        int indexToRemove = *m[val].begin();
+
+        // Step 3: Get the last element in the vector
+        int lastElement = elements.back();
+        int lastIndex = elements.size() - 1;
+
+        // Step 4: Overwrite the element at indexToRemove with lastElement
+        elements[indexToRemove] = lastElement;
+
+        // Step 5: Update the map
+        // Remove indexToRemove from val's index set
+        m[val].erase(indexToRemove);
+
+        // Update lastElement's index set:
+        // Remove the old index (lastIndex) and insert the new one
+        // (indexToRemove)
+        m[lastElement].erase(lastIndex);
+        if (indexToRemove != lastIndex) {
+            m[lastElement].insert(indexToRemove);
+        }
+
+        // Step 6: Remove the last element from the vector
+        elements.pop_back();
+
+        // Step 7: If val has no more indices left, clean it up from the map
+        if (m[val].empty()) {
+            m.erase(val);
+        }
+
+        return true;
     }
-    
+
     int getRandom() {
-        return elements[rand() % elements.size()];                //rand() is used to get a random number and it is % by v.size() so that the result is within the size of vector v
+        return elements
+            [rand() % elements.size()]; // rand() is used to get a random number
+                                        // and it is % by v.size() so that the
+                                        // result is within the size of vector v
     }
 };
