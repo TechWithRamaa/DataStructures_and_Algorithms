25507fce1179dedea1a873faab5a9ac7f37727ab
Ramaa <rkramadhevi@gmail.com>
2025-05-10 15:00:16 +0530

    Time: 6 ms (87.4%), Space: 42.6 MB (56.06%) - LeetHub

--- 1316-fizz-buzz-multithreaded/1316-fizz-buzz-multithreaded.java
+++ 1316-fizz-buzz-multithreaded/1316-fizz-buzz-multithreaded.java
@@ -6,40 +6,60 @@ class FizzBuzz {
         this.n = n;
     }
 
+    // printFizz.run() outputs "fizz".
     public synchronized void fizz(Runnable printFizz) throws InterruptedException {
         while (i <= n) {
-            while (i <= n && (i % 3 != 0 || i % 5 == 0)) wait();
-            if (i > n) break;
+            while (i <= n && (i % 3 != 0 || i % 5 == 0))
+                wait();
+
+            if (i > n)
+                break;
+
             printFizz.run();
             i++;
             notifyAll();
         }
     }
 
+    // printBuzz.run() outputs "buzz".
     public synchronized void buzz(Runnable printBuzz) throws InterruptedException {
         while (i <= n) {
-            while (i <= n && (i % 5 != 0 || i % 3 == 0)) wait();
-            if (i > n) break;
+            while (i <= n && (i % 5 != 0 || i % 3 == 0))
+                wait();
+
+            if (i > n)
+                break;
+
             printBuzz.run();
             i++;
             notifyAll();
         }
     }
 
+    // printFizzBuzz.run() outputs "fizzbuzz".
     public synchronized void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
         while (i <= n) {
-            while (i <= n && (i % 15 != 0)) wait();
-            if (i > n) break;
+            while (i <= n && (i % 5 != 0 || i % 3 != 0))
+                wait();
+
+            if (i > n)
+                break;
+
             printFizzBuzz.run();
             i++;
             notifyAll();
         }
     }
 
+    // printNumber.accept(x) outputs "x", where x is an integer.
     public synchronized void number(IntConsumer printNumber) throws InterruptedException {
         while (i <= n) {
-            while (i <= n && (i % 3 == 0 || i % 5 == 0)) wait();
-            if (i > n) break;
+            while (i <= n && (i % 3 == 0 || i % 5 == 0)) 
+                wait();
+
+            if (i > n) 
+                break;
+
             printNumber.accept(i);
             i++;
             notifyAll();
