be791f622f4d7a64b85cfd58bc4aa79e7cbe2298
Ramaa <rkramadhevi@gmail.com>
2025-05-11 20:58:48 +0530

    Time: 0 ms (100%), Space: 12 MB (20.63%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool threeConsecutiveOdds(vector<int>& arr) {
+    // Approach 1
+    bool threeConsecutiveOdds1(vector<int>& arr) {
         int countOfOdds = 0;
 
         for(auto num : arr) {
@@ -16,6 +17,19 @@ public:
             countOfOdds = 0; // reset to zero
         }
 
+        return false;
+    }
+
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        // Loop through the array up to the third-to-last element
+        for (int i = 0; i < n - 2; i++) {
+            // Check if the current element and the next two elements are all
+            // odd
+            if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1) {
+                return true;
+            }
+        }
         return false;
     }
 };
