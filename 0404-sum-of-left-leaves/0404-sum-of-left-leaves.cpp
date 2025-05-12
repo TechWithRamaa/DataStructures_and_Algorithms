4ba6eb2f05181f77402c88d8570a0909c68518f1
Ramaa <rkramadhevi@gmail.com>
2025-05-12 09:24:22 +0530

    Time: 2 ms (2.53%), Space: 16.4 MB (5.04%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -15,21 +15,16 @@ public:
         if(!root)
             return 0;
         
-        if(isLeaf(root))
-            return 0;
-
         int leftSum = 0;
 
         if(root->left && isLeaf(root->left)) {
-            if(root->right && isLeaf(root->right))
                 leftSum = root->left->val;
-            else 
-                leftSum = root->left->val + sumOfLeftLeaves(root->right);
-            
-            return leftSum;
         }
 
-        return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+        leftSum += sumOfLeftLeaves(root->left);
+        leftSum += sumOfLeftLeaves(root->right);
+
+        return leftSum;
     }
 
 private:
