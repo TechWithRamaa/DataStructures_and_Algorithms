e70ab2f8723d6300c74b52d20443cbbd06529ca4
Ramaa <rkramadhevi@gmail.com>
2025-05-17 15:36:25 +0530

    Time: 2 ms (58.22%), Space: 9 MB (60.52%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,27 +1,60 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
+        int M = s.length(), N = p.length();
+        vector<vector<bool>> dp(M + 1, vector<bool>(N + 1, false));
+        dp[0][0] = true;
+
+        // Initialize first row (empty string s)
+        for (int j = 2; j <= N; j++) {
+            if (p[j - 1] == '*')
+                dp[0][j] = dp[0][j - 2]; // '*' cancels the char before it
+        }
+
+        for (int i = 1; i <= M; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2]; // zero occurrence
+
+                    // check one or more only if preceding char matches
+                    if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[M][N];
+    }
+
+    // Recursion + Memoization
+    bool isMatch1(string s, string p) {
         vector<vector<int>> dp(s.size() + 1, vector<int>(p.size() + 1, -1));
         return isMatch(s, p, 0, 0, dp);
     }
+
 private:
-    bool isMatch(string &s, string &p, int i, int j, vector<vector<int>>& dp) {
+    bool isMatch(string& s, string& p, int i, int j, vector<vector<int>>& dp) {
         // when pattern is completely matched, string should be exhausted
-        if(j == p.size())
+        if (j == p.size())
             return i == s.size();
-        
-        if(dp[i][j] != -1)
+
+        if (dp[i][j] != -1)
             return dp[i][j];
-    
+
         bool firstMatch = false;
-        if((i < s.size() && (s[i] == p[j] || p[j] == '.')))
+        if ((i < s.size() && (s[i] == p[j] || p[j] == '.')))
             firstMatch = true;
 
-        if(((j + 1) < p.size()) && p[j + 1] == '*') 
-            dp[i][j] = isMatch(s, p, i, j + 2, dp) || (firstMatch && isMatch(s, p, i + 1, j, dp));
-        else 
+        if (((j + 1) < p.size()) && p[j + 1] == '*')
+            dp[i][j] = isMatch(s, p, i, j + 2, dp) ||
+                       (firstMatch && isMatch(s, p, i + 1, j, dp));
+        else
             dp[i][j] = firstMatch && isMatch(s, p, i + 1, j + 1, dp);
 
-         return dp[i][j];
+        return dp[i][j];
     }
 };
