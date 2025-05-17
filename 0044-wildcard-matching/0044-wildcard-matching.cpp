be68d695b30d332b4401431fc922daee17700e3d
Ramaa <rkramadhevi@gmail.com>
2025-05-17 15:18:03 +0530

    Time: 32 ms (56.07%), Space: 14.9 MB (62.61%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -16,11 +16,10 @@ public:
 
         for (int i = 1; i <= M; ++i) {
             for (int j = 1; j <= N; ++j) {
-                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '?')
                     dp[i][j] = dp[i - 1][j - 1];
-                } else if (p[j - 1] == '*') {
+                else if (p[j - 1] == '*')
                     dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
-                }
             }
         }
 
