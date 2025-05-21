0f393a272e0f0a69576ee5b8646168e7d0642f91
Ramaa <rkramadhevi@gmail.com>
2025-05-22 01:04:25 +0530

    Time: 13 ms (39.42%), Space: 35.5 MB (88.44%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -10,6 +10,9 @@ public:
         for(auto num : nums) {
             runningSum += num;
 
+            // Handles negative values safely
+            // Always returns a number in [0, k-1]
+            // Common trick used in hashing, prefix mod, and circular array problems.
             int mod = (runningSum % k + k) % k;
             if(modMap.count(mod))
                 result += modMap[mod];
