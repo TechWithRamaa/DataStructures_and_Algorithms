f0b61b8f6978841d306a89664b2e06c50797019f
Ramaa <rkramadhevi@gmail.com>
2025-05-22 23:55:38 +0530

    Time: 82 ms (62.92%), Space: 89 MB (29.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,10 +5,14 @@ public:
         int longestSeq = 0;
 
         for(int element: seen) {
+            // previous element should not be there
+            // then current element is the start of the sequence
             if(!seen.count(element - 1)) {
                 int currentLong = 1;
                 int currentElement = element;
 
+                // next element should be there
+                // extend the sequence as long as possible
                 while(seen.count(currentElement + 1)) {
                     currentLong += 1;
                     currentElement += 1;
