f3602621fa0b822b8e6a67ce3e32dca93f2d6aaf
Ramaa <rkramadhevi@gmail.com>
2025-05-24 15:56:21 +0530

    Time: 217 ms (26.42%), Space: 132.1 MB (26.92%) - LeetHub

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> graph(n);
+        unordered_set<long long> directions;
+
+        for(auto connection : connections) {
+            int u = connection[0], v = connection[1];
+
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+
+            directions.insert((long long) u * n + v);
+        }
+
+        return dfs(graph, directions, 0, -1, n);
+    }
+
+private:
+    int dfs(vector<vector<int>>& graph, unordered_set<long long>& directions, int node, int parent, int n) {
+        int changes = 0;
+
+        for(int neighbor : graph[node]) {
+            if(neighbor == parent)
+                continue;
+
+            // If original edge is from node to neighbor, it needs to be reversed
+            if(directions.count((long long) node * n + neighbor)) {
+                changes++;
+            }
+
+            changes += dfs(graph, directions, neighbor, node, n);
+        }
+
+        return changes;
+    }
+};
