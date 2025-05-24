20e4c5be03fb9d87b130bf43c970a7359eb77966
Ramaa <rkramadhevi@gmail.com>
2025-05-24 14:43:33 +0530

    Time: 0 ms (100%), Space: 8.4 MB (49.02%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int lates = 0, absents = 0;
+
+        for(auto ch : s) {
+            if(ch == 'A') {
+                absents++;
+            }
+
+            if(ch == 'L') {
+                lates++;
+            } else {
+                lates = 0;
+            }
+
+            if(absents >= 2 || lates >= 3)
+                return false;
+
+        }
+
+        return true;
+    }
+};
