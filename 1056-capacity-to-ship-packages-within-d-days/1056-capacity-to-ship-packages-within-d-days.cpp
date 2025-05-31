f6088adf29b53f0fc7e7f79452b9c328127e3ec2
Ramaa <rkramadhevi@gmail.com>
2025-05-31 23:41:58 +0530

    Time: 8 ms (84.87%), Space: 35 MB (34.67%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int totalWeight = 0, maxCapacity = 0;
+
+        for(auto weight : weights) {
+            totalWeight += weight;
+            maxCapacity = max(maxCapacity, weight);
+        }
+
+        int left = maxCapacity, right = totalWeight;
+
+        while(left < right) {
+            int mid = left + ((right - left) / 2);
+            
+            if(isFeasible(weights, days, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+
+        }
+        
+        return left;
+    }
+
+private:
+    bool isFeasible(vector<int>& weights, const int DAYS, const int CAPACITY) {
+        int currentCapacity = 0, predictedDays = 1;
+
+        for(auto weight : weights) {
+            currentCapacity += weight;
+
+            if(currentCapacity > CAPACITY) {
+                predictedDays++;
+                currentCapacity = weight; 
+            }
+        }
+
+        return predictedDays <= DAYS;
+    }
+};
