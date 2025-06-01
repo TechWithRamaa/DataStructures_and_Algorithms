6ce619d4b2561b9aa0197720d86bd967703e7c26
Ramaa <rkramadhevi@gmail.com>
2025-06-01 13:20:32 +0530

    Time: 0 ms (100%), Space: 19.1 MB (44.9%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -6,39 +6,47 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     // Post Order DFS Traversal
+    // Compute bottom-up values
     int maxDepthR(TreeNode* root) {
-        if(!root) 
+        if (!root)
             return 0;
-        
+
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 
     int maxDepth(TreeNode* root) {
-    if (!root) return 0;
-    
-    queue<TreeNode*> q;
-    q.push(root);
-    int depth = 0;
-    
-    while (!q.empty()) {
-        int levelSize = q.size(); // number of nodes in current level
-        for (int i = 0; i < levelSize; ++i) {
-            TreeNode* node = q.front();
-            q.pop();
-            if (node->left) q.push(node->left);
-            if (node->right) q.push(node->right);
+        if (!root)
+            return 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        int depth = 0;
+
+        while (!q.empty()) {
+            int levelSize = q.size(); // number of nodes in current level
+
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node->left)
+                    q.push(node->left);
+
+                if (node->right)
+                    q.push(node->right);
+            }
+
+            depth++; // increase depth after processing a full level
         }
-        depth++; // increase depth after processing a full level
+
+        return depth;
     }
-    
-    return depth;
-}
-
 };
-
