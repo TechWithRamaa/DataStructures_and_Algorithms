44e302c03f25ba757c0e7f4b335a8eae72a3fe8d
Ramaa <rkramadhevi@gmail.com>
2025-06-01 17:30:57 +0530

    Time: 5 ms (99.31%), Space: 24.3 MB (93.08%) - LeetHub

---
+++ 1177-tree-diameter/1177-tree-diameter.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int treeDiameter(vector<vector<int>>& edges) {
+        const int NODES = edges.size() + 1;
+
+        vector<vector<int>> graph(NODES);
+
+        for(auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        int diameter = 0;
+        dfs(0, -1, graph, diameter);
+
+        return diameter;
+    }
+
+private:
+    int dfs(int node, int parent, vector<vector<int>>& graph, int& diameter) {
+        int max1 = 0, max2 = 0;
+
+        for(auto neighbor : graph[node]) {
+            if(neighbor == parent)
+                continue;
+            
+            int height = dfs(neighbor, node, graph, diameter);
+
+            if(height > max1) {
+                max2 = max1;
+                max1 = height;
+            } else if (height > max2) {
+                max2 = height;
+            }
+
+        }
+
+        diameter = max(diameter, max1 + max2);
+
+        return 1 + max(max1, max2);
+    }
+};
