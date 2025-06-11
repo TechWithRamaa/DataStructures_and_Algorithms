134a8baa310747248aa80a84fc83d11c59069e01
Ramaa <rkramadhevi@gmail.com>
2025-06-12 01:08:48 +0530

    Time: 7 ms (76.19%), Space: 17.1 MB (79.37%) - LeetHub

---
+++ 0788-minimize-max-distance-to-gas-station/0788-minimize-max-distance-to-gas-station.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    double minmaxGasDist(vector<int>& stations, int k) {
+        double low = 0.0, high = stations.back() - stations.front();
+        double epsilon = 1e-6;
+
+        while (high - low > epsilon) {
+
+            double mid = (low + high) / 2.0;
+            
+            if (isPossible(stations, k, mid))
+                high = mid;
+            else
+                low = mid;
+        }
+
+        return low;
+    }
+private:
+    bool isPossible(vector<int>& stations, const int k, double maxDist) {
+        int needed = 0;
+
+        for (int i = 1; i < stations.size(); i++) {
+            double dist = stations[i] - stations[i - 1];
+            needed += int(dist / maxDist);
+        }
+        
+        return needed <= k;
+    }
+};
