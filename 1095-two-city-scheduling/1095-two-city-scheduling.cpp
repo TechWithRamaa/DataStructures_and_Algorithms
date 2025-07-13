f7c60837b344a4d8b72b1a6f2171f6d75ea00268
Ramaa <rkramadhevi@gmail.com>
2025-07-14 01:14:09 +0530

    Time: 0 ms (100%), Space: 11.1 MB (47.71%) - LeetHub

--- 1095-two-city-scheduling/1095-two-city-scheduling.cpp
+++ 1095-two-city-scheduling/1095-two-city-scheduling.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // Greedy by Cost Difference
     int twoCitySchedCost(vector<vector<int>>& costs) {
         sort(costs.begin(), costs.end(), [](const vector<int> &a, const vector<int> &b){
             return a[0] - a[1] < b[0] - b[1];
