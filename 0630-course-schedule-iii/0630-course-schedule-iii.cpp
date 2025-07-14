92ac55167d775b1ea2231affd1cdbb14c2e7921c
Ramaa <rkramadhevi@gmail.com>
2025-07-14 09:26:41 +0530

    Time: 24 ms (75.94%), Space: 60.3 MB (75.94%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    /* Fails for all test cases, dont have functionalities to swap 
+       the longer courses with 2 shorter courses
+       Goal is to maximize the no of courses taken 
+    int scheduleCourse(vector<vector<int>>& courses) {
+        // sort based on the last day ie second parameter in ascending order
+        sort(courses.begin(), courses.end(), [] (const vector<int>&a, const vector<int>&b) {
+            return a[1] < b[1];
+        });
+
+        int currentDay = 0, count = 0;
+        for(const auto& current : courses) {
+            int duration = current[0], lastDay = current[1];
+    
+            int projectedCompletion = currentDay + duration;
+            if(projectedCompletion <= lastDay) {
+                count++;
+                currentDay += duration;
+            }
+        }
+
+        return count;
+    }
+    */
+
+    int scheduleCourse(vector<vector<int>>& courses) {
+        // sort based on the last day ie second parameter in ascending order
+        sort(courses.begin(), courses.end(), [] (const vector<int>&a, const vector<int>&b) {
+            return a[1] < b[1];
+        });
+
+        priority_queue<int> maxHeap;
+        int currentDay = 0;
+
+        for(const auto& course : courses) {
+            int duration = course[0], lastDay = course[1];
+            
+            currentDay += duration;
+            maxHeap.push(duration);
+
+            if(currentDay > lastDay) {
+                currentDay -= maxHeap.top();
+                maxHeap.pop();
+            }
+        }
+
+        return maxHeap.size();
+    }
+};
+
