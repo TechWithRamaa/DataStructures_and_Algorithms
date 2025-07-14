7f6d2e82d5f1487f718a82c3f55f3fb3186da858
Ramaa <rkramadhevi@gmail.com>
2025-07-14 08:00:14 +0530

    Time: 3 ms (29.95%), Space: 9 MB (47.03%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    // Partition Labels = Minimal segmentation without character overlap.
+    // Greedy, two-pointers, character-to-range mapping (Preprocessing)
+
+    // why ? any real-world analogy - Sharding without overlap, scene segmentation
+    // You want to split the movie into scenes such that no person crosses scenes.
+    
+    // Preprocessing + forward scan for local optima
     vector<int> partitionLabels(string s) {
         unordered_map<char, int> last; // Store last occurrence of each character
         vector<int> result;
@@ -17,6 +24,8 @@ public:
         for (int i = 0; i < n; i++) {
             maxEnd = max(maxEnd, last[s[i]]); // Expand partition range
             
+            // All characters in the current window end here or earlier
+            // So we can safely cut — this is a locally optimal partition
             if (i == maxEnd) { // If we reach the furthest last occurrence, partition here
                 result.push_back(i - partitionStart + 1);
                 partitionStart = i + 1;
