649de891cb46758e0e40679bd7ec46103aab22c7
Ramaa <rkramadhevi@gmail.com>
2025-07-14 21:57:47 +0530

    Time: 36 ms (37.95%), Space: 47.7 MB (18.5%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,73 +1,51 @@
 class Solution {
 public:
-    // Greedy Simulation with MaxHeap (a.k.a. CPU Cycle Simulation)
-    // FrequenceMap + MaxHeap + Scheduling & Counting the intervals
     int leastInterval(vector<char>& tasks, int n) {
-        int totalIntervals = 0;
-
         unordered_map<char, int> freqMap;
-        for (auto task : tasks) {
-            freqMap[task] += 1;
+
+        for(const auto& ch : tasks) {
+            freqMap[ch]++;
         }
 
-        priority_queue<int> maxHeap; // Greedy logic of being taking the most
-                                     // frequently occuring tasks
-        for (auto [task, freq] : freqMap) {
-            maxHeap.push(freq);
+        // maxHeap to store the most frequent tasks at the top
+        priority_queue<int> maxHeap;
+        for(const auto& [key, entry] : freqMap) {
+            maxHeap.push(entry);
         }
 
-        while (!maxHeap.empty()) {
+        int totalTime = 0;
+
+        while(!maxHeap.empty()) {
             vector<int> waitList;
             int interval = 0;
 
-            for (int i = 0; i < n + 1; i++) {
-                if (!maxHeap.empty()) {
+            for(int i = 0; i < n + 1; i++) {
+                if(!maxHeap.empty()) {
                     auto task = maxHeap.top();
                     maxHeap.pop();
 
-                    interval += 1; // task execution // 1 unit of cpu time
+                    interval++; // real-clock
 
-                    if (--task > 0) {
+                    // if a task still has frequency to execute, push into waitList to be 
+                    // executed after N clocks/interval
+                    if(--task > 0) { 
                         waitList.push_back(task);
                     }
                 } else {
-                    if (waitList.empty())
-                        break;
-                    interval += 1; // Idle time
+                    if(waitList.empty())
+                        break; // break when no tasks in maxHeap as well as in waitList
+                    
+                    interval++; // stay idle as there no tasks in MaxHeap
                 }
             }
 
-            for (auto task : waitList) {
+            for(auto task : waitList) {
                 maxHeap.push(task);
             }
 
-            totalIntervals += maxHeap.empty() ? interval : n + 1;
+            totalTime += maxHeap.empty() ? interval : n + 1;
         }
-        return totalIntervals;
-    }
 
-    // This approach is based on mathematical greedy approach
-    // Best suited for CP 
-    // Quicker calculation, but works only on static set of tasks & interval
-    // TC ~ O ( n log n ) ; SC ~ O (n)
-    int leastIntervalMathGreedy(vector<char>& tasks, int n) {
-        unordered_map<char, int> freqMap;
-        for (char task : tasks) 
-            freqMap[task]++;
-
-        vector<int> frequencies;
-        for (auto& pair : freqMap) 
-            frequencies.push_back(pair.second);
-        
-        sort(frequencies.rbegin(), frequencies.rend());
-
-        int maxFreq = frequencies[0];
-        int idleSlots = (maxFreq - 1) * n;
-
-        for (int i = 1; i < frequencies.size(); i++) 
-            idleSlots -= min(frequencies[i], maxFreq - 1);
-        
-        idleSlots = max(0, idleSlots);
-        return tasks.size() + idleSlots;  
+        return totalTime;
     }
 };
