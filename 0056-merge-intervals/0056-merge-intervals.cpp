de35d214d5f10dfa06bf9f8ec8f8a4f672732faf
Ramaa <rkramadhevi@gmail.com>
2025-07-14 07:13:17 +0530

    Time: 7 ms (41.81%), Space: 24 MB (31.61%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
+    // Merging Overlapping Intervals
+    // Core Idea (Greedy + Sorting)
+    //      Sorting the intervals by start time.
+    //      Iterating through them and:
+    //          If the current interval overlaps with the previous one (i.e., start ≤ previous end), merge them.
+    //          Otherwise, simply add it to the result list.
+
+    // TC -> Sorting: O(n log n) and Merging: O(n) => Total: O(n log n)
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        
+
         vector<vector<int>> merged;
 
-        for(const auto& interval : intervals) {
-            int start = interval[0], end = interval[1];
+        for(const auto& current : intervals) {
+            int start = current[0], end = current[1];
 
             if(!merged.empty() && start <= merged.back()[1]) {
                 merged.back()[1] = max(merged.back()[1], end);
@@ -18,5 +26,3 @@ public:
         return merged;
     }
 };
-
-//[1, 6],[8, 10],[15, 18]
