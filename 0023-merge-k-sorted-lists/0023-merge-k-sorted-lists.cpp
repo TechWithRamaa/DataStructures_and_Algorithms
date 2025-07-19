16441d9e587e114cc5a88722e2c1a89bdc8e4bc3
Ramaa <rkramadhevi@gmail.com>
2025-07-19 16:40:17 +0530

    Time: 0 ms (100%), Space: 18.4 MB (66.63%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -81,9 +81,9 @@ public:
 
         priority_queue<ListNode*, vector<ListNode*>, Compare> minHeap;
 
-        for (auto list : lists) {
-            if (list)
-                minHeap.push(list);
+        for (auto listHead : lists) {
+            if (listHead)
+                minHeap.push(listHead);
         }
 
         ListNode dummy(0);
@@ -101,43 +101,6 @@ public:
             temp = temp->next;
         }
 
-        return dummy.next;
-    }
-
-    // Approach 2 (repeated for practice)
-    ListNode* mergeKListsX(vector<ListNode*>& lists) {
-        if (lists.empty())
-            return nullptr;
-
-        int n = lists.size();
-        while (n > 1) {
-            for (int i = 0; i < n / 2; i++)
-                lists[i] = mergeTwoSortedLists(lists[i], lists[n - 1 - i]);
-            n = (n + 1) / 2;
-        }
-        return lists[0];
-    }
-
-private:
-    ListNode* mergeTwoSortedLists(ListNode* head1, ListNode* head2) {
-        if (!head1)
-            return head2;
-        if (!head2)
-            return head1;
-
-        ListNode dummy(0);
-        ListNode* current = &dummy;
-        while (head1 && head2) {
-            if (head1->val <= head2->val) {
-                current->next = head1;
-                head1 = head1->next;
-            } else {
-                current->next = head2;
-                head2 = head2->next;
-            }
-            current = current->next;
-        }
-        current->next = (head1) ? head1 : head2;
         return dummy.next;
     }
 };
