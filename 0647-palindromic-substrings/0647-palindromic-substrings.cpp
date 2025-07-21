fbd8d2513c265dc3b4667a0f3cfebba07ca7eddb
Ramaa <rkramadhevi@gmail.com>
2025-07-21 17:04:58 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.01%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    // Expand Around Centers
+    /*
+    A palindrome is defined by its center.
+    Each character can be the center of an odd-length palindrome.
+    Each pair of characters can be the center of an even-length palindrome.
+
+    Algorithm:-
+
+    Iterate over each index i in the string.
+    Expand around i (odd-length palindromes).
+    Expand around i and i+1 (even-length palindromes).
+    Count palindromes as we expand.
+    */
     int countSubstrings(string s) {
         int n = s.size();
         int count = 0;
