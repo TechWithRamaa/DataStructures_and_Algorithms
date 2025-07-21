6db8784c1e9db5415c8385277d6dd5ad8e86925a
Ramaa <rkramadhevi@gmail.com>
2025-07-21 17:06:29 +0530

    Time: 0 ms (100%), Space: 8.4 MB (91.57%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -16,7 +16,17 @@ public:
     int countSubstrings(string s) {
         int n = s.size();
         int count = 0;
-        
+
+        /*
+        Number of centers
+        For a string of length n, there are:
+
+        n centers for odd-length palindromes (each character can be the center).
+
+        n - 1 centers for even-length palindromes (between every adjacent pair).
+
+        Total centers = n + (n - 1) = 2n - 1.
+        */
         for (int center = 0; center < 2 * n - 1; center++) {
             int left = center / 2;
             int right = left + (center % 2);
