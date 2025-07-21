529d75bb94cb556bd468b91ca1d5de86dbff0531
Ramaa <rkramadhevi@gmail.com>
2025-07-22 01:58:06 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -15,22 +15,52 @@ public:
 class Solution {
 public:
     // Top-Down
-     int solve(int m, int n, vector<vector<int>>& dp) {
+     int solveR(int m, int n, vector<vector<int>>& dp) {
         if (m == 0 || n == 0)
             return 1;
 
         if (dp[m][n] != -1) 
             return dp[m][n]; 
 
-        int noOfLeftPaths = solve(m, n - 1, dp);
-        int noOfTopPaths = solve(m - 1, n, dp);
+        int noOfLeftPaths = solveR(m, n - 1, dp);
+        int noOfTopPaths = solveR(m - 1, n, dp);
 
         return dp[m][n] = noOfLeftPaths + noOfTopPaths;
     }
 
-    int uniquePaths(int m, int n) {
+    int uniquePathsR(int m, int n) {
         vector<vector<int>> dp;
         dp.resize(m, vector<int>(n, -1));  
-        return solve(m - 1, n - 1, dp);
+        return solveR(m - 1, n - 1, dp);
+    }
+
+    /* Bottom-Up
+    
+    dp[i][j] = the number of unique paths to reach cell (i,j)
+    
+    To reach (i,j), we could have come:
+    From the top: (i-1, j)
+    From the left: (i, j-1)
+
+    dp[i][j]=dp[i−1][j]+dp[i][j−1]
+    */
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // Base cases: first row and first column
+        for (int j = 0; j < n; j++) 
+            dp[0][j] = 1;
+        for (int i = 0; i < m; i++) 
+            dp[i][0] = 1;
+
+        // Fill the remaining cells
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
