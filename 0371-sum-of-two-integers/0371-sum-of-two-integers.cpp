81a57734541e75556c9fd2881654b398ea7fe754
Ramaa <rkramadhevi@gmail.com>
2025-07-22 23:41:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (66.12%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int getSum(int a, int b) {
         while (b != 0) {
-            int sum = a ^ b;
-            int carry = (a & b) << 1;
+            int sum = a ^ b;    // Gives the sum of bits without carry
+            int carry = (a & b) << 1;  // Computes the carry that needs to be added in the next iteration.
 
             a = sum;
             b = carry;
