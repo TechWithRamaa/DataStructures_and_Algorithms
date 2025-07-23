67433f2cbd3789c2da1eb3484e66f31d96d513c4
Ramaa <rkramadhevi@gmail.com>
2025-07-24 04:30:54 +0530

    Time: 63 ms (63.88%), Space: 14.3 MB (64.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     // Binary Search + DP ~ O ( n log n )
     int lengthOfLISBS(vector<int>& nums) {
-        vector<int> sub; 
+        vector<int> sub;
 
         for (int num : nums) {
-            auto it = lower_bound(sub.begin(), sub.end(), num); 
+            auto it = lower_bound(sub.begin(), sub.end(), num);
 
             // extend the sequence
             if (it == sub.end()) {
-                sub.push_back(num); 
+                sub.push_back(num);
             } else { // replace the sequence
                 *it = num;
             }
@@ -23,40 +23,44 @@ public:
         vector<vector<int>> dp(n, vector<int>(n, -1));
         return lis(0, -1, nums, dp);
     }
- 
+
     // DP ~ O ( n ^ 2)
     /* Goal of the DP approach:
-        
+
        We are computing:
 
-          For each i, what is the length of the Longest Increasing Subsequence (LIS) that ends at index i?
+          For each i, what is the length of the Longest Increasing Subsequence
+       (LIS) that ends at index i?
 
           To do that, we consider every previous index j < i, because:
 
-          We are asking: “Can nums[i] be added to an increasing subsequence that ends at j?”
+          We are asking: “Can nums[i] be added to an increasing subsequence that
+       ends at j?”
 
-          If yes (i.e., nums[j] < nums[i]), then we can extend that subsequence by 1.
+          If yes (i.e., nums[j] < nums[i]), then we can extend that subsequence
+       by 1.
 
     */
 
     // dp[i]=length of LIS ending at index i
-    
+
     int lengthOfLIS(vector<int>& nums) {
         int N = nums.size();
-        vector<int> dp(N, 1); // dp[i] starts with 1 (each element is an LIS of length 1)
+        vector<int> dp(
+            N, 1); // dp[i] starts with 1 (each element is an LIS of length 1)
 
         int maxLength = 1;
         for (int i = 1; i < N; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
-                  dp[i] = max(dp[i], dp[j] + 1);
+                    dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
-           maxLength = max(maxLength, dp[i]);
+            maxLength = max(maxLength, dp[i]);
         }
 
         return maxLength;
-}
+    }
 
 private:
     int lis(int index, int prevIndex, vector<int>& nums,
