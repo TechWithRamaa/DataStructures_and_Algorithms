9fd01a1db834f29478b076b23c0d3d68d4d120eb
Ramaa <rkramadhevi@gmail.com>
2025-07-24 03:37:51 +0530

    Time: 0 ms (100%), Space: 10.9 MB (39.62%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -3,18 +3,42 @@ public:
     int rob(vector<int>& nums) {
         int N = nums.size();
 
-        if(N == 1) {
+        if (N == 1) {
             return nums[0];
         }
 
         return max(rob(0, N - 2, nums), rob(1, N - 1, nums));
     }
+
 private:
-    int rob(int start, int end, const vector<int>& nums) {
+    int rob(int start, int end, vector<int>& nums) {
+        int N = end - start + 1;
+        
+        if(N == 0)
+            return 0;
+
+        if (N == 1)
+            return nums[start];
+
+        vector<int> dp(N);
+        dp[0] = nums[start];
+        dp[1] = max(nums[start], nums[start + 1]);
+
+        for (int i = 2; i < N; i++) {
+            int rob = nums[start + i] + dp[i - 2];
+            int skip = dp[i - 1];
+
+            dp[i] = max(skip, rob);
+        }
+
+        return dp[N - 1];
+    }
+
+    int robS(int start, int end, const vector<int>& nums) {
         int maxRob = 0;
 
         int secondLast = 0, last = 0;
-        for(int i = start; i <= end; i++) {
+        for (int i = start; i <= end; i++) {
             int rob = nums[i] + secondLast;
             int dontRob = last;
 
