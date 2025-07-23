04e14d510410ba5047b5ab75ca530888d35afd77
Ramaa <rkramadhevi@gmail.com>
2025-07-24 00:08:34 +0530

    Time: 2 ms (82.5%), Space: 12.5 MB (70.83%) - LeetHub

---
+++ 0723-candy-crush/0723-candy-crush.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    // simulation until stable
+    vector<vector<int>> candyCrush(vector<vector<int>>& board) {
+        int M = board.size(), N = board[0].size();
+        bool changed = true;
+
+        while(changed) {
+            changed = false; 
+
+            // Mark candies
+            for(int i = 0; i < M; i++) {
+                for(int j = 0; j < N; j++) {
+                    int val = abs(board[i][j]);
+                    
+                    if(val == 0)
+                        continue;
+                    
+                    // check for minimum 3 cells & extend further & mark it
+                    if(j + 2 < N && abs(board[i][j + 1]) == val && abs(board[i][j + 2]) == val) {
+                        int k = j;
+                        
+                        // marking them for crushing / dropping
+                        while(k < N && abs(board[i][k]) == val)
+                            board[i][k++] = -val;
+
+                        changed = true; 
+                    }
+
+                    // check for minimum 3 cells & extend further & mark it
+                    if(i + 2 < M && abs(board[i + 1][j]) == val && abs(board[i + 2][j]) == val) {
+                        int k = i;
+                        
+                        // marking them for crushing / dropping
+                        while(k < M && abs(board[k][j]) == val)
+                            board[k++][j] = -val;
+
+                        changed = true; 
+                    }
+                }
+            }
+            
+            // crush it
+            for(int i = 0; i < M; i++) {
+                for(int j = 0; j < N; j++) {
+                    if(board[i][j] < 0)
+                        board[i][j] = 0;
+                }
+            }
+
+            // gravity - core technique => move non-zero elements to the front 
+            for(int j = 0; j < N; j++) {
+                int idx = M - 1; // write pointer
+
+                for(int i = M - 1; i >= 0; i--) {
+                    if(board[i][j] > 0) 
+                        board[idx--][j] = board[i][j];
+                }
+
+                // mark the rest of the fields zero
+                while(idx >= 0) 
+                    board[idx--][j] = 0; 
+            }
+        }
+
+        return board;
+    }
+};
