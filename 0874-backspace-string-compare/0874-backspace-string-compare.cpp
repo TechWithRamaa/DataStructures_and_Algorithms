8d1e28dee8237c0ae08f0744f840a92f453f3f05
Ramaa <rkramadhevi@gmail.com>
2025-07-24 00:25:21 +0530

    Time: 0 ms (100%), Space: 8.3 MB (96.41%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool backspaceCompare(string s, string t) {
+    // takes extra space, hence not optimized
+    bool backspaceCompareB(string s, string t) {
         string temp1;
         for(auto& ch : s) {
             if(ch == '#') {
@@ -20,5 +21,52 @@ public:
         }
 
         return temp1 == temp2;
+    }
+
+    bool backspaceCompare(string s, string t) {
+        int i = s.size() - 1, j = t.size() - 1;
+        int skipS = 0, skipT = 0;
+        
+        while (i >= 0 || j >= 0) {
+            // Process backspaces in s
+            while (i >= 0) {
+                if (s[i] == '#') { 
+                    skipS++; 
+                    i--;
+                }
+                else if (skipS > 0) { 
+                    skipS--; 
+                    i--; 
+                }
+                else 
+                    break;
+            }
+
+            // Process backspaces in t
+            while (j >= 0) {
+                if (t[j] == '#') { 
+                    skipT++; 
+                    j--; 
+                }
+                else if (skipT > 0) { 
+                    skipT--; 
+                    j--; 
+                }
+                else 
+                    break;
+            }
+
+            // Compare current characters
+            if (i >= 0 && j >= 0 && s[i] != t[j]) 
+                return false;
+
+            if ((i >= 0) != (j >= 0)) 
+                return false; // One string ended
+
+            i--;
+            j--;
+        }
+        
+        return true;
     }
 };
