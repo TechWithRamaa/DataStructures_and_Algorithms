b2ad40cc810fe521bb83564ea8c3fc1e8884e396
Ramaa <rkramadhevi@gmail.com>
2025-07-24 04:14:38 +0530

    Time: 55 ms (19.43%), Space: 53.1 MB (13.48%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
+        int N = coins.size();
         const int INF = 1e9; // Large value to represent impossible states
 
         // dp[i][a] = minimum number of coins to make 'a' using first i coins
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INF)); // (INF) to represent "impossible".
+        vector<vector<int>> dp(N + 1, vector<int>(amount + 1, INF)); // (INF) to represent "impossible".
 
         // Base Case: To form amount = 0, we need 0 coins
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i <= N; i++) {
             dp[i][0] = 0;
         }
 
         // Fill the table
-        for (int i = 1; i <= n; i++) {          // i = 1..n (coin index)
+        for (int i = 1; i <= N; i++) {          // i = 1..n (coin index)
             for (int a = 1; a <= amount; a++) { // a = 1..amount
                 int notTake = dp[i - 1][a];     // Don't take the i-th coin
 
@@ -30,6 +30,6 @@ public:
         }
 
         // If dp[n][amount] is INF, it means it's impossible to make that amount
-        return (dp[n][amount] == INF) ? -1 : dp[n][amount];
+        return (dp[N][amount] == INF) ? -1 : dp[N][amount];
     }
 };
