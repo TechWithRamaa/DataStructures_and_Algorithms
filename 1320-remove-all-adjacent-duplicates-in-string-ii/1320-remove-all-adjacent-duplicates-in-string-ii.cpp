d3714dd5a4d5c53e549d40a6ea9261820557e06e
Ramaa <rkramadhevi@gmail.com>
2025-07-23 23:40:41 +0530

    Time: 4 ms (78.01%), Space: 13.7 MB (79.3%) - LeetHub

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    /*
+    Greedy + Stack Approach
+    
+    Traverse the string character by character.
+    Use a stack to keep track of (character, count):
+        If the current character is the same as the stack’s top character, increment its count.
+        If the count becomes k, pop it from the stack (remove those k characters).
+        Otherwise, push a new pair (char, 1).
+    Build the result from the stack at the end.
+    */
+    string removeDuplicates(string s, int k) {
+        vector<pair<char, int>> stack;
+
+        for(auto& ch : s) {
+            if(!stack.empty() && stack.back().first == ch) {
+                stack.back().second++;
+
+                if(stack.back().second == k) {
+                    stack.pop_back();
+                }
+            } else {
+                stack.push_back({ch, 1});
+            }
+        }
+
+        string result = "";
+        for(auto& [ch, count] : stack) {
+            result.append(count, ch);
+        }
+
+        return result;
+    }
+};
