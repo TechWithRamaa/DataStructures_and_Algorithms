8e2d41d96bf4fc9a3648eb39e6702c69d0bcb399
Ramaa <rkramadhevi@gmail.com>
2025-07-24 17:54:17 +0530

    Time: 4 ms (20.35%), Space: 15.9 MB (73.18%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -12,8 +12,7 @@ public:
         dp[0][2] = 0;          // rest
 
         for (int i = 1; i < n; ++i) {
-            dp[i][0] = max(dp[i - 1][0],
-                           dp[i - 1][2] - prices[i]);   // buy or carry forward
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i]);   // buy or carry forward
             dp[i][1] = dp[i - 1][0] + prices[i];        // sell
             dp[i][2] = max(dp[i - 1][1], dp[i - 1][2]); // rest
         }
