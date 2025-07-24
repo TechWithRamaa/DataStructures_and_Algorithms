ffb0d808ac4547222464fa2e3dbbd2501f37730d
Ramaa <rkramadhevi@gmail.com>
2025-07-24 18:42:54 +0530

    Time: 6 ms (35.63%), Space: 9.2 MB (77.7%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // dp[i][j] = true if s3[0...(i+j-1)] is formed by interleaving s1[0...(i-1)] and s2[0...(j-1)]
     bool isInterleave(string s1, string s2, string s3) {
         int N = s1.size(), M = s2.size(), T = s3.size();
         if (N + M != T)
@@ -8,9 +9,16 @@ public:
         vector<vector<bool>> dp(N + 1, vector<bool>(M + 1, false));
         dp[0][0] = true;
 
+        /*
+        Base Cases:
+        dp[0][0] = true → empty strings interleave to form empty string.
+        dp[i][0] = dp[i-1][0] && s1[i-1] == s3[i-1] → only s1 is used.
+        dp[0][j] = dp[0][j-1] && s2[j-1] == s3[j-1] → only s2 is used.
+        */
+
         for (int i = 0; i <= N; ++i) {
             for (int j = 0; j <= M; ++j) {
-                if (i > 0 && s1[i - 1] == s3[i + j - 1]) 
+                if (i > 0 && s1[i - 1] == s3[i + j - 1]) // The base cases are being handled inside the loop when i == 0 or j == 0.
                     dp[i][j] = dp[i][j] || dp[i - 1][j];
 
                 if (j > 0 && s2[j - 1] == s3[i + j - 1]) {
