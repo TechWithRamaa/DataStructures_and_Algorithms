8ef4aba45b0cdd0db26708258e7e4ca824fc47d1
Ramaa <rkramadhevi@gmail.com>
2025-07-26 02:10:22 +0530

    Time: 0 ms (100%), Space: 8.8 MB (30.57%) - LeetHub

---
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    /*
+    We use prefix-suffix in splitting problems because:
+        We're analyzing two parts of a structure.
+        We want to avoid repeated computation.
+        We can compute one part on the fly (prefix), and derive the other (suffix) from totals.
+    */
+    int maxScore(string s) {
+        int totalOnes = count(s.begin(), s.end(), '1');
+
+        int leftZeros = 0, rightOnes = totalOnes;
+        int maxScore = 0;
+
+        for (int i = 0; i < s.length() - 1; ++i) {
+            if (s[i] == '0')
+                leftZeros++;
+            else
+                rightOnes--;
+            maxScore = max(maxScore, leftZeros + rightOnes);
+        }
+
+        return maxScore;
+    }
+};
