d9c8e03ed2020ac32cfd1e0a3769cb1258efb7c6
Ramaa <rkramadhevi@gmail.com>
2025-07-27 02:36:02 +0530

    Time: 0 ms (100%), Space: 11.6 MB (75.4%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    /*
+    Pattern: Two Pointer - Overwrite Style
+
+    Related to: Move Zeros, Remove Duplicates from Sorted Array
+
+    Key trick: Don’t care what's beyond index k, just make sure the first k values are valid.
+    */
     int removeElement(vector<int>& nums, int val) {
         int k = 0; // index for placing valid elements
         for (int i = 0; i < nums.size(); i++) {
