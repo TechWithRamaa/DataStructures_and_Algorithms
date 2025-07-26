ae2a43afe85d4e85c07a8c5a98aa0e1550c41b6d
Ramaa <rkramadhevi@gmail.com>
2025-07-27 04:04:46 +0530

    Time: 0 ms (100%), Space: 8.6 MB (67.97%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
+    bool isPalindromeX(int x) {
         string s = to_string(x);
         int l = 0, r = s.size() - 1;
         while (l < r) {
@@ -8,5 +8,20 @@ public:
                 return false;
         }
         return true;
+    }
+
+    bool isPalindrome(int x) {
+        // Edge case: negative numbers or those ending in 0 (but not 0 itself)
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even/odd digits, compare accordingly
+        return x == reversedHalf || x == reversedHalf / 10;
     }
 };
