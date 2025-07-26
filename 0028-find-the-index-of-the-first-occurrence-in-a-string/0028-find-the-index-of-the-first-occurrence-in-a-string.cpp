b94f4ccc6c94d543f1ee97d9b3c17c88faa50a15
Ramaa <rkramadhevi@gmail.com>
2025-07-27 01:05:39 +0530

    Time: 0 ms (100%), Space: 8.8 MB (32.24%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,35 +5,80 @@ public:
     int strStr3(string haystack, string needle) {
         int n = haystack.length(), m = needle.length();
 
-        for(int i = 0; i <= n - m; i++) {
-            if(haystack.substr(i, m) == needle) {
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substr(i, m) == needle) {
                 return i;
             }
         }
 
         return -1;
     }
-    
+
     // BruteForce using TwoPointer
     // TC ~ O ( n * m )
-    int strStr(string hayback, string needle) {
+    int strStrB(string hayback, string needle) {
         int n = hayback.length(), m = needle.length();
-        int left = 0;
+        int i = 0;
 
-        while(left <= n - m) {
-            int right = 0;
-            while(right < m && hayback[left + right] == needle[right]) {
-                right++;
+        while (i <= n - m) {
+            int end = 0; // always start end from 0 at each iteration // end
+                         // tracks indices over needle
+
+            // fix i as the starting point & expand end
+            while (end < m && hayback[i + end] == needle[end]) {
+                end++; // expand the matching part further
             }
 
-            if(right == m) {
-                return left;
+            // if end exactly becomes the length of substring, then match found
+            if (end == m) {
+                return i;
             }
 
-            left++;
+            i++; // otherwise try from next position onwards again
         }
 
         return -1;
     }
 
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+
+        if (m == 0)
+            return 0;
+
+        // Build LPS
+        vector<int> lps(m, 0);
+        int len = 0; // length of the previous longest prefix suffix
+        for (int i = 1; i < m;) {
+            if (needle[i] == needle[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0)
+                    len = lps[len - 1];
+                else
+                    lps[i++] = 0;
+            }
+        }
+
+        // KMP Search
+        int i = 0, j = 0;
+        while (i < n) {
+            if (haystack[i] == needle[j]) {
+                i++, j++;
+            }
+            if (j == m)
+                return i - j;
+            else if (i < n && haystack[i] != needle[j]) {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+
+        return -1;
+    }
 };
