f913f7dd7cd380ccd9f39d3cdcec3e76bd31c77b
Ramaa <rkramadhevi@gmail.com>
2025-07-28 01:21:25 +0530

    Time: 6 ms (7.5%), Space: 14.3 MB (8.6%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -1,43 +1,52 @@
 class Solution {
 public:
-int possibleStringCount(string word) {
-    vector<pair<char, int>> groups;
-    int n = word.size();
-    
-    for (int i = 0; i < n;) {
-        int j = i;
-        while (j < n && word[j] == word[i]) j++;
-        groups.push_back({word[i], j - i});
-        i = j;
-    }
+    int possibleStringCount(string word) {
+        int N = word.size();
+        vector<pair<char, int>> groups;
 
-    set<string> possible;
+        for (size_t i = 0; i < N;) {
+            int j = i;
 
-    // Case 1: No long press
-    string original = "";
-    for (auto [ch, cnt] : groups)
-        original += string(cnt, ch);
-    possible.insert(original);
+            while (j < N && word[i] == word[j])
+                j++;
 
-    // Case 2: Try reducing exactly one group with count > 1
-    for (int i = 0; i < groups.size(); ++i) {
-        auto [ch, cnt] = groups[i];
-        if (cnt == 1) continue;
+            groups.push_back({word[i], j - i});
 
-        // Try all reduced sizes (from cnt-1 down to 1)
-        for (int reduceTo = 1; reduceTo < cnt; ++reduceTo) {
-            string temp = "";
-            for (int j = 0; j < groups.size(); ++j) {
-                if (j == i)
-                    temp += string(reduceTo, groups[j].first);
-                else
-                    temp += string(groups[j].second, groups[j].first);
-            }
-            possible.insert(temp);
+            i = j;
         }
+
+        set<string> possible;
+
+        // Case 1 - No long press
+        string original = "";
+        for (auto& [ch, count] : groups) {
+            original += string(count, ch);
+        }
+
+        possible.insert(original);
+
+        // Case 2 - Try reducing exactly one group with count > 1
+        for (int i = 0; i < groups.size(); i++) {
+            auto [ch, count] = groups[i];
+            
+            if(count == 1)
+                continue;
+            
+            for(int reduceTo = 1; reduceTo < count; reduceTo++) {
+                string temp = "";
+                
+                for(int j = 0; j < groups.size(); j++) {
+                    if(j == i) {
+                        temp += string(reduceTo, groups[j].first);
+                    } else {
+                        temp += string(groups[j].second, groups[j].first);
+                    }
+                }
+
+                possible.insert(temp);
+            }
+        }
+
+        return possible.size();
     }
-
-    return possible.size();
-}
-
 };
