f542da624a8d3ea35af1618396e4f6addf9646b5
Ramaa <rkramadhevi@gmail.com>
2025-07-28 22:20:11 +0530

    Time: 10 ms (70.29%), Space: 13.5 MB (24.88%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    int count = 0;
-    int maxOR = 0;
+    int countMaxOrSubsets(vector<int>& nums) {
+        int maxOR = 0;
+        for (int x : nums) maxOR |= x;
 
-    void dfs(vector<int>& nums, int i, int currOR) {
-        if (i == nums.size()) {
-            if (currOR == maxOR) count++;
-            return;
+        // dp[orValue] = number of ways to reach that OR value
+        unordered_map<int, int> dp;
+        dp[0] = 1; // base case: one way to get OR = 0 (empty subset)
+
+        for (int num : nums) {
+            unordered_map<int, int> new_dp = dp;
+
+            for (auto [prevOR, count] : dp) {
+                // ----- EXCLUDE num -----
+                // new_dp[prevOR] already has count from previous dp
+
+                // ----- INCLUDE num -----
+                int newOR = prevOR | num;
+                new_dp[newOR] += count;
+            }
+
+            dp = move(new_dp); // update dp with include + exclude results
         }
 
-        // Exclude nums[i]
-        dfs(nums, i + 1, currOR);
-
-        // Include nums[i]
-        dfs(nums, i + 1, currOR | nums[i]);
-    }
-
-    int countMaxOrSubsets(vector<int>& nums) {
-        for (int x : nums) maxOR |= x;
-        dfs(nums, 0, 0);
-        return count;
+        return dp[maxOR];
     }
 };
