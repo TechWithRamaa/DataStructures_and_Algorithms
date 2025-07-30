c2b88670112d5bc3ab373956fc8f41bf171b9c41
Ramaa <rkramadhevi@gmail.com>
2025-07-30 12:09:37 +0530

    Time: 0 ms (100%), Space: 16.4 MB (77.13%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,5 +1,47 @@
 class Solution {
 public:
+    // Find with path compression
+    int find(vector<int>& parent, int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent, parent[x]);
+        return parent[x];
+    }
+
+    // Union operation
+    void unite(vector<int>& parent, int x, int y) {
+        int rootX = find(parent, x);
+        int rootY = find(parent, y);
+        if (rootX != rootY) {
+            parent[rootY] = rootX;  // Merge the components
+        }
+    }
+
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<int> parent(n);
+        
+        // Initially, each node is its own parent (n components)
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+
+        // Merge nodes based on edges
+        for (const auto& edge : edges) {
+            unite(parent, edge[0], edge[1]);
+        }
+
+        // Count unique roots (representing connected components)
+        unordered_set<int> uniqueRoots;
+        for (int i = 0; i < n; ++i) {
+            uniqueRoots.insert(find(parent, i));
+        }
+
+        return uniqueRoots.size();
+    }
+};
+
+
+class Solution1 {
+public:
     void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
         visited[node] = true;
         for (int neighbor : adj[node]) {
