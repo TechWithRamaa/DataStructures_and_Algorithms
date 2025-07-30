77ccec1ae808a278c3b34fd8a5e06b16acf838bb
Ramaa <rkramadhevi@gmail.com>
2025-07-30 10:28:24 +0530

    Time: 26 ms (83.75%), Space: 171.8 MB (75.35%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -6,28 +6,28 @@ public:
             count[word]++;
         }
 
-        int answer = 0;
+        int length = 0;
         bool central = false;
 
         for (const auto& [word, countOfTheWord] : count) {
             // if the word is a palindrome
             if (word[0] == word[1]) {
                 if (countOfTheWord % 2 == 0) {
-                    answer += countOfTheWord;
+                    length += countOfTheWord;
                 } else {
-                    answer += countOfTheWord - 1;
+                    length += countOfTheWord - 1;
                     central = true;
                 }
                 // consider a pair of non-palindrome words such that one is the
                 // reverse of another ({word[1], word[0]} is the reversed word)
             } else if (word[0] < word[1] && count.count({word[1], word[0]})) {
-                answer += 2 * min(countOfTheWord, count[{word[1], word[0]}]);
+                length += 2 * min(countOfTheWord, count[{word[1], word[0]}]);
             }
         }
 
         if (central) {
-            answer++;
+            length++;
         }
-        return 2 * answer;
+        return 2 * length;
     }
 };
